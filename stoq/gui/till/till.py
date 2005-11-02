1ef7fea1307cf47f8ab5587cd2a4945ce09efaaf
evandro <None>
2005-11-02 21:03:56 +0000

    Removing BaseListSlave references since this is a useless slave.

--- stoq/gui/till/till.py
+++ stoq/gui/till/till.py
@@ -29,13 +29,13 @@ stoq/gui/till/till.py:
     Implementation of till application.
 """
 
-import gtk
 import gettext
-from datetime import date
+import datetime
 
+import gtk
 from sqlobject.sqlbuilder import AND
 from kiwi.ui.widgets.list import Column
-from stoqlib.gui.search import BaseListSlave, SearchBar
+from stoqlib.gui.search import SearchBar
 from stoqlib.gui.columns import ForeignKeyColumn
 from stoqlib.exceptions import TillError
 from stoqlib.database import rollback_and_begin
@@ -49,14 +49,16 @@ from stoq.lib.validators import get_formatted_price
 from stoq.gui.application import AppWindow
 from stoq.gui.editors.till import TillOpeningEditor, TillClosingEditor
 from stoq.gui.till.operation import TillOperationDialog
+
 _ = gettext.gettext
+
 
 class TillApp(AppWindow):
 
     app_name = _('Till')
     gladefile = 'till'
     widgets = ('searchbar_holder',
-               'klist_holder',
+               'sales_list',
                'confirm_order_button',
                'TillMenu',
                'TillOpen',
@@ -73,7 +75,7 @@ class TillApp(AppWindow):
 
     def get_title(self):
         today_format = _('%d of %B')
-        today_str = date.today().strftime(today_format)
+        today_str = datetime.datetime.today().strftime(today_format)
         return _('Stoq - %s of %s') % (self.app_name, today_str)
 
     def _setup_widgets(self):
@@ -84,38 +86,27 @@ class TillApp(AppWindow):
         self.TillClose.set_sensitive(has_till)
         self.TillOpen.set_sensitive(not has_till)
         self.TillOperations.set_sensitive(has_till)
-        has_sales = len(self.sale_list) > 0
+        has_sales = len(self.sales_list) > 0
         self.confirm_order_button.set_sensitive(has_sales)
 
     def _setup_slaves(self):
-        list_slave = BaseListSlave(columns=self.get_columns())
-        self.attach_slave('klist_holder', list_slave)
-        self.sale_list = list_slave.klist
-        self.searchbar = SearchBar(self, Sale, self.get_columns())
+        self.sales_list.set_columns(self._get_columns())
+        self.searchbar = SearchBar(self, Sale, self._get_columns())
         self.searchbar.set_result_strings(_('sale'), _('sales'))
         self.searchbar.set_searchbar_labels(_('Sales Matching:'))
         self.searchbar.search_items()
         self.attach_slave('searchbar_holder', self.searchbar)
 
-
-
-    #
-    # BaseListSlave hooks
-    #
-
-
-
-    def get_columns(self):
+    def _get_columns(self):
         return [Column('order_number', title=_('Order'), width=100, 
                        data_type=int, sorted=True),
                 Column('open_date', title=_('Date'), width=120, 
-                       data_type=date, justify=gtk.JUSTIFY_RIGHT),
+                       data_type=datetime.date, justify=gtk.JUSTIFY_RIGHT),
                 ForeignKeyColumn(Person, 'name', title=_('Client'), expand=True,
                                  data_type=str, obj_field='client._original'),
                 Column('total_sale_amount', title=_('Total'), width=150, 
                        data_type=float, justify=gtk.JUSTIFY_RIGHT,
                        format_func=get_formatted_price)]
-                       
 
     def get_extra_query(self):
         q1 = Sale.q.clientID == PersonAdaptToClient.q.id
@@ -125,12 +116,12 @@ class TillApp(AppWindow):
 
     def update_klist(self, sales=[]):
         rollback_and_begin(self.conn)
-        self.sale_list.clear()
+        self.sales_list.clear()
         for sale in sales:
             # Since search bar change the connection internally we must get
             # the objects back in our main connection
             obj = Sale.get(sale.id, connection=self.conn)
-            self.sale_list.append(obj)
+            self.sales_list.append(obj)
         self._update_widgets()
 
     def open_till(self):
@@ -188,18 +179,14 @@ class TillApp(AppWindow):
             sale.till = new_till
         self.conn.commit()
 
-
-
     #
     # Kiwi callbacks
     #
    
-
-
     def on_confirm_order_button__clicked(self, *args):
         rollback_and_begin(self.conn)
         # TODO Call SaleWizard dialog and let the user change the sale here
-        sale = self.sale_list.get_selected()
+        sale = self.sales_list.get_selected()
         sale.confirm()
         self.conn.commit()
         self.searchbar.search_items()
