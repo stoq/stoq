7ae9573b99aa0694fc308f5ccdc47835c64b955d
evandro <None>
2006-01-12 14:30:08 +0000

    Ops...

--- stoq/examples/payment.py
+++ stoq/examples/payment.py
@@ -107,7 +107,6 @@ def create_payments():
 
     pm = sysparam(conn).BASE_PAYMENT_METHOD
     for iface in [ICheckPM, IBillPM]:
-        print 'original'
         method = iface(pm, connection=conn)
         method.max_installments_number = MAX_INSTALLMENTS_NUMBER
         
