e45b63845920be9fbd47ce585978fd3df2cfe8f7
evandro <None>
2006-01-18 22:51:32 +0000

    Partial fix for bug 2256: implement a ReceivingOrderWizard.
    r=henrique

--- stoq/gui/search/product.py
+++ stoq/gui/search/product.py
@@ -20,7 +20,8 @@
 ## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 ## USA.
 ##
-## Author(s):   Bruno Rafael Garcia      <brg@async.com.br>
+## Author(s):   Bruno Rafael Garcia         <brg@async.com.br>
+##              Evandro Vale Miquelito      <evandro@async.com.br>
 ##
 """
 stoq/gui/search/product.py
@@ -30,12 +31,14 @@ stoq/gui/search/product.py
 
 import gettext
 
+import gtk
 from kiwi.datatypes import currency
+from sqlobject.sqlbuilder import AND
 from stoqlib.gui.search import SearchEditor
-from stoqlib.gui.columns import Column, AccessorColumn
+from stoqlib.gui.columns import Column, AccessorColumn, ForeignKeyColumn
 
 from stoq.lib.defaults import ALL_ITEMS_INDEX
-from stoq.domain.sellable import AbstractSellable
+from stoq.domain.sellable import AbstractSellable, BaseSellableInfo
 from stoq.domain.interfaces import ISellable
 from stoq.domain.product import Product
 from stoq.gui.editors.product import ProductEditor
@@ -51,9 +54,13 @@ class ProductSearch(SearchEditor):
     search_table = Product.getAdapterClass(ISellable)
     editor_class = ProductEditor
     
-    def __init__(self, conn):
+    def __init__(self, conn, hide_footer=True, hide_toolbar=False,
+                 selection_mode=gtk.SELECTION_BROWSE):
         SearchEditor.__init__(self, conn, self.table, self.editor_class,
                               search_table=self.search_table, 
+                              hide_footer=hide_footer,
+                              hide_toolbar=hide_toolbar,
+                              selection_mode=selection_mode,
                               title=self.title)
         self.search_bar.set_result_strings(_('product'), _('products'))
         self.search_bar.set_searchbar_labels(_('matching'))
@@ -88,18 +95,26 @@ class ProductSearch(SearchEditor):
     def get_columns(self):
         return [Column('code', _('Code'), data_type=str, sorted=True, 
                        width=80),
-                Column('base_sellable_info.description', 
-                       _('Description'), data_type=str, 
-                       width=260),
+                ForeignKeyColumn(BaseSellableInfo, 'description', 
+                                 _('Description'), data_type=str, 
+                                 obj_field='base_sellable_info',
+                                 width=260),
                 AccessorColumn('suppliers', self.get_main_supplier_name,
                                title=_('Supplier'), data_type=str,
                                width=200),
                 Column('cost', _('Cost'), data_type=currency, width=80),
-                Column('base_sellable_info.price', _('Price'), 
-                       data_type=currency, width=80),
+                ForeignKeyColumn(BaseSellableInfo, 'price', 
+                                 _('Price'), data_type=currency, 
+                                 obj_field='base_sellable_info',
+                                 width=80),
                 Column('status_string', _('Status'), data_type=str)]
 
     def get_extra_query(self):
+        # FIXME Waiting for a SQLObject bug Fix. We can not create sqlbuilder
+        # queries for foreignkeys and inherited tables
+        # table = self.search_table
+        # q1 = AbstractSellable.q.id == table.q.id
+        # q2 = BaseSellableInfo.q.id == table.q.base_sellable_infoID
         status = self.filter_slave.get_selected_status()
         if status != ALL_ITEMS_INDEX:
             return AbstractSellable.q.status == status
---
+++ stoq/gui/wizards/receiving.py
@@ -0,0 +1,238 @@
+# -*- Mode: Python; coding: iso-8859-1 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2006 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+## USA.
+##
+## Author(s):   Evandro Vale Miquelito      <evandro@async.com.br>
+##
+"""
+stoq/gui/wizards/receiving.py:
+
+    Receiving wizard definition
+"""
+
+import datetime
+import gettext
+
+import gtk
+from kiwi.datatypes import currency
+from kiwi.ui.widgets.list import Column
+from stoqlib.gui.wizards import BaseWizardStep, BaseWizard
+from stoqlib.gui.search import SearchBar
+from stoqlib.gui.columns import ForeignKeyColumn
+from stoqlib.gui.dialogs import run_dialog
+from sqlobject.sqlbuilder import AND
+
+from stoq.lib.parameters import sysparam
+from stoq.lib.runtime import get_current_user
+from stoq.lib.validators import format_quantity
+from stoq.domain.person import Person
+from stoq.domain.purchase import PurchaseOrder
+from stoq.domain.receiving import (ReceivingOrder, ReceivingOrderItem,
+                                   get_receiving_items_by_purchase_order)
+from stoq.domain.interfaces import IUser, ISupplier
+from stoq.gui.search.product import ProductSearch
+from stoq.gui.wizards.abstract import AbstractProductStep
+
+_ = gettext.gettext
+
+
+#
+# Wizard Steps
+#
+
+
+class ReceivingOrderProductStep(AbstractProductStep):
+    model_type = ReceivingOrder
+    item_table = ReceivingOrderItem
+    summary_label_text = "<b>%s</b>" % _('Total Received:')
+
+    def __init__(self, wizard, previous, conn, model):
+        AbstractProductStep.__init__(self, wizard, previous, conn, model)
+        self.add_product_button.hide()
+
+    def _get_columns(self):
+        return [Column('sellable.base_sellable_info.description', 
+                       title=_('Description'), 
+                       data_type=str, expand=True, searchable=True),
+                Column('quantity_received', title=_('Quantity'), 
+                       data_type=float, width=90, 
+                       format_func=format_quantity, editable=True),
+                Column('sellable.unit_description', title=_('Unit'),
+                        data_type=str, width=50),
+                Column('cost', title=_('Cost'), data_type=currency, 
+                       editable=True, width=90),
+                Column('total', title=_('Total'), data_type=currency,
+                       width=100)]
+
+    def _get_order_item(self, sellable, cost, quantity):
+        return ReceivingOrderItem(connection=self.conn, sellable=sellable,
+                                  receiving_order=self.model, 
+                                  cost=cost, quantity_received=quantity)
+
+    def _get_saved_items(self):
+        if not self.model.purchase:
+            return []
+        return get_receiving_items_by_purchase_order(self.model.purchase,
+                                                     self.model)
+    #
+    # WizardStep hooks
+    #
+
+    def next_step(self):
+        return
+
+    #
+    # callbacks
+    #
+
+    def on_product_button__clicked(self, *args):
+        # We are going to call a SearchEditor subclass which means
+        # database synchronization... Outch, time to commit !
+        self.conn.commit()
+        products = run_dialog(ProductSearch, self, self.conn,
+                              hide_footer=False, hide_toolbar=True,
+                              selection_mode=gtk.SELECTION_MULTIPLE)
+        for product in products:
+            self._update_list(product)
+
+
+class PurchaseSelectionStep(BaseWizardStep):
+    gladefile = 'PurchaseSelectionStep'
+    model_type = ReceivingOrder
+
+    def __init__(self, wizard, conn, model):
+        BaseWizardStep.__init__(self, conn, wizard, model)
+        self._update_view()
+
+    def _refresh_next(self, validation_value):
+        if sysparam(self.conn).RECEIVE_PRODUCTS_WITHOUT_ORDER:
+            validation_value = True
+        else:
+            validation_value = len(self.orders) == 1
+        self.wizard.refresh_next(validation_value)
+
+    def _get_columns(self):
+        return [Column('order_number', title=_('Number'), sorted=True,
+                       data_type=int, width=100, format='%03d'),
+                Column('open_date', title=_('Date Started'),
+                       data_type=datetime.date, justify=gtk.JUSTIFY_RIGHT),
+                ForeignKeyColumn(Person, 'name', title=_('Supplier'), 
+                                 data_type=str,
+                                 obj_field='supplier', adapted=True,
+                                 searchable=True, width=270),
+                Column('purchase_total', title=_('Ordered'), 
+                       data_type=currency, width=120),
+                Column('received_total', title=_('Received'), 
+                       data_type=currency)]
+
+    def _get_extra_query(self):
+        supplier_table = Person.getAdapterClass(ISupplier)
+        q1 = PurchaseOrder.q.supplierID == supplier_table.q.id
+        q2 = supplier_table.q._originalID == Person.q.id
+        q3 = PurchaseOrder.q.status == PurchaseOrder.ORDER_CONFIRMED
+        return AND(q1, q2, q3)
+
+    def on_searchbar_before_activate(self, *args):
+        self.conn.commit()
+
+    def on_searchbar_activate(self, slave, objs):
+        """Use this callback with SearchBar search-activate signal"""
+        self.orders.add_list(objs, clear=True)
+
+    def _update_view(self):
+        has_selection = self.orders.get_selected() is not None
+        self.details_button.set_sensitive(has_selection)
+            
+    #
+    # WizardStep hooks
+    #
+
+    def post_init(self):
+        self.register_validate_function(self._refresh_next)
+        self.force_validation()
+
+    def next_step(self):
+        self.model.purchase = self.orders.get_selected()
+        return ReceivingOrderProductStep(self.wizard, self, self.conn, 
+                                         self.model)
+              
+    def has_previous_step(self):
+        return False
+
+    def setup_slaves(self):
+        self.order_label.set_size('large')
+        self.order_label.set_bold(True)
+        self.orders.set_columns(self._get_columns())
+        self.searchbar = SearchBar(self.conn, PurchaseOrder, 
+                                   self._get_columns(), 
+                                   searching_by_date=True)
+        self.searchbar.register_extra_query(self._get_extra_query())
+        self.searchbar.set_result_strings(_('order'), _('orders'))
+        self.searchbar.set_searchbar_labels(_('Orders Maching:'))
+        self.searchbar.connect('before-search-activate', 
+                               self.on_searchbar_before_activate)
+        self.searchbar.connect('search-activate', self.on_searchbar_activate)
+        self.attach_slave('searchbar_holder', self.searchbar)
+        self.searchbar.set_focus()
+
+    #
+    # Kiwi callbacks
+    #
+
+    def on_orders__selection_changed(self, *args):
+        self.force_validation()
+        self._update_view()
+
+    def on_details_button__clicked(self, *args):
+        pass
+
+#
+# Main wizard
+#
+
+class ReceivingOrderWizard(BaseWizard):
+    title = "Receiving Order"
+    size = (750, 500)
+    
+    def __init__(self, conn):
+        model = self._create_model(conn)
+        first_step = PurchaseSelectionStep(self, conn, model)
+        BaseWizard.__init__(self, conn, first_step, model)
+
+    def _create_model(self, conn):
+        current_user = get_current_user()
+        table = Person.getAdapterClass(IUser)
+        current_user = table.get(current_user.id, connection=conn)
+        branch = sysparam(conn).CURRENT_BRANCH
+        return ReceivingOrder(responsible=current_user, supplier=None,
+                              branch=branch, connection=conn)
+
+    #
+    # WizardStep hooks
+    #
+
+    def finish(self):
+        if not self.model.get_valid():
+            self.model.set_valid()
+        self.retval = self.model
+        # TODO waiting for support in domain classes
+        self.model.confirm()
+        self.close()
