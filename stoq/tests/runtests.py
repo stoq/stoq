c37e3a7742bb9976055f973d3f228119140b8b9c
jdahlin <None>
2006-01-24 15:41:46 +0000

    add a post_hook for unittests and add some database integration tests for the saleswizard

--- stoq/tests/runtests.py
+++ stoq/tests/runtests.py
@@ -30,33 +30,38 @@ stoq/tests/domain/runtests.py:
    Run all the domain test suite
 """
 
+import doctest
+import optparse
 import os
 import sys
-import doctest
+
+import gobject
+gobject.threads_init()
+from kiwi import environ
 import py
 
-from kiwi import environ
-from stoq.lib.runtime import print_immediately, set_test_mode, set_verbose
-# This must be called *before* anything else since it will switch to test
-# database and also change the runtime module behaviour
-set_test_mode(True)
-
-from stoq.examples.createall import create
-from stoq.lib.admin import initialize_system, setup_tables
-
-VERBOSE = '-v' in sys.argv
+from stoq.lib.runtime import (print_immediately, get_connection,
+                              set_test_mode, set_verbose)
 
 DEFAULT_SEPARATORS = 79
 
-def setup():
+def setup(options):
+    # This must be called *before* anything else since it will switch to test
+    # database and also change the runtime module behaviour
+    set_test_mode(True)
+    from stoq.examples.createall import create
+    from stoq.lib.admin import initialize_system, setup_tables
+
     set_verbose(False)
     setup_tables(delete_only=True, list_tables=True, verbose=True)
-    set_verbose(VERBOSE)
+    set_verbose(options.verbose)
     initialize_system("Superuser", "administrator", "", verbose=True)
     create()
 
-def test_gui():
-    if VERBOSE:
+def test_gui(options, tests):
+    from stoq.lib.runtime import new_transaction
+
+    if options.verbose:
         print_immediately('Performing gui module tests... ')
 
     root = os.path.abspath(os.path.join(sys.argv[0], '..', '..', '..'))
@@ -64,47 +69,90 @@ def test_gui():
     os.chdir(root)
 
     # Running kiwi-ui tests
-    gui_tests_dir = os.path.abspath(os.path.join(root, 'stoq',
-                                                 'tests', 'gui'))
-    gui_tests = [filename
-                 for filename in os.listdir(gui_tests_dir)
+    if not tests:
+        test_dir = os.path.abspath(os.path.join(root, 'stoq', 'tests', 'gui'))
+        tests = [os.path.join(test_dir, filename)
+                 for filename in os.listdir(test_dir)
                      if filename.endswith('.py') and filename[0] != '_']
-    for test_file in gui_tests:
-        if VERBOSE:
-            print 'RUNNING', test_file
+    else:
+        tests = [os.path.join(oldpwd, test) for test in tests]
+
+    conn = get_connection()
+
+    for test_name in tests:
+        if options.verbose:
+            print 'RUNNING', os.path.basename(test_name)
             print '=' * DEFAULT_SEPARATORS
+        globs = {}
         environ.app = None
-        execfile(os.path.join(gui_tests_dir, test_file))
-        if VERBOSE:
+
+        execfile(test_name, globs)
+
+        post_hook = globs.get('post_hook')
+        if post_hook:
+            post_hook(new_transaction())
+
+        if options.verbose:
             print '=' * DEFAULT_SEPARATORS
     os.chdir(oldpwd)
 
-    if VERBOSE:
+    if options.verbose:
         print_immediately('gui tests ok')
 
-def test_domain():
-    if VERBOSE:
-        print_immediately('Performing domain tests... ')
+def test_domain(options):
+    if options.verbose:
+        print_immediately('Running domain doctests... ')
 
     # Running doctests
     domain_tests_dir = os.path.abspath(os.path.join(
-             os.path.dirname(sys.argv[0]), '..', '..', 'docs', 'domain'))
+        os.path.dirname(sys.argv[0]), '..', '..', 'docs', 'domain'))
     doc_files = [filename for filename in os.listdir(domain_tests_dir)
                               if filename.endswith('.txt')]
 
     for doc_file in doc_files:
         doc_file = os.path.join(domain_tests_dir, doc_file)
-        doctest.testfile(doc_file, verbose=VERBOSE, module_relative=False)
+        doctest.testfile(doc_file,
+                         verbose=options.verbose,
+                         module_relative=False)
 
-    if '-v' in sys.argv:
-        sys.argv.remove('-v')
+    if options.verbose:
+        print_immediately('Running domain unittests... ')
+
+    sys.argv = sys.argv[:1]
     py.test.cmdline.main()
 
-    if VERBOSE:
-        print_immediately('domain tests ok')
+    if options.verbose:
+        print_immediately('Domain tests are ok')
 
-setup()
+def main(args):
+    parser = optparse.OptionParser()
+    parser.add_option('-v', '--verbose',
+                      action="store_true",
+                      dest="verbose")
+    parser.add_option('-g', '--gui',
+                      action="store_true",
+                      dest="only_gui",
+                      help='Only run gui tests')
+    parser.add_option('-d', '--domain',
+                      action="store_true",
+                      dest="only_domain",
+                      help='Only run domain tests')
 
-test_domain()
-test_gui()
+    if '--g-fatal-warnings' in args:
+        args.remove('--g-fatal-warnings')
+    options, args = parser.parse_args(args)
+    setup(options)
 
+    if options.only_domain:
+        test_domain(options)
+        return
+
+    if options.only_gui:
+        test_gui(options, args[1:])
+        return
+
+    test_domain(options)
+    test_gui(options)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[:]))
