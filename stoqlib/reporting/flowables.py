66060be066521b3705318548148a7abda8d75a6e
henrique <None>
2006-02-02 14:42:49 +0000

    Updating module headers

--- stoqlib/reporting/flowables.py
+++ stoqlib/reporting/flowables.py
@@ -1,32 +1,36 @@
 # -*- Mode: Python; coding: iso-8859-1 -*-
 # vi:si:et:sw=4:sts=4:ts=4
-#
-# Copyright (C) 2005 Async Open Source
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
 
-""" 
-Este módulo disponibiliza novos elementos (flowables) de forma à permitir
-extender a funcionalidade do ReportLab. Os elementos referidos são
-PageNumberChanger, ReportLine e Signature.
+##
+## Copyright (C) 2006 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+## USA.
+##
+"""
+stoqlib/reporting/flowables.py:
 
-O módulo também importa todas as constantes de estilos do módulo
-"default_style" e fornece as seguintes constantes para alinhamento: LEFT, 
-CENTER e RIGHT, alinhamento à esquerda, ao centro e à direita,
-respectivamente.
+    Este módulo disponibiliza novos elementos (flowables) de forma à
+    permitir extender a funcionalidade do ReportLab. Os elementos referidos
+    são PageNumberChanger, ReportLine e Signature.
+
+    O módulo também importa todas as constantes de estilos do módulo
+    default_style e fornece as seguintes constantes para alinhamento: LEFT,
+    CENTER e RIGHT, alinhamento à esquerda, ao centro e à direita,
+    respectivamente.
 """
 
 from reportlab.lib.units import mm
