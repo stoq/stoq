66ee05d2711563610cb34c302c70c5dfdeba692c
evandro <None>
2006-02-07 22:46:56 +0000

    Updating module header

--- stoq/lib/applist.py
+++ stoq/lib/applist.py
@@ -20,11 +20,10 @@
 ## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 ## USA.
 ##
-"""
-stoq/lib/applist.py:
-
-    Listing and importing applications
-"""
+##  Author(s):  Evandro Vale Miquelito  <evandro@async.com.br>
+##
+##
+""" Listing and importing applications """
 
 import glob
 import os
