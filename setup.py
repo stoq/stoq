708831196a26fa66d97202e38df5c5e86f332a85
Evandro Vale Miquelito <evandro@async.com.br>
2006-03-08 22:18:10 +0000

    Adding a dependencies check and updating setup informations

--- setup.py
+++ setup.py
@@ -3,6 +3,30 @@
 # Setup file for StoqDrivers
 # Code by Async Open Source <http://www.async.com.br>
 
+
+#
+# Dependencies check
+#
+
+
+dependencies = [('zope.interface', '3.0', None),
+                ('kiwi', (1, 9, 6), lambda x: x.__version__),
+                ('serial', '1.3', None)]
+for package_name, version, attr in dependencies:
+    try:
+        module = __import__(package_name, {}, {}, [])
+        if attr:
+            assert attr(module) >= version
+    except ImportError, AssertionError:
+        raise SystemExit("Stoqdrivers requires %s %s or higher"
+                         % (package_name, version))
+
+
+#
+# Package installation
+#
+
+
 from distutils.core import setup
 
 from kiwi.dist import listpackages, listfiles, KiwiInstallData, KiwiInstallLib
@@ -17,11 +41,16 @@ assert version
 setup(
     name = "stoqdrivers",
     version =  ".".join(map(str, version)),
+    author="Async Open Source",
+    author_email="stoq-devel@async.com.br",
     description = "Useful drivers for Stoq and retail systems",
-    long_description = """ """,
-    author = "Async Open Source",
-    author_email = "kiko@async.com.br",
-    url = "http://www.async.com.br/projects/",
+    long_description="""
+    This is a powerful collection of device drivers written in Python and
+    totally focused on retail systems. Stoqdrivers also offers an
+    unified API for devices like fiscal printers which makes it easy to
+    embed in many applications.
+    """,
+    url = "http://www.stoq.com.br",
     license = "GNU LGPL 2.1 (see COPYING)",
     packages = listpackages('stoqdrivers'),
     data_files = [("$datadir/conf",
