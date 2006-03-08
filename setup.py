7d46d9fa04b3b8cd8febb7200157f14ca729b5e2
evandro <None>
2006-03-08 15:11:20 +0000

    Adding a header for setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,37 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2005, 2006 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+## USA.
+##
+##  Author(s):  Evandro Vale Miquelito      <evandro@async.com.br>
+##              Johan Dahlin                <jdahlin@async.com.br>
+##
+##
+"""Setup file for Stoq package"""
+
+
 from distutils.core import setup
 from distutils.command.install_data import install_data
 
-from kiwi.dist import (KiwiInstallData, KiwiInstallLib, compile_po_files, 
-     listfiles, listpackages)
+from kiwi.dist import (KiwiInstallData, KiwiInstallLib, compile_po_files,
+                       listfiles, listpackages)
 
 from stoq import __version__
 
