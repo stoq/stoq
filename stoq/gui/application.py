675b68b6e065ce6c34355cd31a0fd758f0a7333b
evandro <None>
2006-03-09 02:00:40 +0000

    fixing repression

--- stoq/gui/application.py
+++ stoq/gui/application.py
@@ -130,10 +130,10 @@ class AppWindow(BaseAppWindow):
 
     def _run_about(self, *args):
         about = gtk.AboutDialog()
-        about.set_name(stoq.__program_name__)
-        about.set_version(stoq.__version__)
-        about.set_website(stoq.__website__)
-        release_date = stoq.__release_date__
+        about.set_name(stoq.program_name)
+        about.set_version(stoq.version)
+        about.set_website(stoq.website)
+        release_date = stoq.release_date
         about.set_comments('Release Date: %s' %
                            datetime.datetime(*release_date).strftime('%x'))
         about.set_copyright('Copyright (C) 2005 Async Open Source')
