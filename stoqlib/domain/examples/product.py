572389cdda84898a428b00917016bf9368ce96af
evandro <None>
2006-03-16 14:50:24 +0000

    Updating licences since the FSF address has been changed

--- stoqlib/domain/examples/product.py
+++ stoqlib/domain/examples/product.py
@@ -18,8 +18,7 @@
 ##
 ## You should have received a copy of the GNU Lesser General Public License
 ## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-## USA.
+## Foundation, Inc., or visit: http://www.gnu.org/.
 ##
 """ Create product objects for an example database.
 
