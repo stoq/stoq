93604ffd0679426890623dffe79a4ece50206922
evandro <None>
2006-03-16 14:50:24 +0000

    Updating licences since the FSF address has been changed

--- stoqlib/gui/slaves/client.py
+++ stoqlib/gui/slaves/client.py
@@ -17,8 +17,7 @@
 ##
 ## You should have received a copy of the GNU Lesser General Public License
 ## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-## USA.
+## Foundation, Inc., or visit: http://www.gnu.org/.
 ##
 ##  Author(s):  Henrique Romano             <henrique@async.com.br>
 ##              Evandro Vale Miquelito      <evandro@async.com.br>
