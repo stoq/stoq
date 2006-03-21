6a689e43ecf8d8941eee3842f44a0e101ecb112a
Johan Dahlin <jdahlin@async.com.br>
2006-03-21 14:20:01 +0000

    locale resource not needed with latest kiwi

--- stoqdrivers/__init__.py
+++ stoqdrivers/__init__.py
@@ -6,10 +6,5 @@ __all__ = ["library"]
 
 library = Library("stoqdrivers", root="..")
 if library.uninstalled:
-    # XXX: Move this to enable_translation()
-    try:
-        library.add_resources(locale='locale')
-    except EnvironmentError:
-        pass
     library.add_global_resource("conf", "stoqdrivers/conf")
 library.enable_translation()
