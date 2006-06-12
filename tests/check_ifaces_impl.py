19f239260e6d637ab7b31bb0539089d938874abf
Henrique Romano <henrique@async.com.br>
2006-06-12 19:17:28 +0000

    Improve implementation

--- tests/check_ifaces_impl.py
+++ tests/check_ifaces_impl.py
@@ -35,36 +35,35 @@ from stoqdrivers.devices.interfaces import (ICouponPrinter,
 from stoqdrivers.devices.printers.base import get_supported_printers_by_iface
 from stoqdrivers.devices.scales.base import get_supported_scales
 
-def check_printers():
-    for iface in (ICouponPrinter, IChequePrinter):
-        printers = get_supported_printers_by_iface(iface)
-        print "\n\nChecking drivers that implements %s..." % iface.__name__
-        for brand in printers:
-            print "\n\tChecking %s printers:" % brand
-            for model in printers[brand]:
-                print "\t\t%s" % model.model_name,
-                try:
-                    verifyClass(iface, model)
-                except Invalid, e:
-                    print "ERROR:", e
-                else:
-                    print "OK"
+def _check_drivers(iface, brand, drivers):
+    print "\t- Checking %s devices:" % brand
+    for driver in drivers:
+        print "\t\t- %s\n" % driver.model_name,
+        try:
+            verifyClass(iface, driver)
+        except Invalid, e:
+            print "ERROR: ", e
+
+def _check_printers(iface):
+    printers_dict = get_supported_printers_by_iface(iface)
+    for brand, drivers in printers_dict.items():
+        _check_drivers(iface, brand, drivers)
+
+def check_coupon_printers():
+    print "Checking Coupon Printers..."
+    _check_printers(ICouponPrinter)
+
+def check_cheque_printers():
+    print "Checking Cheque Printers..."
+    _check_printers(IChequePrinter)
 
 def check_scales():
-    print "\n\nChecking Scales..."
-    scales = get_supported_scales()
+    print "Checking Scales..."
     for brand, drivers in get_supported_scales().items():
-        print "\n\tChecking %s scales: " % brand
-        for driver in drivers:
-            print "\t\t%s" % driver.model_name,
-            try:
-                verifyClass(IScale, driver)
-            except Invalid, e:
-                print "ERROR:", e
-            else:
-                print "OK"
+        _check_drivers(IScale, brand, drivers)
 
 if __name__ == "__main__":
-    check_printers()
+    check_coupon_printers()
+    check_cheque_printers()
     check_scales()
 
