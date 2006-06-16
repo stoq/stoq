d0ef8dbcccfc5f4bcc6fa2ae558043900676c93a
Henrique Romano <henrique@async.com.br>
2006-06-16 14:32:05 +0000

    whitespace

--- tests/test_log.py
+++ tests/test_log.py
@@ -33,58 +33,58 @@ class LogTest(unittest.TestCase):
     def setUp(self):
         try:self.log = Log(sys.argv[1])
         except:self.log = Log()
-        
+
     def test_accessible_file(self):
         """Simple test that writes log to an accessible file"""
         print "\n>>> Writing log to accessible file:"
-        
+
         log = Log('/tmp/fiscal_test.log')
         log.log("Testing Log with an accessible file")
         print ">>> Written log:", open('/tmp/fiscal_test.log').readlines()[-1]
-        
+
     def test_stdout(self):
         """Test if Log writes to standard output correctly"""
         print "\n>>> Writing log to standard output:"
-                
+
         log = Log(sys.stdout)
         log.log("Testing Log going to standard output")
-        
+
     def test_stderr(self):
         """Test if Log write to standard error output correctly"""
         print "\n>>> Writing to standard error output:"
-        
+
         log = Log(sys.stderr)
         log.log("Testing Log going to standard error output")
-        
+
     def test_file_without_perm(self):
         """Test Log behavior when user send file without write perms"""
         print "\n>>> Trying to write into file withou write perms:"
-                
+
         log = Log('/etc/shadow')
         log.log("Testing Log with a file without write permissions")
-        
+
     def test_file_read_mode(self):
         """Test Log behavior when user send an file object withou write mode"""
         print "\n>>> Trying to write into file object in read-only mode"
-        
+
         log = Log(open('/tmp/fiscal_test.log'))
         log.log("Testing Log with file object withou write mode")
 
     def test_debug(self):
         self.log.debug("Debug Message!")
-        
+
     def test_info(self):
         self.log.info("Info Message!")
-        
+
     def test_warning(self):
         self.log.warning("Warning Message!")
-        
+
     def test_error(self):
         self.log.error("Error Message!")
-        
+
     def test_critical(self):
         self.log.critical("Critical Message!")
-        
+
     def test_log(self):
         self.log.log("Default Log Message!")
 
