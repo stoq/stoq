f6743d5356663ceac1c28f60681ac77cd4172962
Henrique Romano <henrique@async.com.br>
2006-06-19 17:24:43 +0000

    Add correct payment value, considering surcharge specified to totalize().

--- tests/test_coupon.py
+++ tests/test_coupon.py
@@ -147,7 +147,7 @@ class TestCoupon(BaseTest):
         # 3. surcharge with taxcode equals to TAX_ICMS
         # (daruma FS345 specific)
         self._device.totalize(surcharge=Decimal("1"), taxcode=TAX_ICMS)
-        self._device.add_payment(MONEY_PM, Decimal("10"))
+        self._device.add_payment(MONEY_PM, Decimal("12"))
         self._device.close()
 
         # 4. surcharge with taxcode different of TAX_ICMS
