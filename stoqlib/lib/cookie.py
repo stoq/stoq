0cdaeca8411f0ed204f8921d279d3a2eee2875ca
jdahlin <None>
2006-07-26 20:08:16 +0000

    Stoqlib part of #2708

---
+++ stoqlib/lib/cookie.py
@@ -0,0 +1,78 @@
+# -*- Mode: Python; coding: iso-8859-1 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2006 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s):   Johan Dahlin   <jdahlin@async.com.br>
+##
+
+""" Simple Base64 ICookieFile implementation """
+
+import binascii
+import os
+
+from kiwi.component import implements
+from kiwi.log import Logger
+
+from stoqlib.lib.interfaces import CookieError, ICookieFile
+
+log = Logger("cookie")
+
+class Base64CookieFile(object):
+
+    implements(ICookieFile)
+
+    def __init__(self, filename):
+        self._filename = filename
+
+    def get(self):
+        if not os.path.exists(self._filename):
+            raise CookieError("%s does not exist" % self._filename)
+
+        cookiedata = open(self._filename).read()
+        if not ':' in cookiedata:
+            log.info("invalid cookie file")
+            raise CookieError("%s is invalid" % self._filename)
+
+        data = cookiedata.split(":", 1)
+        try:
+            return data[0], binascii.a2b_base64(data[1])
+        except binascii.Error:
+            raise CookieError("invalid format")
+
+    def clear(self):
+        try:
+            os.remove(self._filename)
+            log.info("Removed cookie %s" % self._filename)
+        except OSError, e:
+            log.info("Could not remove file %s: %r" % (self._filename, e))
+
+    def store(self, username, password):
+        try:
+            fd = open(self._filename, "w")
+        except IOError, e:
+            raise CookieError("Could not remove file %s: %r" % (
+                self._filename, e))
+
+        # obfuscate password to avoid it being easily identified when
+        # editing file on screen. this is *NOT* encryption!
+        fd.write("%s:%s" % (username, binascii.b2a_base64(password)))
+        fd.close()
+
+        log.info("Saved cookie %s" % self._filename)
---
+++ tests/test_cookiefile.py
@@ -0,0 +1,45 @@
+import os
+import unittest
+
+from stoqlib.lib.interfaces import CookieError
+from stoqlib.lib.cookie import SimpleCookieFile
+
+class CookieTest(unittest.TestCase):
+    def setUp(self):
+        self.cookie = SimpleCookieFile('test.cookie')
+
+    def _write_cookie(self, data):
+        print >> open('test.cookie', "w"), data
+
+    def testGet(self):
+        self.assertRaises(CookieError, self.cookie.get)
+
+        self._write_cookie('abc')
+        self.assertRaises(CookieError, self.cookie.get)
+
+        self._write_cookie('abc:J')
+        self.assertRaises(CookieError, self.cookie.get)
+
+        self._write_cookie('abc:')
+        self.assertEqual(self.cookie.get(), ('abc', ''))
+
+        self._write_cookie('abc:YWJj')
+        self.assertEqual(self.cookie.get(), ('abc', 'abc'))
+
+        os.remove('test.cookie')
+
+    def testClear(self):
+        self.cookie.clear()
+        self._write_cookie('abc')
+        self.cookie.clear()
+
+        self.failIf(os.path.exists("test.cookie"))
+
+    def testStore(self):
+        self._write_cookie('abc')
+        os.chmod('test.cookie', 0)
+        self.assertRaises(CookieError, self.cookie.store,'', '')
+        self.cookie.clear()
+        self.cookie.store('abc', 'abc')
+        self.assertEqual(open('test.cookie').read(), 'abc:YWJj\n')
+        os.remove('test.cookie')
