428a421ab78d36ccc25de31df861c27f8d881343
jdahlin <None>
2006-08-23 16:13:30 +0000

    Loggify

--- stoqlib/lib/admin.py
+++ stoqlib/lib/admin.py
@@ -31,12 +31,13 @@ from kiwi.argcheck import argcheck
 from kiwi.component import get_utility, provide_utility
 from kiwi.datatypes import currency
 from kiwi.environ import environ
+from kiwi.log import Logger
 
 from stoqdrivers.constants import UNIT_WEIGHT, UNIT_LITERS, UNIT_METERS
 
 from stoqlib.database import setup_tables, finish_transaction, run_sql_file
 from stoqlib.lib.interfaces import ICurrentUser, IDatabaseSettings
-from stoqlib.lib.runtime import new_transaction, print_msg
+from stoqlib.lib.runtime import new_transaction
 from stoqlib.lib.parameters import sysparam, ensure_system_parameters
 from stoqlib.lib.translation import stoqlib_gettext
 from stoqlib.domain.interfaces import (IIndividual, IEmployee, IUser,
@@ -44,13 +45,14 @@ from stoqlib.domain.interfaces import (IIndividual, IEmployee, IUser,
 
 _ = stoqlib_gettext
 
+log = Logger('stoqlib.admin')
 USER_ADMIN_DEFAULT_NAME = _('administrator')
 
 def ensure_admin_user(administrator_password):
     from stoqlib.domain.person import EmployeeRole, PersonAdaptToUser
     from stoqlib.domain.profile import UserProfile
     from stoqlib.domain.person import EmployeeRoleHistory
-    print_msg("Creating administrator user...", break_line=False)
+    log.info("Creating administrator user")
     conn = new_transaction()
 
     # XXX Person for administrator user is the same of Current Branch. I'm not
@@ -74,10 +76,13 @@ def ensure_admin_user(administrator_password):
     # must have all the facets.
     person_obj.addFacet(ISalesPerson, connection=conn)
 
+    log.info("Creating user profile for administrator")
     profile = UserProfile.create_profile_template(conn, _('Administrator'),
                                                   has_full_permission=True)
 
+
     username = USER_ADMIN_DEFAULT_NAME
+    log.info("Attaching IUser facet (%s)" % (username,))
     user = person_obj.addFacet(IUser, username=username,
                                password=administrator_password,
                                profile=profile, connection=conn)
@@ -88,14 +93,15 @@ def ensure_admin_user(administrator_password):
     assert ret[0].password == administrator_password
 
     finish_transaction(conn, 1)
+
     # We can't provide the utility until it's actually in the database
+    log.info('providing utility ICurrentUser')
     provide_utility(ICurrentUser, user)
-    print_msg('done')
 
 def ensure_sellable_units():
     from stoqlib.domain.sellable import SellableUnit
     """ Create native sellable units. """
-    print_msg("Creating sellable units... ", break_line=False)
+    log.info("Creating sellable units")
     conn = new_transaction()
     unit_list = [("Kg", UNIT_WEIGHT),
                  ("Lt", UNIT_LITERS),
@@ -103,7 +109,6 @@ def ensure_sellable_units():
     for desc, index in unit_list:
         SellableUnit(description=desc, index=index, connection=conn)
     finish_transaction(conn, 1)
-    print_msg("done")
 
 def create_base_schema():
     filename = '%s-schema.sql' % get_utility(IDatabaseSettings).rdbms
