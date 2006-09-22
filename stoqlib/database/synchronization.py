1363297b3eda2e9a5fd35fdad914fbfa30fa9063
jdahlin <None>
2006-09-22 14:38:24 +0000

    Add basic update() support, improve error handling and refactor some common parts

--- stoqlib/database/synchronization.py
+++ stoqlib/database/synchronization.py
@@ -29,18 +29,19 @@ import subprocess
 from kiwi.component import get_utility
 from kiwi.log import Logger
 
-from stoqlib.database.admin import create_base_schema, ensure_sellable_units
+from stoqlib.database.admin import create_base_schema
 from stoqlib.database.database import db_table_name
 from stoqlib.database.policy import get_policy_by_name
 from stoqlib.database.runtime import new_transaction
 from stoqlib.database.tables import create_tables, get_table_type_by_name
 from stoqlib.domain.station import BranchStation
 from stoqlib.domain.synchronization import BranchSynchronization
-from stoqlib.domain.system import SystemTable
+from stoqlib.domain.transaction import TransactionEntry
 from stoqlib.enums import SyncPolicy
 from stoqlib.lib.component import AdaptableSQLObject
 from stoqlib.lib.interfaces import IDatabaseSettings
 from stoqlib.lib.xmlrpc import ServerProxy, XMLRPCService
+from sqlobject.sqlbuilder import AND
 
 log = Logger('stoqlib.synchronization')
 
@@ -51,24 +52,28 @@ class SynchronizationService(XMLRPCService):
         XMLRPCService.__init__(self, hostname, port)
         self._processes = {}
 
-    def _add_remaning_schema(self):
-        log.info('adding remaining schema')
-        trans = new_transaction()
-        ensure_sellable_units()
-
-        SystemTable.update(trans, check_new_db=True)
-        trans.commit()
-
     #
     # Protocol / Public Methods
     #
 
     def clean(self):
+        """
+        Cleans the database
+        """
         log.info('service.clean()')
         create_tables()
         create_base_schema()
 
+    def get_station_name(self):
+        """
+        @returns: the name of the station
+        """
+        return socket.gethostname()
+
     def sql_prepare(self):
+        """
+        @returns: an integer representing the insert
+        """
         log.info('service.sql_prepare()')
         settings = get_utility(IDatabaseSettings)
 
@@ -82,43 +87,54 @@ class SynchronizationService(XMLRPCService):
         log.info('sql_prepare: executing %s' % cmd)
         proc = subprocess.Popen(cmd, shell=True,
                                 stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE)
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
 
         self._processes[proc.pid] = proc
         log.info('sql_prepare: return %d' % (proc.pid, ))
+
         return proc.pid
 
-    def sql_insert(self, pid, dump):
-        log.info('service.sql_insert(%d, %d bytes)' % (pid, len(dump)))
-        proc = self._processes[pid]
+    def sql_insert(self, sid, data):
+        """
+        @param sid:
+        @param data:
+        """
+        log.info('service.sql_insert(%d, %d bytes)' % (sid, len(data)))
+        proc = self._processes[sid]
 
-        proc.stdin.write(dump.encode('utf-8'))
+        # XMLRPC sends us unicode, postgres expects utf-8
+        proc.stdin.write(data.encode('utf-8'))
 
-    def sql_finish(self, pid):
-        log.info('service.sql_finish(%d)' % (pid,))
+    def sql_finish(self, sid):
+        """
+        @param sid:
+        @returns: None if successful, otherwise a string
+        """
+        log.info('service.sql_finish(%d)' % (sid,))
 
-        proc = self._processes.pop(pid)
+        proc = self._processes.pop(sid)
 
-        log.info('sql_finish: closing stdin for pg_dump process %d' % (pid,))
+        log.info('sql_finish: closing stdin for pg_dump process %d' % (sid,))
         proc.stdin.close()
-
         returncode = proc.wait()
         log.info('sql_finish: psql process returned %d' % (returncode,))
 
-        self._add_remaning_schema()
+        if returncode == 3:
+            return proc.stderr.read()[:-1]
 
-    def get_station_name(self):
-        return socket.gethostname()
+        return None
 
     # Twisted
     xmlrpc_clean = clean
+    xmlrpc_get_station_name = get_station_name
     xmlrpc_sql_prepare = sql_prepare
     xmlrpc_sql_insert = sql_insert
     xmlrpc_sql_finish = sql_finish
-    xmlrpc_get_station_name = get_station_name
 
 class SynchronizationClient(object):
     def __init__(self, hostname, port):
+        self._commit = True
         log.info('Connecting to %s:%d' % (hostname, port))
         self.proxy = ServerProxy("http://%s:%d" % (hostname, port))
 
@@ -193,22 +209,68 @@ class SynchronizationClient(object):
        ##        foreign_table = findClass(column.foreignKey)
        ##        self._collect_table(tables, foreign_table)
 
-        log.info('Adding table %s' % table)
         tables.append(table)
 
         if issubclass(table, AdaptableSQLObject):
             for facet_type in table.getFacetTypes():
                 self._collect_table(tables, facet_type)
 
-    def _get_tables(self, policy):
+    def _get_tables(self, policy, filter):
         tables = []
         for table_name, table_policy in policy.tables:
-            if table_policy == SyncPolicy.FROM_TARGET:
+            if table_policy in filter:
                 continue
             table = get_table_type_by_name(table_name)
             self._collect_table(tables, table)
 
         return tables
+
+    def _serialize_update(self, trans, obj):
+        values = []
+        for column in obj.sqlmeta.columnList:
+            value = getattr(obj, column.name)
+            values.append("%s = %s" % (column.dbName, trans.sqlrepr(value)))
+
+        return ("UPDATE %s SET %s WHERE %s = %s;" %
+                (obj.sqlmeta.table,
+                 ", ".join(values),
+                 obj.sqlmeta.idName,
+                 trans.sqlrepr(obj.id)))
+
+    def _serialize_updates(self, trans, results):
+        data = ""
+        for so in results:
+            te = TransactionEntry.get(so.te_modifiedID, connection=trans)
+            data += self._serialize_update(trans, te)
+            data += self._serialize_update(trans, so)
+
+        return data
+
+    def _serialize_insert(self, trans, obj):
+        names = [obj.sqlmeta.idName]
+        values = [str(obj.id)]
+        for column in obj.sqlmeta.columnList:
+            value = getattr(obj, column.name)
+            names.append(column.dbName)
+            values.append(trans.sqlrepr(value))
+
+        return ("INSERT INTO %s (%s) VALUES (%s);" %
+                (obj.sqlmeta.table,
+                 ", ".join(names),
+                 ", ".join(values)))
+
+    def _serialize_inserts(self, trans, results):
+        data = ""
+        for so in results:
+            te = TransactionEntry.get(so.te_createdID, connection=trans)
+            data += self._serialize_insert(trans, te)
+            data += self._serialize_insert(trans, so)
+
+        return data
+
+    def _sql_insert(self, sid, data):
+        if self._commit:
+            self.proxy.sql_insert(sid, data)
 
     # Public API
 
@@ -218,7 +280,71 @@ class SynchronizationClient(object):
     def get_station_name(self):
         return self.proxy.get_station_name()
 
+    def update(self, station_name):
+        """
+        @param station_name:
+        """
+        policy = self._get_policy('shop')
+
+        trans = new_transaction()
+        station = self._get_station(trans, station_name)
+        sync = self._get_synchronization(trans, station.branch)
+        if not sync:
+            raise SystemExit(
+                "Tried to synchronize against for station %s which does "
+                "not have an entry in the BranchSynchronization table")
+
+
+        sid = self.proxy.sql_prepare()
+
+        timestamp = datetime.datetime.now()
+
+        data = ''
+
+        for table in self._get_tables(policy,
+                                      filter=(SyncPolicy.FROM_TARGET,
+                                              SyncPolicy.INITIAL)):
+            # Created
+            results = table.select(
+                AND(TransactionEntry.q.id == table.q.te_createdID,
+                    TransactionEntry.q.timestamp > sync.timestamp),
+                connection=trans)
+            if results.count():
+                data += self._serialize_inserts(trans, results)
+
+            # Modified
+            results = table.select(
+                AND(TransactionEntry.q.id == table.q.te_modifiedID,
+                    TransactionEntry.q.timestamp > sync.timestamp),
+                connection=trans)
+            if results.count():
+                data += self._serialize_updates(trans, results)
+
+            if len(data) >= CHUNKSIZE:
+                self._sql_insert(sid, data[:CHUNKSIZE])
+                data = data[CHUNKSIZE:]
+
+        # Finally send leftovers
+        if data:
+            self._sql_insert(sid, data)
+
+        error = self.proxy.sql_finish(sid)
+        if error:
+            raise SystemExit('sql_finish returned an error:\n %s' % error)
+
+        self._update_synchronization(trans, station_name, timestamp,
+                                     policy.name)
+
+        if self._commit:
+            trans.commit()
+
     def clone(self, station_name):
+        """
+        Clones the database of the current machine and sends over the complete
+        state to the client as raw SQL commands.
+
+        @param station_name:
+        """
         policy = self._get_policy('shop')
 
         trans = new_transaction()
@@ -228,7 +354,8 @@ class SynchronizationClient(object):
 
         combined = ""
 
-        for table in self._get_tables(policy):
+        for table in self._get_tables(policy,
+                                      filter=(SyncPolicy.FROM_TARGET,)):
             proc = self._pg_dump_table(table)
 
             # This is kind of tricky, only send data when we reached CHUNKSIZE,
@@ -240,15 +367,25 @@ class SynchronizationClient(object):
 
                 combined += data
                 if len(combined) >= CHUNKSIZE:
-                    self.proxy.sql_insert(sid, combined[:CHUNKSIZE])
+                    self._sql_insert(sid, combined[:CHUNKSIZE])
                     combined = combined[CHUNKSIZE:]
 
         # Finally send leftovers
         if combined:
-            self.proxy.sql_insert(sid, combined)
+            self._sql_insert(sid, combined)
 
-        self.proxy.sql_finish(sid)
+        error = self.proxy.sql_finish(sid)
+        if error:
+            raise SystemExit(error)
 
-        self._update_synchronization(trans, station_name, timestamp, policy)
+        self._update_synchronization(trans, station_name, timestamp,
+                                     policy.name)
 
-        trans.commit()
+        if self._commit:
+            trans.commit()
+
+    def disable_commit(self):
+        """
+        Disables committing and sending data to the server
+        """
+        self._commit = False
