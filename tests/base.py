297d19a3fb7e89f237bea40075fe6d3c52a1ac9f
jdahlin <None>
2006-09-28 16:47:29 +0000

    Add a Slave test which tests some slaves

--- tests/base.py
+++ tests/base.py
@@ -38,12 +38,14 @@ from sqlobject.col import (SOUnicodeCol, SOIntCol, SODecimalCol, SODateTimeCol,
 
 from stoqlib.database.columns import SOPriceCol
 from stoqlib.database.runtime import new_transaction
+from stoqlib.database.runtime import get_current_station
 from stoqlib.exceptions import StoqlibError
 from stoqlib.lib.component import Adapter
 from stoqlib.lib.parameters import sysparam
 from stoqlib.domain.interfaces import (IBranch, ICompany, IEmployee,
                                        IIndividual, ISupplier,
-                                       ISellable, IStorable, ISalesPerson)
+                                       ISellable, IStorable, ISalesPerson,
+                                       IClient, IUser)
 
 from bootstrap import bootstrap_testsuite
 
@@ -101,6 +103,10 @@ class DomainTest(unittest.TestCase):
     def tearDown(self):
         self.trans.rollback()
 
+    def create_person(self):
+        from stoqlib.domain.person import Person
+        return Person(name='John', connection=self.trans)
+
     def create_branch(self):
         from stoqlib.domain.person import Person
         person = Person(name='Dummy', connection=self.trans)
@@ -113,10 +119,51 @@ class DomainTest(unittest.TestCase):
         person.addFacet(ICompany, fancy_name='Company Name', connection=self.trans)
         return person.addFacet(ISupplier, connection=self.trans)
 
+
+    def create_employee(self):
+        from stoqlib.domain.person import Person, EmployeeRole
+        person = Person(name='SalesPerson', connection=self.trans)
+        person.addFacet(IIndividual, connection=self.trans)
+        role = EmployeeRole(name='Role', connection=self.trans)
+        return person.addFacet(IEmployee, role=role, connection=self.trans)
+
+    def create_salesperson(self):
+        employee = self.create_employee()
+        return employee.person.addFacet(ISalesPerson, connection=self.trans)
+
+    def create_client(self):
+        from stoqlib.domain.person import Person
+        person = Person(name='Client', connection=self.trans)
+        person.addFacet(IIndividual, connection=self.trans)
+        return person.addFacet(IClient, connection=self.trans)
+
+    def create_individual(self):
+        from stoqlib.domain.person import Person
+        person = Person(name='individual', connection=self.trans)
+        return person.addFacet(IIndividual, connection=self.trans)
+
+    def create_user(self):
+        individual = self.create_individual()
+        profile = self.create_user_profile()
+        return individual.person.addFacet(IUser, username='username',
+                                          password='password',
+                                          profile=profile,
+                                          connection=self.trans)
+
     def create_storable(self):
         from stoqlib.domain.product import Product
         product = Product(connection=self.trans)
         return product.addFacet(IStorable, connection=self.trans)
+
+    def create_product(self):
+        from stoqlib.domain.product import ProductSupplierInfo
+        sellable = self.create_sellable()
+        product = sellable.get_adapted()
+        product.addFacet(IStorable, connection=self.trans)
+        ProductSupplierInfo(connection=self.trans,
+                            supplier=self.create_supplier(),
+                            product=product, is_main_supplier=True)
+        return product
 
     def create_sellable(self):
         from stoqlib.domain.product import Product
@@ -128,15 +175,6 @@ class DomainTest(unittest.TestCase):
         return product.addFacet(ISellable,
                                 base_sellable_info=sellable_info,
                                 connection=self.trans)
-
-    def create_salesperson(self):
-        from stoqlib.domain.person import Person, EmployeeRole
-        person = Person(name='SalesPerson', connection=self.trans)
-        person.addFacet(IIndividual, connection=self.trans)
-        role = EmployeeRole(name='Role', connection=self.trans)
-        person.addFacet(IEmployee, role=role, connection=self.trans)
-        return person.addFacet(ISalesPerson, connection=self.trans)
-
     def create_sale(self):
         from stoqlib.domain.sale import Sale
         from stoqlib.domain.till import Till
@@ -149,10 +187,6 @@ class DomainTest(unittest.TestCase):
                     cfop=sysparam(self.trans).DEFAULT_SALES_CFOP,
                     connection=self.trans)
 
-    def create_person(self):
-        from stoqlib.domain.person import Person
-        return Person(name='John', connection=self.trans)
-
     def create_city_location(self):
         from stoqlib.domain.address import CityLocation
         return CityLocation(country='Groenlandia',
@@ -160,6 +194,67 @@ class DomainTest(unittest.TestCase):
                             state='Wisconsin',
                             connection=self.trans)
 
+    def create_parameter_data(self):
+        from stoqlib.domain.parameter import ParameterData
+        return ParameterData.select(connection=self.trans)[0]
+
+    def create_service_sellable_item(self):
+        from stoqlib.domain.service import ServiceSellableItem
+        sale = self.create_sale()
+        sellable = self.create_sellable()
+        return ServiceSellableItem(
+            sellable=sellable,
+            quantity=1, price=10,
+            sale=sale, connection=self.trans)
+
+    def create_device_settings(self):
+        from stoqlib.lib.drivers import get_fiscal_printer_settings_by_station
+
+        station = get_current_station(self.trans)
+        return get_fiscal_printer_settings_by_station(self.trans, station)
+
+    def create_company(self):
+        from stoqlib.domain.person import Person
+        person = Person(name='Dummy', connection=self.trans)
+        return person.addFacet(ICompany, fancy_name='Dummy shop', connection=self.trans)
+
+    def create_till(self):
+        from stoqlib.domain.till import Till
+        station = get_current_station(self.trans)
+        return Till(connection=self.trans, station=station)
+
+    def create_user_profile(self):
+        from stoqlib.domain.profile import UserProfile
+        return UserProfile(connection=self.trans, name='assistant')
+
+    def get_station(self):
+        return get_current_station(self.trans)
+
+    def create_by_type(self, model_type):
+        known_types = {
+            'ASellable': self.create_sellable,
+            'BranchStation': self.get_station,
+            'IClient': self.create_client,
+            'IBranch': self.create_branch,
+            'IEmployee': self.create_employee,
+            'IIndividual': self.create_individual,
+            'ISupplier': self.create_supplier,
+            'IUser': self.create_user,
+            'ParameterData': self.create_parameter_data,
+            'Person': self.create_person,
+            'PersonAdaptToCompany': self.create_company,
+            'PersonAdaptToClient': self.create_client,
+            'Product': self.create_product,
+            'ProductAdaptToSellable' : self.create_sellable,
+            'Sale': self.create_sale,
+            'ServiceSellableItem': self.create_service_sellable_item,
+            'Till': self.create_till,
+            'UserProfile': self.create_user_profile,
+            }
+
+        model_name = model_type.__name__
+        if model_name in known_types:
+            return known_types[model_name]()
 
 class BaseDomainTest(unittest.TestCase):
     """Base class to be used by all domain test classes.
