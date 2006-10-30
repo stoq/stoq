32424537a172a94515c2cac78fae02a64609452f
jdahlin <None>
2006-10-30 21:36:40 +0000

    Remove set_person_utilities

--- stoqlib/domain/examples/createall.py
+++ stoqlib/domain/examples/createall.py
@@ -22,7 +22,7 @@
 """ Create all objects for an example database used by Stoq applications"""
 
 from stoqlib.domain.examples import log
-from stoqlib.domain.examples.person import create_people, set_person_utilities
+from stoqlib.domain.examples.person import create_people
 from stoqlib.domain.examples.product import create_products
 from stoqlib.domain.examples.service import create_services
 from stoqlib.domain.examples.sale import create_sales
@@ -31,11 +31,9 @@ from stoqlib.domain.examples.purchase import create_purchases
 from stoqlib.domain.examples.giftcertificate import create_giftcertificates
 from stoqlib.domain.examples.devices import create_device_settings
 
-def create(utilities=False):
+def create():
     log.info('Creating example database')
     create_people()
-    if utilities:
-        set_person_utilities()
     create_products()
     create_services()
     create_payments()
