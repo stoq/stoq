122f85643d8665a40160668f7935f9f5e19968bf
jdahlin <None>
2007-01-24 21:04:48 +0000

    Create the examples by sending an order an actually receive them instead of doign a big hack on the IStorable facets. Reduce the number of orders created, it's enough to create one for the items we want to buy.

--- stoqlib/domain/examples/purchase.py
+++ stoqlib/domain/examples/purchase.py
@@ -23,117 +23,78 @@
 ##
 """ Create purchase objects for an example database"""
 
-from decimal import Decimal
-import datetime
-
-from kiwi.datatypes import currency
-
-from stoqlib.database.runtime import new_transaction
+from stoqlib.database.runtime import (new_transaction, get_current_branch,
+                                      get_current_user)
 from stoqlib.domain.examples import log
-from stoqlib.domain.purchase import PurchaseOrder, PurchaseItem
+from stoqlib.domain.purchase import PurchaseOrder
 from stoqlib.domain.person import Person
-from stoqlib.domain.interfaces import ISupplier, IBranch, IPaymentGroup
+from stoqlib.domain.interfaces import (ISupplier, IPaymentGroup,
+                                       ITransporter)
+from stoqlib.domain.receiving import ReceivingOrder, ReceivingOrderItem
 from stoqlib.domain.sellable import ASellable
 from stoqlib.lib.defaults import INTERVALTYPE_MONTH, METHOD_BILL
 
-
-EXPECTED_BRANCHES = 1
-MAX_PURCHASES_NUMBER = 3
-SELLABLES_PER_PURCHASE = 2
 
 def create_purchases():
     log.info('Creating purchase orders')
     trans = new_transaction()
 
-    supplier_table = Person.getAdapterClass(ISupplier)
-    suppliers = supplier_table.get_active_suppliers(trans)
-    if suppliers.count() < MAX_PURCHASES_NUMBER:
-        raise ValueError('You must have at least %d suppliers in your '
-                         'database at this point.' % MAX_PURCHASES_NUMBER)
+    suppliers = Person.getAdapterClass(ISupplier).get_active_suppliers(trans)
+    if not suppliers.count():
+        raise ValueError('You must have at least one suppliers in your '
+                         'database at this point.')
 
-    branch_table = Person.getAdapterClass(IBranch)
-    branches = branch_table.get_active_branches(trans)
-    if branches.count() != EXPECTED_BRANCHES:
-        raise ValueError(
-            'Expected %d branches in your database, but found %d' % (
-            EXPECTED_BRANCHES, branches.count()))
-    branch = branches[0]
+    transporters = Person.getAdapterClass(ITransporter
+                                          ).select(connection=trans)
+    if not transporters.count():
+        raise ValueError('You must have at least one suppliers in your '
+                         'database at this point.')
 
     sellables = ASellable.select(connection=trans)
-    sellables_total = SELLABLES_PER_PURCHASE * MAX_PURCHASES_NUMBER
-    if sellables.count() < sellables_total:
-        raise ValueError('You must have at least %d sellables in your '
-                         'database at this point.' % sellables_total)
+    if not sellables.count():
+        raise ValueError('You must have at least one sellables in your '
+                         'database at this point.')
 
-    dates = []
-    open_dates = []
-    purchaseitem_data = []
-    for i in range(sellables_total):
-        date = datetime.datetime.now() + datetime.timedelta(i + 7)
-        dates.append(date)
-        open_date = datetime.datetime.now() + datetime.timedelta(i + 5)
-        open_dates.append(open_date)
-        purchaseitem_data.append(dict(quantity=i + 10,
-                                      quantity_received=i + 5))
+    sellables = ASellable.select(connection=trans)
+    if not sellables.count():
+        raise ValueError('You must have at least one sellables in your '
+                         'database at this point.')
 
-    purchase_data = [dict(status=PurchaseOrder.ORDER_QUOTING,
-                          salesperson_name='Milena Albuquerque',
-                          discount_value=10,
-                          open_date=open_dates[0],
-                          quote_deadline=dates[0]),
-                     dict(status=PurchaseOrder.ORDER_PENDING,
-                          salesperson_name='Cristiana Oliveira',
-                          open_date=open_dates[1],
-                          discount_value=currency(Decimal('15.50'))),
-                     dict(status=PurchaseOrder.ORDER_CONFIRMED,
-                          salesperson_name='Livia Lemos',
-                          open_date=open_dates[2],
-                          expected_receival_date=dates[1],
-                          confirm_date=dates[2],
-                          surcharge_value=currency(Decimal('13.70'))),
-                     dict(status=PurchaseOrder.ORDER_CLOSED,
-                          salesperson_name='Graziela Maia',
-                          open_date=open_dates[3],
-                          receival_date=dates[3],
-                          surcharge_value=currency(Decimal('34.32')))]
+    branch = get_current_branch(trans)
+    user = get_current_user(trans)
 
-    sellable_index = 0
+    order = PurchaseOrder(connection=trans,
+                          status=PurchaseOrder.ORDER_PENDING,
+                          supplier=suppliers[1],
+                          branch=branch)
+    order.set_valid()
+    order.addFacet(IPaymentGroup,
+                   default_method=METHOD_BILL,
+                   intervals=1,
+                   interval_type=INTERVALTYPE_MONTH,
+                   connection=trans)
 
-    for index in range(MAX_PURCHASES_NUMBER):
+    receiving_order = ReceivingOrder(purchase=order,
+                                     responsible=user,
+                                     supplier=suppliers[0],
+                                     invoice_number=1,
+                                     transporter=transporters[0],
+                                     branch=branch,
+                                     connection=trans)
 
-        supplier = suppliers[index]
+    for sellable in sellables:
+        item = order.add_item(sellable, 5)
+        order.receive_item(item, 5)
 
-        purchase_args = purchase_data[index]
-        order = PurchaseOrder(connection=trans, supplier=supplier,
-                              branch=branch, **purchase_args)
-        order.set_valid()
+        ReceivingOrderItem(connection=trans,
+                           cost=sellable.cost / 2,
+                           sellable=sellable,
+                           quantity_received=5,
+                           receiving_order=receiving_order)
+    order.confirm_order()
 
-
-        indexes = sellable_index, sellable_index + 1
-        # Increment sellable index in two positions and get exclusive
-        # sellable instance in the next loop
-        sellable_index += 2
-        for index in indexes:
-            sellable = sellables[index]
-            item_args = purchaseitem_data[index]
-            PurchaseItem(connection=trans, cost=sellable.cost,
-                         order=order, sellable=sellable,
-                         **item_args)
-    new_order = PurchaseOrder(connection=trans,
-                              status=PurchaseOrder.ORDER_PENDING,
-                              supplier=suppliers[1],
-                              branch=branch)
-    new_order.set_valid()
-    new_order.addFacet(IPaymentGroup,
-                       default_method=METHOD_BILL,
-                       intervals=1,
-                       interval_type=INTERVALTYPE_MONTH,
-                       connection=trans)
-    new_order.confirm_order()
-    item = new_order.add_item(sellables[1], purchaseitem_data[0]['quantity'])
-    new_order.receive_item(item,
-                           purchaseitem_data[0]['quantity'])
-    new_order.close()
+    receiving_order.set_valid()
+    receiving_order.confirm()
 
     trans.commit()
 
