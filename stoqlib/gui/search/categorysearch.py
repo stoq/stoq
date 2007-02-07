443d28cbc261ff30c6b1cddd1532cddc230f2710
jdahlin <None>
2007-02-07 17:41:41 +0000

    #3116: Resurrect Sellable Category Search, stoqlib part

--- stoqlib/gui/search/categorysearch.py
+++ stoqlib/gui/search/categorysearch.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 
 ##
-## Copyright (C) 2005, 2006 Async Open Source <http://www.async.com.br>
+## Copyright (C) 2005-2007 Async Open Source <http://www.async.com.br>
 ## All rights reserved
 ##
 ## This program is free software; you can redistribute it and/or modify
@@ -20,6 +20,7 @@
 ## Foundation, Inc., or visit: http://www.gnu.org/.
 ##
 ##  Author(s):  Evandro Vale Miquelito      <evandro@async.com.br>
+##              Johan Dahlin                <jdahlin@async.com.br>
 ##
 ##
 """ A dialog for sellable categories selection, offering buttons for
@@ -30,9 +31,10 @@ from kiwi.ui.objectlist import Column
 
 from stoqlib.lib.translation import stoqlib_gettext
 from stoqlib.gui.base.search import SearchEditor
-from stoqlib.gui.editors.categoryeditor import BaseSellableCategoryEditor
-
-from stoqlib.domain.sellable import BaseSellableCategory
+from stoqlib.gui.editors.categoryeditor import (BaseSellableCategoryEditor,
+                                                SellableCategoryEditor)
+from stoqlib.domain.sellable import (BaseSellableCategory,
+                                     SellableCategory)
 
 _ = stoqlib_gettext
 
@@ -55,3 +57,26 @@ class BaseSellableCatSearch(SearchEditor):
                        _("Salesperson Commission (%)"), data_type=float,
                        width=200),
             ]
+
+class SellableCategorySearch(SearchEditor):
+    size = (700, 500)
+    title = _('Sellable Category Search')
+
+    def __init__(self, conn):
+        SearchEditor.__init__(self, conn, SellableCategory,
+                              SellableCategoryEditor)
+        self.set_searchbar_labels(_('Categories Matching:'))
+        self.set_result_strings(_('category'), _('categories'))
+
+    def get_columns(self):
+        return [
+            Column("description", _("Description"), data_type=str,
+                   sorted=True, expand=True),
+            Column("suggested_markup", _("Suggested Markup (%)"),
+                   data_type=str, width=170),
+            Column("salesperson_commission",
+                   _("Suggested Commission (%)"), data_type=str,
+                   width=190),
+            ]
+
+
