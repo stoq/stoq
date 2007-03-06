f4ff4c244ecd2d207bbf35cc81d18ea26eea9841
Johan Dahlin <jdahlin@async.com.br>
2007-03-06 21:47:29 +0000

    Add debugging log

--- stoqdrivers/devices/printers/fiscal.py
+++ stoqdrivers/devices/printers/fiscal.py
@@ -3,7 +3,7 @@
 
 ##
 ## Stoqdrivers
-## Copyright (C) 2005 Async Open Source <http://www.async.com.br>
+## Copyright (C) 2005-2007 Async Open Source <http://www.async.com.br>
 ## All rights reserved
 ##
 ## This program is free software; you can redistribute it and/or modify
@@ -28,6 +28,7 @@
 from decimal import Decimal
 
 from kiwi.argcheck import number, percent
+from kiwi.log import Logger
 
 from stoqdrivers.exceptions import (CloseCouponError, PaymentAdditionError,
                                     AlreadyTotalized, InvalidValue)
@@ -41,6 +42,8 @@ from stoqdrivers.utils import encode_text
 from stoqdrivers.translation import stoqdrivers_gettext
 
 _ = lambda msg: stoqdrivers_gettext(msg)
+
+log = Logger('stoqdrivers.fiscalprinter')
 
 #
 # Extra data types to argcheck
@@ -89,14 +92,18 @@ class FiscalPrinter(BasePrinter):
 
     @capcheck(basestring, basestring, basestring)
     def identify_customer(self, customer_name, customer_address, customer_id):
-        self.info('identify_customer')
+        self.debug('identify_customer(customer_name=%r, '
+                   'customer_address=%r, customer_id=%r)' % (
+            customer_name, customer_address, customer_id))
+
         self._driver.coupon_identify_customer(
             self._format_text(customer_name),
             self._format_text(customer_address),
             self._format_text(customer_id))
 
     def open(self):
-        self.info('coupon_open')
+        log.debug('coupon_open()')
+
         return self._driver.coupon_open()
 
     @capcheck(basestring, basestring, Decimal, str, Decimal, unit,
@@ -105,6 +112,12 @@ class FiscalPrinter(BasePrinter):
                  items_quantity=Decimal("1.0"), unit=UNIT_EMPTY,
                  discount=Decimal("0.0"), surcharge=Decimal("0.0"),
                  unit_desc=""):
+        log.debug("add_item(code=%r, description=%r, price=%r, "
+                  "taxcode=%r, quantity=%r, unit=%r, discount=%r, "
+                  "surcharge=%r, unit_desc=%r)" % (
+            item_code, item_description, item_price, taxcode,
+            items_quantity, unit, discount, surcharge, unit_desc))
+
         if self._has_been_totalized:
             raise AlreadyTotalized("the coupon is already totalized, you "
                                    "can't add more items")
@@ -129,12 +142,14 @@ class FiscalPrinter(BasePrinter):
     @capcheck(percent, percent, taxcode)
     def totalize(self, discount=Decimal("0.0"), surcharge=Decimal("0.0"),
                  taxcode=TAX_NONE):
+        log.debug('totalize(discount=%r, surcharge=%r, taxcode=%r)' % (
+            discount, surcharge, taxcode))
+
         if discount and surcharge:
             raise TypeError("discount and surcharge can not be used together")
         if surcharge and taxcode == TAX_NONE:
             raise ValueError("to specify a surcharge you need specify its "
                              "tax code")
-        self.info('coupon_totalize')
         result = self._driver.coupon_totalize(discount, surcharge, taxcode)
         self._has_been_totalized = True
         self.totalized_value = result
@@ -143,6 +158,10 @@ class FiscalPrinter(BasePrinter):
     @capcheck(payment_method, Decimal, basestring, basestring)
     def add_payment(self, payment_method, payment_value,
                     payment_description='', custom_pm=''):
+        log.debug("add_payment(method=%r, value=%r, description=%r, "
+                  "custom_pm=%r)" % (payment_method, payment_value,
+                                     payment_description, custom_pm))
+
         self.info('coupon_add_payment')
         if not self._has_been_totalized:
             raise PaymentAdditionError(_("You must totalize the coupon "
@@ -162,17 +181,21 @@ class FiscalPrinter(BasePrinter):
         return result
 
     def cancel(self):
-        self.info('coupon_cancel')
+        log.debug('coupon_cancel()')
+
         return self._driver.coupon_cancel()
 
     @capcheck(int)
     def cancel_item(self, item_id):
-        self.info('coupon_cancel_item')
+        log.debug('coupon_cancel_item(item_id=%r)' % (item_id,))
+
         return self._driver.coupon_cancel_item(item_id)
 
     @capcheck(basestring)
     def close(self, promotional_message=''):
-        self.info('coupon_close')
+        log.debug('coupon_close(promotional_message=%r)' % (
+            promotional_message))
+
         if not self._has_been_totalized:
             raise CloseCouponError(_("You must totalize the coupon before "
                                      "closing it"))
@@ -191,19 +214,24 @@ class FiscalPrinter(BasePrinter):
         return res
 
     def summarize(self):
-        self.info('summarize')
+        log.debug('summarize()')
+
         return self._driver.summarize()
 
     def close_till(self):
-        self.info('close_till')
+        log.debug('close_till')
+
         return self._driver.close_till()
 
     @capcheck(Decimal)
     def till_add_cash(self, add_cash_value):
-        self.info('till_add_cash')
+        log.debug('till_add_cash(add_cash_value=%r)' % (add_cash_value,))
+
         return self._driver.till_add_cash(add_cash_value)
 
     @capcheck(Decimal)
     def till_remove_cash(self, remove_cash_value):
-        self.info('till_remove_cash')
+        log.debug('till_remove_cash(remove_cash_value=%r)' % (
+            remove_cash_value,))
+
         return self._driver.till_remove_cash(remove_cash_value)
