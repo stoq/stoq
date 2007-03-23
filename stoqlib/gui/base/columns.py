4d3f39381bdcc9103d326bc12b3a6d01a80bc64c
fabio <None>
2007-03-23 18:39:20 +0000

    #3247: Ao classificar produtos/serviços no segundo wizard, a coluna stock não é atualizada. r=jdahlin

--- stoqlib/gui/base/columns.py
+++ stoqlib/gui/base/columns.py
@@ -88,8 +88,6 @@ class AccessorColumn(Column):
             raise TypeError('AccessorColumn needs an accessor argument')
 
         self.accessor = accessor
-        if not kwargs.has_key('cache'):
-            kwargs['cache'] = True
         assert callable(self.accessor)
         Column.__init__(self, attribute=attribute, *args, **kwargs)
 
