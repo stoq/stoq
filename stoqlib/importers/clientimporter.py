d2f3bdc3bf63ab6f52689156b9dbd572bb74e12b
jdahlin <None>
2007-03-30 13:23:03 +0000

    OOps, forgot to add these

---
+++ stoqlib/importers/clientimporter.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2007 Async Open Source
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Lesser General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+##
+## Author(s):       Johan Dahlin                <jdahlin@async.com.br>
+##
+##
+
+from stoqlib.domain.address import Address, CityLocation
+from stoqlib.domain.interfaces import IIndividual, IClient
+from stoqlib.domain.person import Person
+from stoqlib.importers.csvimporter import CSVImporter
+
+class ClientImporter(CSVImporter):
+    fields = ['name',
+              'phone_number',
+              'mobile_number',
+              'email',
+              'rg',
+              'cpf',
+              'city',
+              'country',
+              'state',
+              'street',
+              'street_number',
+              'district']
+
+    def __init__(self):
+        pass
+
+    def process_one(self, data, fields, trans):
+        person = Person(
+            connection=trans,
+            name=data.name,
+            phone_number=data.phone_number,
+            mobile_number=data.mobile_number)
+
+        person.addFacet(IIndividual,
+                        connection=trans,
+                        cpf=data.cpf,
+                        rg_number=data.rg)
+
+        ctloc = CityLocation(connection=trans,
+                             city=data.city,
+                             state=data.state,
+                             country=data.country)
+        address = Address(is_main_address=True,
+                          person=person, city_location=ctloc,
+                          connection=trans,
+                          street=data.street,
+                          number=int(data.street_number),
+                          district=data.district)
+        address.ensure_address()
+
+        person.addFacet(IClient, connection=trans)
