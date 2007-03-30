3ce644aec145465c1c849539a40b04c345a11cdd
jdahlin <None>
2007-03-30 21:54:16 +0000

    Lookup name in permissions instead of trying to access it unconditionally.

--- stoq/gui/runner.py
+++ stoq/gui/runner.py
@@ -104,7 +104,7 @@ class ApplicationRunner(object):
         # sorting by app_full_name
         for name, full, icon, descr in sorted(descriptions,
                                               key=operator.itemgetter(1)):
-            if permissions[name]:
+            if name in permissions:
                 available_applications.append(
                     Application(name, full, icon, descr))
 
