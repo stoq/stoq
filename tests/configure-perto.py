cd83aae333dcef3842bbdbce03d7d5416ff46166
jdahlin <None>
2007-03-30 16:06:07 +0000

    use int(enum)

--- tests/configure-perto.py
+++ tests/configure-perto.py
@@ -11,7 +11,7 @@ station = get_current_station(trans)
 assert station
 for value in [25, 17, 12, 8, 5]:
     SellableTaxConstant(description='%d %%' % (value,),
-                        tax_type=TaxType.CUSTOM,
+                        tax_type=int(TaxType.CUSTOM),
                         tax_value=value,
                         connection=trans)
 
@@ -31,14 +31,14 @@ for device_value, value in  [('\x80', Decimal(17)),
     DeviceConstant(constant_type=DeviceConstant.TYPE_TAX,
                    constant_name='%d %%' % (value,),
                    constant_value=value,
-                   constant_enum=TaxType.CUSTOM,
+                   constant_enum=int(TaxType.CUSTOM),
                    device_value=device_value,
                    device_settings=settings,
                    connection=trans)
 DeviceConstant(constant_type=DeviceConstant.TYPE_TAX,
                constant_name="3 % (ISS)",
                constant_value=3,
-               constant_enum=TaxType.SERVICE,
+               constant_enum=int(TaxType.SERVICE),
                device_value='\x85',
                device_settings=settings,
                connection=trans)
