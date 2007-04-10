d9e94047f5965b20a597871ec6e7240af9c4a6e4
fabio <None>
2007-04-10 15:03:48 +0000

    Fix the SQL patch to delete references in abstract_fiscal_book_entry and broken receving orders

--- data/sql/patch-5.sql
+++ data/sql/patch-5.sql
@@ -1,4 +1,10 @@
 -- #3297: Remove ReceivingOrder IPaymentGroup implementation
-DROP TABLE receiving_order_adapt_to_payment_group;
+DELETE FROM abstract_fiscal_book_entry WHERE id IN (
+  SELECT abstract_fiscal_book_entry.id
+   FROM abstract_fiscal_book_entry, abstract_payment_group
+  WHERE payment_group_id = abstract_payment_group.id AND
+        abstract_payment_group.child_name = 'ReceivingOrderAdaptToPaymentGroup');
 DELETE FROM abstract_payment_group where child_name = 'ReceivingOrderAdaptToPaymentGroup';
+DELETE from receiving_order WHERE purchase_id IS NULL;
 ALTER TABLE receiving_order ALTER COLUMN purchase_id SET NOT NULL;
+DROP TABLE receiving_order_adapt_to_payment_group;
