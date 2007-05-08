73ad85cddabb56cf0f3f12b596f4beac7579391b
fabio <None>
2007-05-08 18:26:36 +0000

    #3053: Implementar opção de impressão para pagamentos. r=jdahlin

--- stoqlib/reporting/payment.py
+++ stoqlib/reporting/payment.py
@@ -32,20 +32,32 @@ from stoqlib.reporting.base.tables import ObjectTableColumn as OTC
 from stoqlib.lib.translation import stoqlib_gettext as _
 from stoqlib.lib.validators import get_formatted_price
 
-class PaymentReport(SearchResultsReport):
-    """
-    This report shows a list of payments. For each payment it shows:
-    payment number, description, drawee, due date, paid date, status and value.
-    """
+class _BasePaymentReport(SearchResultsReport):
+    """Base report for Payable and Receivable reports"""
     report_name = "Payment Report"
     main_object_name = "payments"
 
     def __init__(self, filename, payments, *args, **kwargs):
         self._payments = payments
         SearchResultsReport.__init__(self, filename, payments,
-                                     PaymentReport.report_name,
+                                     _BasePaymentReport.report_name,
                                      landscape=True, *args, **kwargs)
         self._setup_table()
+
+    def _setup_table(self):
+        total_value = sum([item.value for item in self._payments],
+                          Decimal(0))
+        summary_row = ["", "", "", "", "", _("Total:"),
+                       get_formatted_price(total_value)]
+        self.add_object_table(self._payments, self.get_columns(), width=745,
+                              summary_row=summary_row)
+
+
+class ReceivablePaymentReport(_BasePaymentReport):
+    """
+    This report shows a list of receivable payments. For each payment it shows:
+    payment number, description, drawee, due date, paid date, status and value.
+    """
 
     def get_columns(self):
         return [OTC(_("Number"),
@@ -66,13 +78,32 @@ class PaymentReport(SearchResultsReport):
                     align=RIGHT)
             ]
 
-    def _setup_table(self):
-        total_value = sum([item.value for item in self._payments],
-                          Decimal(0))
-        summary_row = ["", "", "", "", "", _("Total:"),
-                       get_formatted_price(total_value)]
-        self.add_object_table(self._payments, self.get_columns(), width=745,
-                              summary_row=summary_row)
+
+class PaymentPayableReport(_BasePaymentReport):
+    """
+    This report shows a list of payable payments. For each payment it shows:
+    payment number, description, supplier, due date, paid date,
+    status and value.
+    """
+
+    def get_columns(self):
+        return [OTC(_("Number"),
+                    lambda obj: obj.id, width=60),
+                OTC(_("Description"),
+                    lambda obj: obj.description, width=150),
+                OTC(_("Supplier"),
+                    lambda obj: obj.supplier_name, width=50,
+                    expand=True, expand_factor=1),
+                OTC(_("Due date"),
+                    lambda obj: format_date(obj.due_date), width=80),
+                OTC(_("Paid date"),
+                    lambda obj: format_date(obj.paid_date), width=80),
+                OTC(_("Status"),
+                    lambda obj: obj.get_status_str(), width=50),
+                OTC(_("Value"),
+                    lambda obj: get_formatted_price(obj.value), width=100,
+                    align=RIGHT)
+            ]
 
 
 def format_date(date):
