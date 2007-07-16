b5b993af61e173c041b7246cdffc41d7769ec88b
Johan Dahlin <jdahlin@async.com.br>
2007-07-16 15:46:15 +0000

    Only send IDENTIFY CUSTOMER if we have a customer set

--- stoqdrivers/printers/daruma/FS345.py
+++ stoqdrivers/printers/daruma/FS345.py
@@ -492,13 +492,16 @@ class FS345(SerialBase):
         self._check_status()
         self._verify_coupon_open()
 
-        customer_name = self._customer_name or _("No client")
-        customer_document = self._customer_document or _("No document")
-        customer_address = self._customer_address or _("No address")
-        self.send_command(CMD_IDENTIFY_CUSTOMER,
-                          "%- 84s%- 84s%- 84s" % (customer_name,
-                                                  customer_address,
-                                                  customer_document))
+        if (self._customer_name or
+            self._customer_address or
+            self._customer_document):
+            customer_name = self._customer_name or _("No client")
+            customer_document = self._customer_document or _("No document")
+            customer_address = self._customer_address or _("No address")
+            self.send_command(CMD_IDENTIFY_CUSTOMER,
+                              "%- 84s%- 84s%- 84s" % (customer_name,
+                                                      customer_address,
+                                                      customer_document))
         LINE_LEN = 48
         msg_len = len(message)
         if msg_len > LINE_LEN:
