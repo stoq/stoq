5277fea9dfee260cb75fce51ba3f621effc4011c
jdahlin <None>
2007-07-17 14:49:10 +0000

    0.9.1

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -26,8 +26,8 @@
 
 program_name    = "Stoq"
 website         = 'http://www.stoq.com.br'
-version         = "0.9.0"
-release_date    = (2007, 05, 23)
+version         = "0.9.1"
+release_date    = (2007, 7, 16)
 
 try:
     from kiwi.environ import Library
