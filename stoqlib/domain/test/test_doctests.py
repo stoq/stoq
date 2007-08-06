361fbe4d8940c780fcbdaf0b9ee3fa6ca4ac40bd
jdahlin <None>
2007-08-06 15:10:01 +0000

    Get rid of tests.base and move that functionallity to stoqlib.database.testsuite.bootstrap

--- stoqlib/domain/test/test_doctests.py
+++ stoqlib/domain/test/test_doctests.py
@@ -20,10 +20,10 @@
 ## Foundation, Inc., or visit: http://www.gnu.org/.
 ##
 
+from stoqlib.database.testsuite import bootstrap
 from stoqlib.lib.doctestloader import create_doctest
 
 # So we can use database connections in doctests.
-import tests.base
-tests.base # pyflakes
+bootstrap()
 
 doctest = create_doctest('*.txt')
