587a53bb6d98850a43f3c4112927c4b519013d26
jdahlin <None>
2007-08-09 19:47:18 +0000

    Update to trunk and add a couple of hacks to sqlite

--- external/sqlobject/sqlite/sqliteconnection.py
+++ external/sqlobject/sqlite/sqliteconnection.py
@@ -1,10 +1,20 @@
 from sqlobject.dbconnection import DBAPI
 from sqlobject.col import popKey
+from sqlobject import col, sqlbuilder
+from sqlobject.dberrors import *
 import thread
 
 sqlite = None
 using_sqlite2 = False
 sqlite2_Binary = None
+
+class ErrorMessage(str):
+    def __new__(cls, e):
+        obj = str.__new__(cls, e[0])
+        obj.code = None
+        obj.module = e.__module__
+        obj.exception = e.__class__.__name__
+        return obj
 
 class SQLiteConnection(DBAPI):
 
@@ -17,11 +27,15 @@ class SQLiteConnection(DBAPI):
         global using_sqlite2
         if sqlite is None:
             try:
-                from pysqlite2 import dbapi2 as sqlite
+                import sqlite3 as sqlite
                 using_sqlite2 = True
             except ImportError:
-                import sqlite
-                using_sqlite2 = False
+                try:
+                    from pysqlite2 import dbapi2 as sqlite
+                    using_sqlite2 = True
+                except ImportError:
+                    import sqlite
+                    using_sqlite2 = False
         self.module = sqlite
         self.filename = filename  # full path to sqlite-db-file
         self._memory = filename == ':memory:'
@@ -38,9 +52,9 @@ class SQLiteConnection(DBAPI):
                 import warnings
                 warnings.warn(DeprecationWarning("pysqlite2 does not support the encoding option"))
             opts["detect_types"] = sqlite.PARSE_DECLTYPES
-            for col_type in "text", "char", "varchar":
-                sqlite.register_converter(col_type, stop_pysqlite2_converting_strings_to_unicode)
-                sqlite.register_converter(col_type.upper(), stop_pysqlite2_converting_strings_to_unicode)
+            for col_type in "text", "char", "varchar", "date", "time", "datetime", "timestamp":
+                sqlite.register_converter(col_type, stop_pysqlite2_converting_strings)
+                sqlite.register_converter(col_type.upper(), stop_pysqlite2_converting_strings)
             try:
                 from sqlite import encode, decode
             except ImportError:
@@ -54,6 +68,11 @@ class SQLiteConnection(DBAPI):
             if sqlite2_Binary is None:
                 sqlite2_Binary = sqlite.Binary
                 sqlite.Binary = lambda s: sqlite2_Binary(sqlite.encode(s))
+            if 'factory' in kw:
+                factory = popKey(kw, 'factory')
+                if isinstance(factory, str):
+                    factory = globals()[factory]
+                opts['factory'] = factory(sqlite)
         else:
             opts['autocommit'] = bool(autoCommit)
             if 'encoding' in kw:
@@ -61,12 +80,16 @@ class SQLiteConnection(DBAPI):
             if 'mode' in kw:
                 opts['mode'] = int(popKey(kw, 'mode'), 0)
         if 'timeout' in kw:
-            opts['timeout'] = float(popKey(kw, 'timeout'))
+            if using_sqlite2:
+                opts['timeout'] = float(popKey(kw, 'timeout'))
+            else:
+                opts['timeout'] = int(float(popKey(kw, 'timeout')) * 1000)
         if 'check_same_thread' in kw:
             opts["check_same_thread"] = bool(popKey(kw, 'check_same_thread'))
         # use only one connection for sqlite - supports multiple)
         # cursors per connection
         self._connOptions = opts
+        self.use_table_info = popKey(kw, "use_table_info", False)
         DBAPI.__init__(self, **kw)
         self._threadPool = {}
         self._threadOrigination = {}
@@ -95,7 +118,10 @@ class SQLiteConnection(DBAPI):
         # pool connections.  Since we are isolating threads here, we
         # don't have to worry about locking as much.
         if self._memory:
-            return self.makeConnection()
+            conn = self.makeConnection()
+            self._connectionNumbers[id(conn)] = self._connectionCount
+            self._connectionCount += 1
+            return conn
         threadid = thread.get_ident()
         if (self._pool is not None
             and self._threadPool.has_key(threadid)):
@@ -148,6 +174,38 @@ class SQLiteConnection(DBAPI):
             return self._memoryConn
         return sqlite.connect(self.filename, **self._connOptions)
 
+    def _executeRetry(self, conn, cursor, query):
+        query = query.replace('NOW()', "datetime('now')")
+
+        if self.debug:
+            self.printDebug(conn, query, 'QueryR')
+        try:
+            return cursor.execute(query)
+        except self.module.OperationalError, e:
+            raise OperationalError(ErrorMessage(e))
+        except self.module.IntegrityError, e:
+            msg = ErrorMessage(e)
+            if msg.startswith('column') and msg.endswith('not unique'):
+                raise DuplicateEntryError(msg)
+            else:
+                raise IntegrityError(msg)
+        except self.module.InternalError, e:
+            raise InternalError(ErrorMessage(e))
+        except self.module.ProgrammingError, e:
+            raise ProgrammingError(ErrorMessage(e))
+        except self.module.DataError, e:
+            raise DataError(ErrorMessage(e))
+        except self.module.NotSupportedError, e:
+            raise NotSupportedError(ErrorMessage(e))
+        except self.module.DatabaseError, e:
+            raise DatabaseError(ErrorMessage(e))
+        except self.module.InterfaceError, e:
+            raise InterfaceError(ErrorMessage(e))
+        except self.module.Warning, e:
+            raise Warning(ErrorMessage(e))
+        except self.module.Error, e:
+            raise Error(ErrorMessage(e))
+
     def _queryInsertID(self, conn, soInstance, id, names, values):
         table = soInstance.sqlmeta.table
         idName = soInstance.sqlmeta.idName
@@ -158,7 +216,7 @@ class SQLiteConnection(DBAPI):
         q = self._insertSQL(table, names, values)
         if self.debug:
             self.printDebug(conn, q, 'QueryIns')
-        c.execute(q)
+        self._executeRetry(conn, c, q)
         # lastrowid is a DB-API extension from "PEP 0249":
         if id is None:
             id = int(c.lastrowid)
@@ -189,8 +247,11 @@ class SQLiteConnection(DBAPI):
         return None
 
     def createIDColumn(self, soClass):
-        key_type = {int: "INTEGER", str: "TEXT"}[soClass.sqlmeta.idType]
-        return '%s %s PRIMARY KEY' % (soClass.sqlmeta.idName, key_type)
+        return self._createIDColumn(soClass.sqlmeta)
+
+    def _createIDColumn(self, sqlmeta):
+        key_type = {int: "INTEGER", str: "TEXT"}[sqlmeta.idType]
+        return '%s %s PRIMARY KEY' % (sqlmeta.idName, key_type)
 
     def joinSQLType(self, join):
         return 'INT NOT NULL'
@@ -200,6 +261,40 @@ class SQLiteConnection(DBAPI):
         # turn it into a boolean:
         return not not result
 
+    def viewExists(self, tableName):
+        result = self.queryOne("SELECT tbl_name FROM sqlite_master WHERE type='view' AND tbl_name = '%s'" % tableName)
+        # turn it into a boolean:
+        return not not result
+
+    def tableHasColumn(self, tableName, columnName):
+        colData = self.queryOne("SELECT sql FROM sqlite_master WHERE type='table' AND name='%s'"
+                                % tableName)
+        if not colData:
+            return False
+        colData = colData[0].split('(', 1)[1].strip()[:-2]
+        while colData.find('(') > -1:
+            st = colData.find('(')
+            en = colData.find(')')
+            if en == -1:
+                break
+            colData = colData[:st] + colData[en+1:]
+        results = []
+        fields = set()
+        for colDesc in colData.split(','):
+            parts = colDesc.strip().split(' ', 2)
+            field = parts[0].strip()
+            # skip comments
+            if field.startswith('--'):
+                continue
+            # get rid of enclosing quotes
+            if field[0] == field[-1] == '"':
+                field = field[1:-1]
+            fields.add(field)
+        return columnName in fields
+
+    def dropColumn(self, tableName, column):
+        pass
+
     def createIndexSQL(self, soClass, index):
         return index.sqliteCreateIndexSQL(soClass)
 
@@ -207,9 +302,105 @@ class SQLiteConnection(DBAPI):
         self.query('ALTER TABLE %s ADD COLUMN %s' %
                    (tableName,
                     column.sqliteCreateSQL()))
+        self.query('VACUUM %s' % tableName)
 
-    def delColumn(self, tableName, column):
-        pass # Oops! There is no DROP COLUMN in SQLite
+    def delColumn(self, sqlmeta, column):
+        self.recreateTableWithoutColumn(sqlmeta, column)
 
-def stop_pysqlite2_converting_strings_to_unicode(s):
+    def recreateTableWithoutColumn(self, sqlmeta, column):
+        new_name = sqlmeta.table + '_ORIGINAL'
+        self.query('ALTER TABLE %s RENAME TO %s' % (sqlmeta.table, new_name))
+        cols = [self._createIDColumn(sqlmeta)] \
+                     + [self.createColumn(None, col)
+                        for col in sqlmeta.columnList if col.name != column]
+        cols = ",\n".join(["    %s" % c for c in cols])
+        self.query('CREATE TABLE %s (\n%s\n)' % (sqlmeta.table, cols))
+        all_columns = ', '.join(['id'] + [col.dbName for col in sqlmeta.columnList])
+        self.query('INSERT INTO %s (%s) SELECT %s FROM %s' % (
+            sqlmeta.table, all_columns, all_columns, new_name))
+        self.query('DROP TABLE %s' % new_name)
+
+    def columnsFromSchema(self, tableName, soClass):
+        if self.use_table_info:
+            return self._columnsFromSchemaTableInfo(tableName, soClass)
+        else:
+            return self._columnsFromSchemaParse(tableName, soClass)
+
+    def _columnsFromSchemaTableInfo(self, tableName, soClass):
+        colData = self.queryAll("PRAGMA table_info(%s)" % tableName)
+        results = []
+        for index, field, t, nullAllowed, default, key in colData:
+            if field == 'id':
+                continue
+            colClass, kw = self.guessClass(t)
+            kw['name'] = soClass.sqlmeta.style.dbColumnToPythonAttr(field)
+            kw['dbName'] = field
+            kw['notNone'] = not nullAllowed
+            kw['default'] = default
+            # @@ skip key...
+            # @@ skip extra...
+            results.append(colClass(**kw))
+        return results
+
+    def _columnsFromSchemaParse(self, tableName, soClass):
+        colData = self.queryOne("SELECT sql FROM sqlite_master WHERE type='table' AND name='%s'"
+                                % tableName)
+        if not colData:
+            raise ValueError('The table %s ws not found in the database. Load failed.' % tableName)
+        colData = colData[0].split('(', 1)[1].strip()[:-2]
+        while colData.find('(') > -1:
+            st = colData.find('(')
+            en = colData.find(')')
+            colData = colData[:st] + colData[en+1:]
+        results = []
+        for colDesc in colData.split(','):
+            parts = colDesc.strip().split(' ', 2)
+            field = parts[0].strip()
+            # skip comments
+            if field.startswith('--'):
+                continue
+            # get rid of enclosing quotes
+            if field[0] == field[-1] == '"':
+                field = field[1:-1]
+            if field == getattr(soClass.sqlmeta, 'idName', 'id'):
+                continue
+            colClass, kw = self.guessClass(parts[1].strip())
+            if len(parts) == 2:
+                index_info = ''
+            else:
+                index_info = parts[2].strip().upper()
+            kw['name'] = soClass.sqlmeta.style.dbColumnToPythonAttr(field)
+            import re
+            nullble = re.search(r'(\b\S*)\sNULL', index_info)
+            default = re.search(r"DEFAULT\s((?:\d[\dA-FX.]*)|(?:'[^']*')|(?:#[^#]*#))", index_info)
+            kw['notNone'] = nullble and nullble.group(1) == 'NOT'
+            kw['default'] = default and default.group(1)
+            # @@ skip key...
+            # @@ skip extra...
+            results.append(colClass(**kw))
+        return results
+
+    def guessClass(self, t):
+        t = t.upper()
+        if t.find('INT') > 0:
+            return col.IntCol, {}
+        elif t.find('TEXT') > 0 or t.find('CHAR') > 0 or t.find('CLOB') > 0:
+            return col.StringCol, {'length': 2**32-1}
+        elif t.find('BLOB') > 0:
+            return col.BLOBCol, {"length": 2**32-1}
+        elif t.find('REAL') > 0 or t.find('FLOAT') > 0:
+            return col.FloatCol, {}
+        elif t.find('DECIMAL') > 0:
+            return col.DecimalCol, {}
+        else:
+            return col.Col, {}
+
+    def dropDatabase(self, dbname, ifExists=False):
+        if os.path.exists(dbname):
+            os.unlink(dbname)
+
+    def createDatabase(self, dbname):
+        pass
+
+def stop_pysqlite2_converting_strings(s):
     return s
