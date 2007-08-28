f9c086117cd93b26ede98f15a8ff123acd443f08
george <None>
2007-08-28 21:40:25 +0000

    #2258: Implementar diálogo de transferência de estoque. r=jdahlin

--- data/sql/schema-2.sql
+++ data/sql/schema-2.sql
@@ -1028,6 +1028,30 @@ CREATE TABLE commission (
     payment_id bigint NOT NULL UNIQUE REFERENCES payment(id)
 );
 
+CREATE TABLE transfer_order (
+    id serial NOT NULL PRIMARY KEY,
+    te_created_id bigint REFERENCES transaction_entry(id),
+    te_modified_id bigint REFERENCES transaction_entry(id),
+
+    open_date timestamp NOT NULL,
+    receival_date timestamp,
+
+    source_branch_id bigint NOT NULL REFERENCES person_adapt_to_branch(id),
+    destination_branch_id bigint NOT NULL REFERENCES person_adapt_to_branch(id),
+    source_responsible_id bigint NOT NULL REFERENCES person_adapt_to_employee(id),
+    destination_responsible_id bigint NOT NULL REFERENCES person_adapt_to_employee(id)
+);
+
+CREATE TABLE transfer_order_item (
+    id serial NOT NULL PRIMARY KEY,
+    te_created_id bigint REFERENCES transaction_entry(id),
+    te_modified_id bigint REFERENCES transaction_entry(id),
+
+    sellable_id bigint NOT NULL REFERENCES asellable(id),
+    transfer_order_id bigint NOT NULL REFERENCES transfer_order(id),
+    quantity numeric(10, 2) NOT NULL CONSTRAINT positive_quantity CHECK (quantity > 0)
+);
+
 ALTER TABLE transaction_entry
     ADD CONSTRAINT transaction_entry_user_id_fkey FOREIGN KEY
         (user_id) REFERENCES person_adapt_to_user (id),
---
+++ stoqlib/domain/transfer.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2007 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+##  Author(s):      George Kussumoto    <george@async.com.br>
+##
+"""  Product transfer management """
+
+import datetime
+from kiwi.argcheck import argcheck
+from zope.interface import implements
+
+from sqlobject import ForeignKey
+from sqlobject.col import DateTimeCol
+
+from stoqlib.database.columns import DecimalCol
+from stoqlib.domain.base import Domain
+from stoqlib.domain.interfaces import IContainer
+from stoqlib.lib.translation import stoqlib_gettext
+
+
+_ = stoqlib_gettext
+
+
+class TransferOrderItem(Domain):
+    """Transfer order item
+
+    @ivar sellable: The sellable to transfer
+    @ivar transfer_order: The order this item belongs to
+    @ivar quantity: The quantity to transfer
+    """
+    sellable = ForeignKey('ASellable')
+    transfer_order = ForeignKey('TransferOrder')
+    quantity = DecimalCol()
+
+
+class TransferOrder(Domain):
+    """ Transfer Order class
+
+    @ivar open_date: The date the order was created
+    @ivar receival_date: The date the order was received
+    @ivar source_branch: The branch sending the stock
+    @ivar destination_branch: The branch receiving the stock
+    @ivar source_responsible: Employee responsible for the transfer at
+        source branch
+     @ivar destination_responsible: Employee responsible for the transfer at
+        destination branch
+    """
+    implements(IContainer)
+
+    open_date = DateTimeCol(default=datetime.datetime.now)
+    receival_date = DateTimeCol(default=datetime.datetime.now)
+    source_branch = ForeignKey('PersonAdaptToBranch')
+    destination_branch = ForeignKey('PersonAdaptToBranch')
+    source_responsible = ForeignKey('PersonAdaptToEmployee')
+    destination_responsible = ForeignKey('PersonAdaptToEmployee')
+
+    #
+    # IContainer implementation
+    #
+
+    def get_items(self):
+        return TransferOrderItem.selectBy(transfer_order=self,
+                                          connection=self.get_connection())
+
+    def add_item(self):
+        pass
+
+    @argcheck(TransferOrderItem)
+    def remove_item(self, item):
+        if item.transfer_order is not self:
+            raise ValueError('The item does not belong to this '
+                             'transfer order')
+        TransferOrderItem.delete(item.id,
+                                 connection=self.get_connection())
