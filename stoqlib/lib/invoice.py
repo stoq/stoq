2b83789ceb94fba87bf6a7e764ca0f3ef0e7aea2
jdahlin <None>
2007-10-31 11:17:24 +0000

    Add support for page breaking for invoices. Also use the pre defined layout for the tests. Make sure that the test creates enough fields to test multiple pages

--- stoqlib/lib/invoice.py
+++ stoqlib/lib/invoice.py
@@ -37,90 +37,268 @@ from stoqlib.lib.parameters import sysparam
 from stoqlib.lib.translation import stoqlib_gettext as _
 
 
-ENABLED = "X"
-INVOICE_TYPE_IN  = 1
-INVOICE_TYPE_OUT = 2
+def splititers(iterator, size):
+    while iterator:
+        yield iterator[:size]
+        iterator = iterator[size:]
 
-class SaleInvoice(object):
-    def __init__(self, sale, layout):
-        """Creates a new sale invoice
-        @param sale: sale to print an invoice for
-        @param layout: the invoice layout to use
+
+class InvoicePage(object):
+    """This represent a page part of an invoice"""
+    def __init__(self, width, height):
         """
-        self.sale = sale
+        @param width: the width of the page
+        @param height: the height of the page
+        """
+        self._data = []
+
+        for lines in range(height):
+            self._data.append(
+                array.array('c', (' ' * width) + '\n'))
+        self.width = width
+        self.height = height
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def _put(self, x, y, width, data):
+        if type(data) != str:
+            raise AssertionError(type(data))
+        output = array.array('c', data.upper())
+        if y > len(self._data):
+            raise ValueError(
+                "maximum invoice layout is %d, got %d" % (
+                self.height, y))
+
+        row = self._data[y]
+        row[x:x+width] = output[:width]
+
+    def _add_boolean(self, x, y, width, data):
+        if data:
+            data = 'X'
+        else:
+            data = ' '
+        self._put(x, y, width, data)
+
+    def _add_integer(self, x, y, width, data):
+        if data is None:
+            data = ''
+        else:
+            data = '%*.2f' % (int(width), data)
+        self._put(x, y, width, data)
+
+    def _add_decimal(self, x, y, width, data):
+        if data is None:
+            data = ''
+        else:
+            data = '%*.2f' % (int(width), float(data))
+        self._put(x, y, width, data)
+
+    def _add_string(self, x, y, width, data):
+        if type(data) == unicode:
+            data = str(data)
+        if data is None:
+            data = ''
+        else:
+            data = '%-*s' % (int(width), data)
+        self._put(x, y, width, data)
+
+    def _add_list(self, data_type, x, y, width, height, data):
+        for y_offset, line in enumerate(data):
+            self.add(data_type[0], x, y + y_offset,
+                     width, height, line)
+
+    def add(self, data_type, x, y, width, height, data):
+        """
+        Adds a new field to the page
+        @param data_type: data type of the field
+        @param x: x position of the left side of the field
+        @param y: y position of the upper side of the field
+        @param width: width of the field
+        @param height: height of the field
+        @param data: data to be printed at the field. This is dependent of
+          the data type
+        """
+        if data_type == bool:
+            self._add_boolean(x, y, width, data)
+        elif data_type == str:
+            self._add_string(x, y, width, data)
+        elif data_type == Decimal:
+            self._add_decimal(x, y, width, data)
+        elif data_type == int:
+            self._add_integer(x, y, width, data)
+        elif type(data_type) == list:
+            self._add_list(data_type, x, y, width, height, data)
+        else:
+            raise AssertionError(
+                "unsupported field type: %s" % (data_type,))
+
+
+class _Invoice(object):
+    def __init__(self, layout):
         self.layout = layout
-        self.type = _('Sale')
-        self.today = datetime.datetime.today()
+        self.header_fields = []
+        self.list_fields = []
+        self.footer_fields = []
 
-    def generate(self):
-        """Formats the data from the sale according to the fields specified
-        in the layout
-        @returns: lines printed
-        @rtype: a list of array
+        self._arrange_fields()
+
+    def _arrange_fields(self):
+        """Arrange all the fields.
+        We have three field types internally, headers, list and footers.
+        The header iterms should appear on all pages
+        The list items should be split up and separated by page
+        Footer items should only appear on the last page
+
+        List items are items are invoice fields which has a list type
+        All items which has are located above a list item is a header item
+        and all item which are located belove is a footer item.
         """
-        printerdata = []
-        for lines in range(self.layout.height):
-            printerdata.append(
-                array.array('c', (' ' * self.layout.width) + '\n'))
 
+        # First pass, figure out upper and lower bounds of the list area,
+        # eg the area which the list items cover.
+
+        top = self.layout.height
+        bottom = 0
         for field in self.layout.fields:
             invoice_field_class = get_invoice_field_by_name(field.field_name)
-            if invoice_field_class is None:
-                print 'WARNING: Could not find field %s' % (field.field_name,)
+            if not isinstance(invoice_field_class.field_type, list):
                 continue
-            invoice_field = invoice_field_class(self)
+            if field.y < top:
+                top = field.y
+            if (field.y + field.height) > bottom:
+                bottom = field.y + field.height
 
-            right = field.x + field.width
+        # Now when we know the interval of all the list items,
+        # Just arrange the items by their relative position to the
+        # list area:
+        #   above area  -> header
+        #   below area  -> footer
+        #   inside arae -> list
 
-            data = invoice_field.fetch()
-            if invoice_field.field_type == bool:
-                assert field.height == 1
-                if data:
-                    data = 'X'
-                else:
-                    data = ' '
-                printerdata[field.y][field.x:right] = array.array(
-                    'c', data.upper())
-            elif invoice_field.field_type == str:
-                if data is None:
-                    data = 'X' * field.width
-                data = str(data)
-                data = '%-*s' % (int(field.width),
-                                 data[:field.width])
-                output = array.array('c', data.upper())
-                printerdata[field.y][field.x:right] = output
-            elif invoice_field.field_type == iter:
-                for y, line in enumerate(data):
-                    if line is None:
-                        line = 'X' * field.width
-                    line = str(line)
-                    line = '%-*s' % (int(field.width), line[:field.width])
-
-                    output = array.array('c', line.upper())
-                    printerdata[field.y+y][field.x:right] = output
+        for field in self.layout.fields:
+            if field.y + field.height < top:
+                self.header_fields.append(field)
+            elif field.y > bottom:
+                self.footer_fields.append(field)
             else:
-                raise AssertionError(
-                    "unsupported field type: %s" % (
-                    invoice_field.field_type,))
-        return printerdata
+                self.list_fields.append(field)
+
+    def _fetch_data_by_field(self, field):
+        invoice_field_class = get_invoice_field_by_name(field.field_name)
+        if invoice_field_class is None:
+            print 'WARNING: Could not find field %s' % (field.field_name,)
+            return
+        invoice_field = invoice_field_class(self)
+        return invoice_field.fetch(), invoice_field.field_type
+
+    def _add_field(self, page, field):
+        data, field_type = self._fetch_data_by_field(field)
+
+        page.add(field_type,
+                 field.x, field.y,
+                 field.width, field.height,
+                 data)
+
+    def _fetch_list_fields(self):
+        """Fetch the list field data and seperate/sort it by page.
+        The dictionary returned is structured like this::
+          - key: page number
+          - value: field, field_type, lines for the page
+            - field comes from the database
+            - field_type is the type of the field
+            - lines is all the lines which should be printed on
+              the current page
+        @returns: a dictionary
+        """
+
+        # Fetch the data from all list fields
+        list_fields = []
+        for field in self.list_fields:
+            data, field_type = self._fetch_data_by_field(field)
+            assert isinstance(field_type, list)
+            list_fields.append((field, field_type, list(data)))
+
+        # Split up the data by page
+        page_list_fields = {}
+        for field, field_type, data in list_fields:
+            line_data = splititers(data, field.height)
+            for n, lines in enumerate(line_data):
+                if not n in page_list_fields:
+                    page_list_fields[n] = []
+                page_list_fields[n].append((field, field_type, lines))
+
+        return page_list_fields
+
+    def generate_pages(self):
+        """Formats the data from the sale according to the fields specified
+        in the layout
+        @returns: pages printed
+        @rtype: a list of pages
+        """
+
+        list_field_data = self._fetch_list_fields()
+
+        pages = []
+        for page_no in sorted(list_field_data):
+            list_fields = list_field_data[page_no]
+
+            page = InvoicePage(self.layout.width, self.layout.height)
+
+            # Header fields
+            for field in self.header_fields:
+                self._add_field(page, field)
+
+            # List fields
+            for field, field_type, lines in list_fields:
+                page.add(field_type,
+                         field.x, field.y,
+                         field.width, field.height,
+                         lines)
+            pages.append(page)
+
+        # Footer fields
+        last_page = pages[-1]
+        for field in self.footer_fields:
+            self._add_field(last_page, field)
+
+        return pages
 
     def send_to_printer(self, device):
+        """Send the printer invoice to the printer
+        @param device: device name of the printer
+        @type device: string
+        """
         try:
             printer = EscPPrinter(device)
         except IOError, e:
             warning(str(e))
             return
-        for line in self.generate():
-            printer.send(line.tostring())
-        printer.form_feed()
+        for page in self.generate_pages():
+            for line in page:
+                printer.send(line.tostring())
+            printer.form_feed()
         printer.done()
 
 
+class SaleInvoice(_Invoice):
+    def __init__(self, sale, layout):
+        """Creates a new sale invoice
+        @param sale: sale to print an invoice for
+        @param layout: the invoice layout to use
+        """
+        _Invoice.__init__(self, layout)
+        self.sale = sale
+        self.type = _('Sale')
+        self.today = datetime.datetime.today()
 
-class PurchaseInvoice(object):
-    def __init__(self, purchase):
+
+class PurchaseInvoice(_Invoice):
+    def __init__(self, purchase, layout):
+        _Invoice.__init__(self, layout)
         self.purchase = purchase
         self.type = _('Purchase')
+        self.today = datetime.datetime.today()
 
 
 class InvoiceFieldDescription(object):
@@ -170,7 +348,7 @@ class F(InvoiceFieldDescription):
     description = _("Outgoing invoice")
     length =  1
     def fetch(self):
-        return isinstance(self.sale, SaleInvoice)
+        return isinstance(self.invoice, SaleInvoice)
 
 _add_invoice_field(F)
 
@@ -181,7 +359,7 @@ class F(InvoiceFieldDescription):
     description = _("Incoming invoice")
     length =  1
     def fetch(self):
-        return isinstance(self.sale, PurchaseInvoice)
+        return isinstance(self.invoice, PurchaseInvoice)
 
 _add_invoice_field(F)
 
@@ -224,7 +402,7 @@ _add_invoice_field(F)
 
 class F(InvoiceFieldDescription):
     name = "CLIENT_DISTRICT"
-    description = _('District')
+    description = _("Client's district")
     length =  15
     def fetch(self):
         return self.sale.client.person.address.district
@@ -244,7 +422,7 @@ _add_invoice_field(F)
 
 class F(InvoiceFieldDescription):
     name = "CLIENT_CITY"
-    description = _("Client's City")
+    description = _("Client's city")
     length =  34
     def fetch(self):
         return self.sale.client.person.address.get_city()
@@ -331,7 +509,7 @@ class F(InvoiceFieldDescription):
     description = _('Creation time')
     length =  8
     def fetch(self):
-        return str(self.invoice.today.strftime('%H:%S:%M'))
+        return str(self.invoice.today.strftime('%H:%S'))
 
 _add_invoice_field(F)
 
@@ -373,6 +551,7 @@ _add_invoice_field(F)
 class F(InvoiceFieldDescription):
     name = "BASE_DE_CALCULO_ICMS"
     length =  1
+    field_type = Decimal
     def fetch(self):
         total = Decimal(0)
         for sale_item in self.sale.products:
@@ -388,6 +567,7 @@ _add_invoice_field(F)
 class F(InvoiceFieldDescription):
     name = "VALOR_ICMS"
     length =  1
+    field_type = Decimal
     def fetch(self):
         total = Decimal(0)
         for sale_item in self.sale.products:
@@ -402,6 +582,7 @@ _add_invoice_field(F)
 class F(InvoiceFieldDescription):
     name = "BASE_DE_CALCULO_ICMS_SUBST"
     length =  1
+    field_type = Decimal
     def fetch(self):
         total = Decimal(0)
         for sale_item in self.sale.products:
@@ -416,6 +597,7 @@ _add_invoice_field(F)
 class F(InvoiceFieldDescription):
     name = "VALOR_ICMS_SUBST"
     length =  1
+    field_type = Decimal
     def fetch(self):
         total = Decimal(0)
         tax_value = sysparam(self.conn).SUBSTITUTION_TAX
@@ -431,6 +613,7 @@ _add_invoice_field(F)
 class F(InvoiceFieldDescription):
     name = "VALOR_TOTAL_PRODUTOS"
     length =  1
+    field_type = Decimal
     def fetch(self):
         return self.sale.get_sale_subtotal()
 
@@ -476,6 +659,7 @@ _add_invoice_field(F)
 class F(InvoiceFieldDescription):
     name = "VALOR_TOTAL_NOTA"
     length =  1
+    field_type = Decimal
     def fetch(self):
         return self.sale.get_sale_subtotal()
 
@@ -486,7 +670,7 @@ class F(InvoiceFieldDescription):
     name = "ADDITIONAL_SALE_NOTES"
     lenght = 1
     def fetch(self):
-        return
+        return ''
 
 _add_invoice_field(F)
 
@@ -512,7 +696,7 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_COUNTER"
     description = _('Product item counter')
     length =  3
-    field_type = iter
+    field_type = [str]
     def fetch(self):
         for i in range(self.sale.products.count()):
             yield '%03d' % (i+1,)
@@ -524,7 +708,7 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_CODE_DESCRIPTION"
     description = _('Product item code / description')
     length =  35
-    field_type = iter
+    field_type = [str]
 
     def fetch(self):
         for sale_item in self.sale.products:
@@ -539,7 +723,7 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_CODE_SITUATION"
     description = _('Product item situation')
     length =  1
-    field_type = iter
+    field_type = [str]
 
     def fetch(self):
         for sale_item in self.sale.products:
@@ -552,7 +736,7 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_CODE_UNIT"
     description = _('Product item unit')
     length =  2
-    field_type = iter
+    field_type = [str]
 
     def fetch(self):
         for sale_item in self.sale.products:
@@ -565,7 +749,7 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_QUANTITY"
     description = _('Product item quantity')
     length =  5
-    field_type = iter
+    field_type = [Decimal]
 
     def fetch(self):
         for sale_item in self.sale.products:
@@ -578,7 +762,7 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_PRICE"
     description = _('Product item price')
     length =  5
-    field_type = iter
+    field_type = [Decimal]
 
     def fetch(self):
         for sale_item in self.sale.products:
@@ -591,7 +775,7 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_TOTAL"
     description = _('Product item total (price * quantity)')
     length =  7
-    field_type = iter
+    field_type = [Decimal]
 
     def fetch(self):
         for sale_item in self.sale.products:
@@ -604,15 +788,15 @@ class F(InvoiceFieldDescription):
     name = "PRODUCT_ITEM_TAX"
     description = _('Product item tax')
     length =  2
-    field_type = iter
+    field_type = [int]
 
     def fetch(self):
         for sale_item in self.sale.products:
             tax = sale_item.sellable.get_tax_constant()
             if tax and tax.tax_value:
-                value = '%02d' % (int(tax.tax_value),)
+                value = int(tax.tax_value)
             else:
-                value = ''
+                value = None
             yield value
 
 _add_invoice_field(F)
@@ -653,5 +837,5 @@ class F(InvoiceFieldDescription):
     name = "INSCR_ESTADUAL_SUBSTITUTO_TRIB"
     length = 4
     def fetch(self):
-        return
+        return ''
 _add_invoice_field(F)
