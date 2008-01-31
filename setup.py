3648f08836f021b1d967c14600c2711e35181b26
fabio <None>
2008-01-31 20:05:00 +0000

    Release 0.9.4

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ import sys
 #
 # Dependency checking
 #
-KIWI_REQUIRED = (1, 9, 19)
-STOQLIB_REQUIRED = '0.9.3'
+KIWI_REQUIRED = (1, 9, 20)
+STOQLIB_REQUIRED = '0.9.4'
 
 # kiwi is only here because we need to use it in setup.py itself,
 # the rest of the dependency checks should be done in stoqlib.
