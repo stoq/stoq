c3278f3e69b3eedce4a95589ae1e03afb2c4a7b7
george <None>
2008-03-04 21:05:50 +0000

    #3165: Usuario sem permissao de uso do sistema gera erro ao acessa-lo. r=jdahlin

--- stoq/gui/runner.py
+++ stoq/gui/runner.py
@@ -116,7 +116,7 @@ class ApplicationRunner(object):
         # sorting by app_full_name
         for name, full, icon, descr in sorted(descriptions,
                                               key=operator.itemgetter(1)):
-            if name in permissions:
+            if permissions[name]:
                 available_applications.append(
                     Application(name, full, icon, descr))
 
