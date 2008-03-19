deca9099b9950a13d11fdd950bb6f1f5e85c7e6a
george <None>
2008-03-19 18:57:48 +0000

    #3662: Alterar data de vencimento pode ser tambem a data atual. r=jdahlin

--- stoqlib/gui/dialogs/paymentchangedialog.py
+++ stoqlib/gui/dialogs/paymentchangedialog.py
@@ -78,6 +78,7 @@ class BasePaymentChangeDialog(BaseEditor):
     #
     # BaseEditor Hooks
     #
+
     def create_model(self, conn):
         return PaymentChangeHistory(payment=self._payment,
                                     connection=conn)
@@ -88,11 +89,21 @@ class BasePaymentChangeDialog(BaseEditor):
 
     def validate_confirm(self):
         if not self.model.change_reason:
-            msg = _(u'You can not change the due date without a reason!')
-            warning(msg)
+            msg = self.get_validate_message()
+            if bool(msg):
+                warning(msg)
             return False
         return True
 
+    #
+    # Public API
+    #
+
+    def get_validate_message(self):
+        """Defines a message to pop out to the user when the
+        validation fails.
+        """
+        pass
 
 
 class PaymentDueDateChangeDialog(BasePaymentChangeDialog):
@@ -102,20 +113,25 @@ class PaymentDueDateChangeDialog(BasePaymentChangeDialog):
 
     def _setup_widgets(self):
         BasePaymentChangeDialog._setup_widgets(self)
-
         self.status_box.hide()
-
         due_date_str = self._payment.due_date.strftime("%x")
         msg = _(u"Set current due date (%s) to:") % due_date_str
         self.due_date_lbl.set_text(msg)
 
     #
+    # BasePaymentChangeDialog
+    #
+
+    def get_validate_message(self):
+        return _(u'You can not change the due date without a reason!')
+
+    #
     # BaseEditor Hooks
     #
+
     def create_model(self, conn):
         model = BasePaymentChangeDialog.create_model(self, conn)
         model.last_due_date = self._payment.due_date
-
         return model
 
     def on_confirm(self):
@@ -125,9 +141,10 @@ class PaymentDueDateChangeDialog(BasePaymentChangeDialog):
     #
     # Kiwi callbacks
     #
+
     def on_due_date__validate(self, widget, value):
-        if value <= datetime.date.today():
-            msg = _(u"The due date must be set to a future date.")
+        if value < datetime.date.today():
+            msg = _(u"The due date must be set to today or a future date.")
             return ValidationError(msg)
 
 
@@ -157,3 +174,11 @@ class PaymentStatusChangeDialog(BasePaymentChangeDialog):
         change_entry = BasePaymentChangeDialog.create_model(self, conn)
         self._payment.set_not_paid(change_entry)
         return change_entry
+
+    #
+    # BasePaymentChangeDialog
+    #
+
+    def get_validate_message(self):
+        return _(u'You can not change the payment status without '
+                 'a reason!')
