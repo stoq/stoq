f0b8c692908fd8111cdaad24bc83f2d2715f4457
george <None>
2008-09-12 16:58:51 +0000

    #Fix: check null values before we add a not-null constraint in product_supplier_info table

--- data/sql/patch-46.sql
+++ data/sql/patch-46.sql
@@ -1,5 +1,8 @@
 -- #2244: Implementar wizard para cotação de compras
 
+-- Assert that we don't have null values before we add the constraint
+DELETE FROM product_supplier_info WHERE product_id IS NULL OR  supplier_id IS NULL;
+
 -- Add not null constraint to supplier_id and product_id
 ALTER TABLE product_supplier_info
     ALTER COLUMN supplier_id SET NOT NULL,
