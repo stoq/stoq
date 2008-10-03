848c7369587abd1a5f3e666f267bcd138467453b
jdahlin <None>
2008-10-03 18:54:50 +0000

    #2260: Implementar interface para cadastro de contas a pagar, r=romaia

--- stoqlib/gui/dialogs/paymentadditiondialog.py
+++ stoqlib/gui/dialogs/paymentadditiondialog.py
@@ -31,7 +31,9 @@ from kiwi.datatypes import currency, ValidationError
 
 from stoqlib.domain.interfaces import IInPayment, IOutPayment
 from stoqlib.domain.payment.category import PaymentCategory
+from stoqlib.domain.payment.group import PaymentGroup
 from stoqlib.domain.payment.payment import Payment
+from stoqlib.domain.person import PersonAdaptToClient, PersonAdaptToSupplier
 from stoqlib.gui.editors.baseeditor import BaseEditor
 from stoqlib.lib.translation import stoqlib_gettext
 
@@ -61,13 +63,14 @@ class BasePaymentAddition(BaseEditor):
     #
 
     def create_model(self, trans):
+        group = PaymentGroup(connection=trans)
         return Payment(open_date=datetime.date.today(),
                        description='',
                        value=currency(0),
                        base_value=currency(0),
                        due_date=None,
                        method=None,
-                       group=None,
+                       group=group,
                        till=None,
                        category=None,
                        destination=None,
@@ -77,11 +80,14 @@ class BasePaymentAddition(BaseEditor):
         self.confirm()
 
     def setup_proxies(self):
-        categories = PaymentCategory.select(connection=self.trans).orderBy('name')
+        categories = PaymentCategory.select(
+            connection=self.trans).orderBy('name')
         if categories:
             self.category.prefill([(c.name, c) for c in categories])
         else:
             self.category.set_sensitive(False)
+
+        self.populate_person()
         self.add_proxy(self.model, BasePaymentAddition.proxy_widgets)
 
     def validate_confirm(self):
@@ -93,6 +99,12 @@ class BasePaymentAddition(BaseEditor):
     def on_confirm(self):
         self.model.set_pending()
         self.model.base_value = self.model.value
+        person = self.person.get_selected()
+        print person
+        if person is not None:
+            setattr(self.model.group,
+                    self.person_attribute,
+                    person.person)
         return self.model
 
     #
@@ -105,26 +117,46 @@ class BasePaymentAddition(BaseEditor):
 
 
 class InPaymentAdditionDialog(BasePaymentAddition):
-
+    person_attribute = 'payer'
     def __init__(self, conn, model=None):
         """ This dialog is responsible to create additional payments with
         IInPayment facet.
 
         @param conn: a database connection
-        @param model: a L{stoqlib.domain.payment.payment.Payment} object or None
+        @param model: a L{stoqlib.domain.payment.payment.Payment} object
+                      or None
         """
         BasePaymentAddition.__init__(self, conn, model)
         self.model.addFacet(IInPayment, connection=self.conn)
+        self.person_label.set_label(_("Payer:"))
+
+    def populate_person(self):
+        clients = PersonAdaptToClient.get_active_clients(self.trans)
+        if clients:
+            self.person.prefill(sorted([(c.person.name, c)
+                                        for c in clients]))
+        else:
+            self.person.set_sensitive(False)
 
 
 class OutPaymentAdditionDialog(BasePaymentAddition):
-
+    person_attribute = 'recipient'
     def __init__(self, conn, model=None):
         """ This dialog is responsible to create additional payments with
         IOutPayment facet.
 
         @param conn: a database connection
-        @param model: a L{stoqlib.domain.payment.payment.Payment} object or None
+        @param model: a L{stoqlib.domain.payment.payment.Payment} object
+                      or None
         """
         BasePaymentAddition.__init__(self, conn, model)
         self.model.addFacet(IOutPayment, connection=self.conn)
+        self.person_label.set_label(_("Recipient:"))
+
+    def populate_person(self):
+        suppliers = PersonAdaptToSupplier.get_active_suppliers(self.trans)
+        if suppliers:
+            self.person.prefill(sorted([(s.person.name, s)
+                                        for s in suppliers]))
+        else:
+            self.person.set_sensitive(False)
