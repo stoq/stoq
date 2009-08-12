ea112be781a181ed650a96c4f64a42adcb83087d
georgeyk <georgeyk.dev@gmail.com>
2009-08-12 17:57:35 -0300

    Added "disable cookies" system parameter.

---
+++ data/sql/patch-01-26.sql
@@ -0,0 +1,4 @@
+-- 3468: As funções "gravar" e "limpar" cookies devem ser habilitadas nos parâmetros do sistema.
+
+-- This file avoid update erros, since we added a system parameter
+-- and we need to bump the database version to register this change.
