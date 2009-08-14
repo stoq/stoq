8540c2aa96c2cd0940ba08dc4ab2539092c70118
georgeyk <georgeyk.dev@gmail.com>
2009-08-14 17:36:01 -0300

    Added migration script to use te_modified user in the existing purchase orders.

--- data/sql/patch-01-27.sql
+++
@@ -1,4 +0,0 @@
--- #3964: Adicao de coluna "Conferido por" no dialogo de recebimento de compras de estoque.
-
-ALTER TABLE purchase_order
-    ADD COLUMN responsible_id bigint REFERENCES person_adapt_to_user(id);
