38ee241383ffefe6c2906d2d909b8eff5c1174c8
georgeyk <georgeyk.dev@gmail.com>
2009-09-04 16:17:51 -0300

    Added lost column in production_item table.

--- data/sql/patch-01-28.sql
+++ data/sql/patch-01-28.sql
@@ -3,3 +3,5 @@
 ALTER TABLE production_item ADD COLUMN produced numeric(10, 2) DEFAULT 0
     CONSTRAINT positive_produced CHECK (produced >= 0);
 
+ALTER TABLE production_item ADD COLUMN lost numeric(10, 2) DEFAULT 0
+    CONSTRAINT positive_lost CHECK (produced >= 0);
