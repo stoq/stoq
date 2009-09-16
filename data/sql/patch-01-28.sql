88fd80051344640bcf54c308d5241362155de225
georgeyk <georgeyk.dev@gmail.com>
2009-09-16 15:57:41 -0300

    Fix positive_lost constraint.

--- data/sql/patch-01-28.sql
+++ data/sql/patch-01-28.sql
@@ -4,4 +4,4 @@ ALTER TABLE production_item ADD COLUMN produced numeric(10, 2) DEFAULT 0
     CONSTRAINT positive_produced CHECK (produced >= 0);
 
 ALTER TABLE production_item ADD COLUMN lost numeric(10, 2) DEFAULT 0
-    CONSTRAINT positive_lost CHECK (produced >= 0);
+    CONSTRAINT positive_lost CHECK (lost >= 0);
