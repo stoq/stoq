2da63a898ae887e6df225b8d887c47632aa76214
georgeyk <georgeyk.dev@gmail.com>
2010-04-12 17:09:52 -0300

    Remove constraints for received/paid value, its possible to be negative when cancel payments.

--- data/sql/patch-01-33.sql
+++ data/sql/patch-01-33.sql
@@ -8,9 +8,9 @@ CREATE TABLE payment_flow_history (
     history_date timestamp,
 
     to_receive numeric(10, 2) CONSTRAINT positive_value_to_receive CHECK (to_receive >= 0),
-    received numeric(10, 2) CONSTRAINT positive_value_received CHECK (received >= 0),
+    received numeric(10, 2),
     to_pay numeric(10, 2) CONSTRAINT positive_value_to_pay CHECK (to_pay >= 0),
-    paid numeric(10, 2) CONSTRAINT positive_value_paid CHECK (paid >= 0),
+    paid numeric(10, 2),
 
     balance_expected numeric(10, 2),
     balance_real numeric(10, 2)
