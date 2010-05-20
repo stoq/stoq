f0d2336397810f1e3679064d6bc0468b3e4a11d4
georgeyk <georgeyk.dev@gmail.com>
2010-05-20 11:44:25 -0300

    Remove patch duplication (patch-01-35).

--- data/sql/patch-01-36.sql
+++ data/sql/patch-01-36.sql
@@ -1,3 +1,8 @@
--- Remove unique constraint of plugin_version column in installed_plugin table.
+-- Add consignment, width, height, depth and weight columns in product table.
 
-ALTER TABLE installed_plugin DROP CONSTRAINT installed_plugin_plugin_version_key;
+ALTER TABLE product
+    ADD COLUMN consignment boolean NOT NULL DEFAULT false,
+    ADD COLUMN width numeric(10, 2) CONSTRAINT positive_width CHECK (width >= 0),
+    ADD COLUMN height numeric(10, 2) CONSTRAINT positive_height CHECK (height >= 0),
+    ADD COLUMN depth numeric(10, 2) CONSTRAINT positive_depth CHECK (depth >= 0),
+    ADD COLUMN weight numeric(10, 2) CONSTRAINT positive_weight CHECK (weight >= 0);
