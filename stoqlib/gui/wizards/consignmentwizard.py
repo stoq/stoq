7bbfacda6440552076643f3a3b98aaf2931f0ef1
Ronaldo Maia <romaia@async.com.br>
2010-06-02 11:27:21 -0300

    start work on close consignment wizard, based on my previous try

--- stoqlib/gui/wizards/consignmentwizard.py
+++ stoqlib/gui/wizards/consignmentwizard.py
@@ -37,7 +37,8 @@ from kiwi.ui.widgets.list import Column
 
 from stoqlib.database.runtime import (get_current_branch, new_transaction,
                                       finish_transaction, get_current_user)
-from stoqlib.domain.interfaces import IBranch, ITransporter, ISupplier
+from stoqlib.domain.interfaces import (IBranch, ITransporter, ISupplier,
+                                       IStorable)
 from stoqlib.domain.payment.group import PaymentGroup
 from stoqlib.domain.payment.method import PaymentMethod
 from stoqlib.domain.payment.operation import register_payment_operations
@@ -47,6 +48,7 @@ from stoqlib.domain.purchase import PurchaseOrder, PurchaseItem
 from stoqlib.domain.receiving import (ReceivingOrder, ReceivingOrderItem,
                                       get_receiving_items_by_purchase_order)
 from stoqlib.domain.sellable import Sellable
+from stoqlib.domain.views import InConsignmentsView
 from stoqlib.lib.translation import stoqlib_gettext
 from stoqlib.lib.parameters import sysparam
 from stoqlib.lib.validators import format_quantity, get_formatted_cost
@@ -111,12 +113,6 @@ class ConsignmentItemStep(PurchaseItemStep):
         return ReceivingInvoiceStep(self.conn, self.wizard,
                                     self.wizard.receiving_model)
 
-#class ConsignmentPaymentStep(PurchasePaymentStep):
-#    pass
-#
-#class FinishConsignmentStep(FinishPurchaseStep):
-#    pass
-
 
 
 #
@@ -141,3 +137,294 @@ class ConsignmentWizard(PurchaseWizard):
         model = PurchaseWizard._create_model(self, conn)
         model.consignment = True
         return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from stoqlib.gui.base.wizards import WizardEditorStep, BaseWizard, BaseWizardStep
+from stoqlib.gui.base.lists import SimpleListDialog, AdditionListSlave
+
+
+
+
+class SupplierSelectionStep(BaseWizardStep):
+    gladefile = 'SupplierSelectionStep'
+
+    def __init__(self, wizard, conn):
+        BaseWizardStep.__init__(self, conn, wizard)
+        self._setup_slaves()
+
+    def _setup_slaves(self):
+        self.slave = AdditionListSlave(
+            self.conn, self._get_columns(),
+            klist_objects=self._get_supliers())
+        self.slave.hide_add_button()
+        self.slave.hide_edit_button()
+        self.slave.hide_del_button()
+        self.attach_slave('place_holder', self.slave)
+        self.slave.klist.set_selection_mode(gtk.SELECTION_BROWSE)
+        self.slave.klist.connect('selection-changed',
+                        self._on_results__selection_changed)
+
+        current_branch = get_current_branch(self.conn)
+        branches = [(b.person.name, b)
+                    for b in Person.iselect(IBranch, connection=self.conn)]
+        self.branch.prefill(branches)
+        self.branch.set_text(current_branch.person.name)
+
+
+    def _get_supliers(self):
+        branch = self.branch.get_selected()
+        if not branch:
+            return []
+
+        items = []
+        #query = InConsignment.q.status ==  InConsignment.CONSIGNMENT_CONFIRMED
+        query = None
+        items = InConsignmentsView.select_by_branch(
+                                    query,
+                                    branch = branch,
+                                    connection=self.conn)
+        return items
+
+    def _get_columns(self):
+        return [Column('supplier_name', title=_('Supplier'), data_type=str,
+                        expand=True),
+                Column('open_consignments', title=_('Open Consignments'),
+                        data_type=int),
+                ]
+
+    def _update_results(self):
+        self.slave.klist.clear()
+        self.slave.klist.add_list(self._get_supliers())
+
+    def _update_view(self):
+        selected = self.slave.klist.get_selected()
+        has_selected = selected is not None
+        self.wizard.refresh_next(has_selected)
+
+    ##
+    ## WizardStep hooks
+    ##
+
+    def next_step(self):
+        selected = self.slave.klist.get_selected()
+        supplier = selected.supplier
+        branch = self.branch.get_selected()
+
+        return ItemSelectionStep(self.wizard, self, self.conn,
+                                 supplier, branch, selected.consignments)
+
+    ##
+    ## Callbacks
+    ##
+
+    def _on_results__selection_changed(self, widget, item):
+        self._update_view()
+
+    def on_branch__content_changed(self, *args):
+        self._update_results()
+
+
+class _InConsignmentItem(object):
+    def __init__(self, item):
+        self.item = item
+        self.order = item.order.get_order_number_str()
+        self.id = item.id
+        self.code = item.sellable.code
+        self.description = item.sellable.get_description()
+        self.consigned = item.quantity_received
+        self.cost = item.cost
+        self.sellable = item.sellable
+
+        self.branch = item.order.branch
+
+        storable = IStorable(item.sellable.product)
+        self.stock = storable.get_stock_item(self.branch).quantity
+        self._sold = -1
+        self.sold_total = 0
+
+    def get_sold(self):
+        return self._sold
+
+    def set_sold(self, qtd):
+        self._sold = qtd
+        if qtd < 0:
+            self.sold_total = 0
+        else:
+            self.sold_total = currency(self._sold * float(self.cost))
+
+    sold = property(get_sold, set_sold)
+
+
+
+
+class ItemSelectionStep(BaseWizardStep):
+    gladefile = 'ItemSelectionStep'
+
+    def __init__(self, wizard, previous, conn, supplier, branch, consignments):
+        self.supplier = supplier
+        self.branch = branch
+        self.consignments = consignments
+        BaseWizardStep.__init__(self, conn, wizard, previous)
+        self._setup_slaves()
+
+    def _setup_slaves(self):
+        self.slave = AdditionListSlave(
+            self.conn, self.get_columns(),
+            klist_objects=self.get_saved_items())
+        self.slave.hide_add_button()
+        self.slave.hide_edit_button()
+        self.slave.hide_del_button()
+        #self.slave.connect('on-edit-item', self._on_list_slave__edit_item)
+
+        self.attach_slave('place_holder', self.slave)
+
+    def get_saved_items(self):
+        for consig in self.consignments:
+            for i in consig.get_items():
+                yield _InConsignmentItem(i)
+
+    def get_columns(self):
+        return [
+            Column('order', title=_('Order'), width=60, data_type=str,
+                   sorted=True),
+            Column('code', title=_('Code'), width=70, data_type=int),
+            Column('description', title=_('Description'),
+                   data_type=str, expand=True, searchable=True),
+            Column('stock', title=_('Stock'), data_type=float, width=70,
+                   format_func=format_quantity),
+            Column('consigned', title=_('Consigned'), data_type=float, width=90,
+                   format_func=format_quantity),
+            Column('sold', title=_('Sold'), data_type=float, width=60,
+                   format_func=format_quantity, editable=True),
+            #Column('to_buy', title=_('Buy'), data_type=float,
+            #       format_func=format_quantity, width=70, editable=True),
+            Column('cost', title=_('Cost'), data_type=currency,
+                   format_func=get_formatted_cost, width=90),
+            Column('sold_total', title=_('Total Sold'), data_type=currency,
+                   width=90),
+            ]
+
+    def has_previous_step(self):
+        return True
+
+    def has_next_step(self):
+        return True
+
+    def next_step(self):
+        for item in self.slave.klist:
+            print item
+            pass
+
+        ## Now, we should create a new PurchaseOrder with the items we sold.
+        #order = self._create_order()
+        #for consig_item in self.slave.klist:
+        #    # add new sale item
+        #    item = order.add_item(consig_item.sellable, consig_item.sold)
+        #    item.cost = consig_item.avg_cost
+        #    order.receive_item(item, consig_item.sold)
+
+        #order.set_valid()
+
+        ## Close previous consignments
+        #for consig in self.consignments:
+        #    consig = self.conn.get(consig)
+        #    consig.purchase_order = order
+        #    consig.close()
+
+        ## Create a new one with the remaining items
+        #if self.create_new_consignment.get_active():
+        #    print 'Creating new consignment'
+
+        ## Next step
+        #return CloseConsignmentPaymentStep(self.wizard, self, self.conn, order)
+
+
+class CloseConsignmentPaymentStep(PurchasePaymentStep):
+
+    def _create_receiving_order(self):
+        # since we will create a new receiving order, we should confirm the
+        # purchase first.
+        self.order.confirm()
+
+        receiving_model = ReceivingOrder(
+            responsible=get_current_user(self.conn),
+            purchase=self.order,
+            supplier=self.order.supplier,
+            branch=self.order.branch,
+            transporter=self.order.transporter,
+            invoice_number=None,
+            connection=self.conn)
+
+        # Creates ReceivingOrderItem's
+        get_receiving_items_by_purchase_order(self.order, receiving_model)
+
+        self.wizard.receiving_model = receiving_model
+
+
+    def next_step(self):
+        # TODO: Create ReceivingOrder
+        #return FinishCloseConsignmentStep(self.wizard, self, self.conn,
+        #                                  self.order)
+
+        self._create_receiving_order()
+        return ReceivingInvoiceStep(self.conn, self.wizard,
+                                    self.wizard.receiving_model)
+
+class FinishCloseConsignmentStep(FinishPurchaseStep):
+
+    def __init__(self, wizard, previous, conn, model):
+        FinishPurchaseStep.__init__(self, wizard, previous, conn, model)
+        self.receive_now.hide()
+
+    def has_next_step(self):
+        return True
+
+    def next_step(self):
+        return ReceivingInvoiceStep(self.conn, self.wizard,
+                                    self.wizard.receiving_model)
+
+
+class CloseInConsignmentWizard(BaseWizard):
+    title = _('Closing In Consignment')
+    size = (790, 400)
+
+    def __init__(self, conn):
+        register_payment_operations()
+        self.receiving_model = None
+        first_step = SupplierSelectionStep(self, conn)
+        BaseWizard.__init__(self, conn, first_step, None)
+        self.next_button.set_sensitive(False)
+
+
+
+    #
+    # WizardStep hooks
+    #
+
+    def finish(self):
+        #if not self.receiving_model.get_valid():
+        #    self.receiving_model.set_valid()
+        #self.receiving_model.confirm(already_received=True)
+
+        self.retval = False
+        self.close()
+
