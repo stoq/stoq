070fd3aa9a5d89cd3061c205f0c3cc5a70609dbc
Gabriel Gerga <gergagabriel@gmail.com>
2010-07-16 16:16:09 -0300

    Adicionados arqivos para edição de categorias de clientes

---
+++ stoqlib/gui/dialogs/clientcategorydialog.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2010 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s):   Gabriel Gerga    <gerga@async.com.br>
+##
+##
+"""Dialog for listing client categories"""
+
+import gtk
+from kiwi.ui.objectlist import Column
+
+from stoqlib.domain.person import ClientCategory, PersonAdaptToClient
+from stoqlib.gui.base.gtkadds import render_pixbuf
+from stoqlib.gui.base.lists import ModelListDialog
+from stoqlib.gui.editors.clientcategoryeditor import ClientCategoryEditor
+from stoqlib.lib.translation import stoqlib_gettext
+
+_ = stoqlib_gettext
+
+
+class ClientCategoryDialog(ModelListDialog):
+
+    #ModelListDialog
+    model_type = ClientCategory
+    editor_class = ClientCategoryEditor
+    title = _('Client categories')
+    size = (620, 300)
+
+    #ListDialog
+    columns = [
+        Column('name', title= ('Category'),
+                data_type=str, expand=True, sorted=True)
+        ]
+
+    def delete_model(self, model, trans):
+        for client in PersonAdaptToClient.selectBy(category=model,
+                                                   connection=trans):
+            client.category = None
+        ClientCategory.delete(model.id, connection=trans)
