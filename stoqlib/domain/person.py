dbdf4bc60806b9d901792ad4938b9361fe79e0c0
Gabriel Gerga <gergagabriel@gmail.com>
2010-07-16 14:01:28 -0300

    Adicionando novas classes de dominio e patch de migração do bd

--- stoqlib/domain/person.py
+++ stoqlib/domain/person.py
@@ -484,6 +484,23 @@ class PersonAdaptToCompany(PersonAdapter):
 
 Person.registerFacet(PersonAdaptToCompany, ICompany)
 
+class ClientCategory(Domain):
+    """I am a client category.
+    I contain a name
+    @ivar name: category name
+    """
+
+    implements(IDescribable)
+
+    name = UnicodeCol(unique=True)
+
+    #
+    # IDescribable implementation
+    #
+
+    def get_description(self):
+        return self.name
+
 class PersonAdaptToClient(PersonAdapter):
     """A client facet of a person."""
 
@@ -502,6 +519,7 @@ class PersonAdaptToClient(PersonAdapter):
     status = IntCol(default=STATUS_SOLVENT)
     days_late = IntCol(default=0)
     credit_limit = PriceCol(default=0)
+    category = ForeignKey('ClientCategory')
 
     #
     # IActive implementation
