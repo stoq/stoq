e58771e1fb9153194ca6255874084cb1417d463a
Ronaldo Maia <romaia@async.com.br>
2010-08-30 15:11:25 -0300

    Add some sleep time before emiting some documents, since the printers stop responding while they are printing something else

--- stoqlib/gui/editors/tilleditor.py
+++ stoqlib/gui/editors/tilleditor.py
@@ -27,6 +27,7 @@
 """ Editors implementation for open/close operation on till operation"""
 
 import datetime
+import time
 
 from kiwi import ValueUnset
 from kiwi.datatypes import ValidationError, currency
@@ -129,6 +130,9 @@ class TillOpeningEditor(BaseEditor):
 
         value = self.proxy.model.value
         if value:
+            # XXX: this is so ugly, but the printer stops responding while
+            # it is printing something. We should wait a little bit...
+            time.sleep(4)
             TillAddCashEvent.emit(till=till, value=value)
             till.add_credit_entry(value,
                             (_(u'Initial Cash amount of %s')
@@ -239,6 +243,9 @@ class TillClosingEditor(BaseEditor):
             till.add_debit_entry(removed,
                                  _(u'Amount removed from Till on %s' %
                                    till.opening_date.strftime('%x')))
+            # XXX: this is so ugly, but the printer stops responding while
+            # it is printing something. We should wait a little bit...
+            time.sleep(4)
 
         try:
             retval = TillCloseEvent.emit(till=till,
