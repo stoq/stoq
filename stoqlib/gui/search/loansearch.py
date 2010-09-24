e9ae216fcb552f2671fbe1fb4698288527b0ead4
Ronaldo Maia <romaia@async.com.br>
2010-09-24 17:57:41 -0300

    Adding loan search, details and receipt

--- stoqlib/gui/search/loansearch.py
+++ stoqlib/gui/search/loansearch.py
@@ -26,15 +26,20 @@
 import datetime
 from decimal import Decimal
 
+import gtk
 from kiwi.datatypes import currency
 from kiwi.enums import SearchFilterPosition
-from kiwi.ui.objectlist import SearchColumn
-from kiwi.ui.search import ComboSearchFilter
+from kiwi.ui.objectlist import SearchColumn, Column
+from kiwi.ui.search import ComboSearchFilter, DateSearchFilter
 
 from stoqlib.domain.loan import Loan
 from stoqlib.domain.views import LoanItemView
+from stoqlib.gui.base.dialogs import run_dialog
 from stoqlib.gui.base.search import SearchDialog
+from stoqlib.gui.dialogs.loandetails import LoanDetailsDialog
+from stoqlib.gui.printing import print_report
 from stoqlib.lib.translation import stoqlib_gettext
+from stoqlib.reporting.loanreceipt import LoanReceipt
 
 _ = stoqlib_gettext
 
@@ -79,3 +84,78 @@ class LoanItemSearch(SearchDialog):
                              data_type=currency),
                 SearchColumn('total', title=_(u'Total'),
                              data_type=currency),]
+
+class LoanSearch(SearchDialog):
+    title = _(u"Loan Search")
+    size = (750, 500)
+    search_table = Loan
+    selection_mode = gtk.SELECTION_MULTIPLE
+    searchbar_result_strings = _(u"loan"), _(u"loans")
+    search_by_date = True
+    advanced_search = False
+
+    def __init__(self, conn):
+        SearchDialog.__init__(self, conn, self.search_table,
+                              title=self.title)
+        self._setup_widgets()
+
+    def _show_details(self, item):
+        run_dialog(LoanDetailsDialog, self, self.conn,
+                   item)
+
+    def _setup_widgets(self):
+        self.results.connect('row_activated', self.on_row_activated)
+        self.update_widgets()
+
+    #
+    # SearchDialog Hooks
+    #
+
+    def update_widgets(self):
+        orders = self.results.get_selected_rows()
+        has_one_selected = len(orders) == 1
+        self.set_details_button_sensitive(has_one_selected)
+        self.set_print_button_sensitive(has_one_selected)
+
+    def _has_rows(self, results, obj):
+        pass
+
+    def create_filters(self):
+        self.set_text_field_columns([])
+        self.set_searchbar_labels(_('matching:'))
+
+        # Date
+        date_filter = DateSearchFilter(_('Date:'))
+        self.add_filter(date_filter, columns=['expire_date', 'open_date'])
+
+    def get_columns(self):
+        return [Column('id', _('#'), data_type=int, width=50),
+                Column('open_date', _('Open Date'),
+                       data_type=datetime.date, sorted=True, width=100),
+                Column('expire_date', _('Expire Date'),
+                       data_type=datetime.date, width=100),
+                Column('branch_name', _('Branch'),
+                       data_type=unicode, expand=True),
+                Column('client_name', _('Client'),
+                       data_type=unicode, width=120),
+                       ]
+
+    #
+    # Callbacks
+    #
+
+    def on_row_activated(self, klist, item):
+        self._show_details(item)
+
+    def on_print_button_clicked(self, button):
+        orders = self.results.get_selected_rows()
+        if len(orders) == 1:
+            print_report(LoanReceipt, orders[0] )
+            pass
+
+    def on_details_button_clicked(self, button):
+        orders = self.results.get_selected_rows()
+        if len(orders) > 1:
+            raise ValueError("You should have only one item selected at "
+                             "this point ")
+        self._show_details(orders[0])
