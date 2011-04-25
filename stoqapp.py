475554d576e2b8c78af20bc6a4090b49022a7f65
jdahlin@async.com.br <None>
2011-04-25 23:32:33 +1000

    Create logdir and avoid importing stoqlib

--- stoqapp.py
+++ stoqapp.py
@@ -4,12 +4,12 @@ import sys
 
 os.environ['LANGUAGE'] = 'pt_BR.UTF-8'
 
-from stoqlib.lib.osutils import get_application_dir
-stoqdir = get_application_dir()
-
+logdir = os.path.join(os.environ['APPDATA'], "stoq", "logs")
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
 # http://www.py2exe.org/index.cgi/StderrLog
 for name in ['stdout', 'stderr']:
-    filename = os.path.join(stoqdir, "logs", name + ".log")
+    filename = os.path.join(logdir, name + ".log")
     try:
         fp = open(filename, "w")
     except IOError, e:
