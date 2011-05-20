fc68f41cd05440ee45f4c9d8c2fc08a76c833cb8
jdahlin@async.com.br <None>
2011-05-20 10:52:15 -0300

    Register more stock icons
    Also register at different sizes

--- stoqlib/gui/base/gtkadds.py
+++ stoqlib/gui/base/gtkadds.py
@@ -26,43 +26,109 @@
 import gtk
 from kiwi.environ import environ
 
+
+# 16: GTK_ICON_SIZE_MENU
+# 18: GTK_ICON_SIZE_SMALL_TOOLBAR
+# 20: GTK_ICON_SIZE_BUTTON
+# 24: GTK_ICON_SIZE_LARGE_TOOLBAR
+# 32: GTK_ICON_SIZE_DND
+# 48: GTK_ICON_SIZE_DIALOG
+
 # register stoq stock icons
 def register_iconsets():
-    icon_info = [("stoq-searchtool-icon1", "searchtool-animation1.png"),
-                 ("stoq-searchtool-icon2", "searchtool-animation2.png"),
-                 ("stoq-searchtool-icon3", "searchtool-animation3.png"),
-                 ("stoq-searchtool-icon4", "searchtool-animation4.png"),
-                 ("stoq-money", "money24px.png"),
-                 ("stoq-products", "products_24.png"),
-                 ("stoq-purchase-quote", "gnome-gnomine24px.png"),
-                 ("stoq-deliveries", "delivery24px.png"),
-                 ("stoq-suppliers", "kuser48px.png"),
-                 ("stoq-bills", "gnome-money48px.png"),
-                 ("stoq-conference", "gtk-stock-book48px.png"),
-                 ("stoq-receiving", "emblem-documentation48px.png"),
-                 ("stoq-transfer", "a-convert48px.png"),
-                 ("stoq-clients", "config-users48px.png"),
-                 ("stoq-services", "gconf-editor48px.png"),
-                 ("stoq-delivery", "delivery24px.png"),
-                 ("stoq-admin-app", "admin_24.png"),
-                 ("stoq-pos-app", "pos_24.png"),
-                 ("stoq-till-app", "till_24.png"),
-                 ("stoq-stock-app", "warehouse_24.png"),
-                 ("stoq-inventory-app", "inventory_24.png"),
-                 ("stoq-purchase-app", "gnome-gnomine48px.png"),
-                 ("stoq-sales-app", "gnome-log48px.png"),
-                 ("stoq-payable-app", "gnome-money-red48px.png"),
-                 ("stoq-production-app", "stoq-production-app.png"),]
+    icon_info = [
+        ("stoq-admin-app",
+         { gtk.ICON_SIZE_MENU: "admin_16.png",
+           gtk.ICON_SIZE_LARGE_TOOLBAR: "admin_24.png",
+           gtk.ICON_SIZE_DND: "admin_32.png",
+           gtk.ICON_SIZE_DIALOG: "admin_48.png" }),
+        ("stoq-bills",
+         { gtk.ICON_SIZE_DIALOG: "gnome-money48px.png" }),
+        ("stoq-clients",
+         { gtk.ICON_SIZE_DIALOG: "config-users48px.png" }),
+        ("stoq-conference",
+         { gtk.ICON_SIZE_DIALOG: "gtk-stock-book48px.png" }),
+        ("stoq-delivery",
+         { gtk.ICON_SIZE_LARGE_TOOLBAR: "delivery24px.png",
+           gtk.ICON_SIZE_DIALOG: "delivery48px.png" }),
+        ("stoq-deliveries",
+         { gtk.ICON_SIZE_LARGE_TOOLBAR: "delivery24px.png" }),
+        ("stoq-hr",
+         { gtk.ICON_SIZE_MENU: "hr_16.png",
+           gtk.ICON_SIZE_LARGE_TOOLBAR: "hr_24.png",
+           gtk.ICON_SIZE_DND: "hr_32.png",
+           gtk.ICON_SIZE_DIALOG: "hr_48.png" }),
+        ("stoq-inventory-app",
+         { gtk.ICON_SIZE_LARGE_TOOLBAR: "inventory_24.png" }),
+        ("stoq-money",
+         { gtk.ICON_SIZE_MENU: "money24px.png",
+           gtk.ICON_SIZE_DIALOG: "money.png" }),
+        ("stoq-payable-app",
+         { gtk.ICON_SIZE_DIALOG: "gnome-money-red48px.png" }),
+        ("stoq-pos-app",
+         { gtk.ICON_SIZE_MENU: "pos_16.png",
+           gtk.ICON_SIZE_LARGE_TOOLBAR: "pos_24.png",
+           gtk.ICON_SIZE_DND: "pos_32.png",
+           gtk.ICON_SIZE_DIALOG: "pos_48.png" }),
+        ("stoq-production-app",
+         { gtk.ICON_SIZE_DIALOG: "stoq-production-app.png" }),
+        ("stoq-products",
+         { gtk.ICON_SIZE_MENU: "products_16.png",
+           gtk.ICON_SIZE_LARGE_TOOLBAR: "products_24.png",
+           gtk.ICON_SIZE_DND: "products_32.png",
+           gtk.ICON_SIZE_DIALOG: "products_48.png" }),
+        ("stoq-purchase-app",
+         { gtk.ICON_SIZE_DIALOG: "gnome-gnomine48px.png" }),
+        ("stoq-purchase-quote",
+         { gtk.ICON_SIZE_LARGE_TOOLBAR: "gnome-gnomine24px.png" }),
+        ("stoq-receiving",
+         { gtk.ICON_SIZE_DIALOG: "emblem-documentation48px.png" }),
+        ("stoq-sales-app",
+         { gtk.ICON_SIZE_DIALOG: "gnome-log48px.png" }),
+        ("stoq-searchtool-icon1",
+         { gtk.ICON_SIZE_DIALOG:
+           "searchtool-animation1.png" }),
+        ("stoq-searchtool-icon2",
+         { gtk.ICON_SIZE_DIALOG:
+           "searchtool-animation2.png" }),
+        ("stoq-searchtool-icon3",
+         { gtk.ICON_SIZE_DIALOG:
+         "searchtool-animation3.png" }),
+        ("stoq-searchtool-icon4",
+         { gtk.ICON_SIZE_DIALOG:
+           "searchtool-animation4.png" }),
+        ("stoq-services",
+         { gtk.ICON_SIZE_DIALOG: "gconf-editor48px.png" }),
+        ("stoq-stock-app",
+         { gtk.ICON_SIZE_MENU: "warehouse_16.png",
+           gtk.ICON_SIZE_LARGE_TOOLBAR: "warehouse_24.png",
+           gtk.ICON_SIZE_DND: "warehouse_32.png",
+           gtk.ICON_SIZE_DIALOG: "warehouse_48.png" }),
+        ("stoq-suppliers",
+         { gtk.ICON_SIZE_DIALOG: "kuser48px.png" }),
+        ("stoq-till-app",
+         { gtk.ICON_SIZE_MENU: "till_16.png",
+           gtk.ICON_SIZE_LARGE_TOOLBAR: "till_24.png",
+           gtk.ICON_SIZE_DND: "till_32.png",
+           gtk.ICON_SIZE_DIALOG: "till_48.png" }),
+        ("stoq-transfer",
+         { gtk.ICON_SIZE_DIALOG: "a-convert48px.png" }),
+        ]
 
     iconfactory = gtk.IconFactory()
     stock_ids = gtk.stock_list_ids()
-    for stock_id, filename in icon_info:
+    for stock_id, arg in icon_info:
         # only load image files when our stock_id is not present
-        if stock_id not in stock_ids:
+        if stock_id in stock_ids:
+            continue
+        iconset = gtk.IconSet()
+        for size, filename in arg.items():
+            iconsource = gtk.IconSource()
             filename = environ.find_resource('pixmaps', filename)
-            pixbuf = gtk.gdk.pixbuf_new_from_file(filename)
-            iconset = gtk.IconSet(pixbuf)
-            iconfactory.add(stock_id, iconset)
+            iconsource.set_filename(filename)
+            iconsource.set_size(size)
+            iconset.add_source(iconsource)
+        iconfactory.add(stock_id, iconset)
     iconfactory.add_default()
 
 def change_toolbar_button_appearance(item, icon=None, text=None):
