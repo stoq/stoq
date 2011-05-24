745217814510362ceffc58a8a45499917559640a
Ronaldo Maia <romaia@async.com.br>
2011-05-24 16:37:37 -0300

    Fix a problem with patch 02-29: When new columns should be NOT NULL, we must set a default value first

--- data/sql/patch-02-29.sql
+++ data/sql/patch-02-29.sql
@@ -2,9 +2,12 @@
 
 ALTER TABLE payment DROP COLUMN destination_id;
 ALTER TABLE payment_method DROP COLUMN destination_id;
-DROP TABLE credit_provider_group_data;
-DROP TABLE payment_method_details;
-DROP TABLE payment_destination;
+DROP TABLE IF EXISTS credit_provider_group_data;
+DROP TABLE IF EXISTS payment_method_details;
+DROP TABLE IF EXISTS payment_destination;
 
 ALTER TABLE payment_method ADD COLUMN destination_account_id
-  bigint NOT NULL REFERENCES account(id);
+  bigint REFERENCES account(id);
+UPDATE payment_method SET destination_account_id = (SELECT field_value::int
+    FROM parameter_data WHERE field_name = 'IMBALANCE_ACCOUNT');
+ALTER TABLE payment_method ALTER COLUMN destination_account_id SET NOT NULL;
