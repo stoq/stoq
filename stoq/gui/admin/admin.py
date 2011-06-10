62ec821d1eaf03ab1ca707f5bbb2ed583e127003
jdahlin@async.com.br <None>
2011-06-10 10:33:53 -0300

    Refactor admin app
    - Use one path for all callbacks (menu and iconview)
    - Make the callbacks work in portuguese
    - Make callbacks private
    - Add a logger

--- stoq/gui/admin/admin.py
+++ stoq/gui/admin/admin.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 
 ##
-## Copyright (C) 2005-2007 Async Open Source <http://www.async.com.br>
+## Copyright (C) 2005-2011 Async Open Source <http://www.async.com.br>
 ## All rights reserved
 ##
 ## This program is free software; you can redistribute it and/or modify
@@ -29,6 +29,7 @@ import glib
 import pango
 import gtk
 from kiwi.enums import SearchFilterPosition
+from kiwi.log import Logger
 from kiwi.ui.objectlist import Column, SearchColumn
 from kiwi.ui.search import ComboSearchFilter
 
@@ -66,10 +67,11 @@ from stoq.gui.application import AppWindow, VersionChecker
 
 _ = gettext.gettext
 
+logger = Logger('stoq.gui.admin')
 
-(COL_PATH,
- COL_PIXBUF) = range(2)
-
+(COL_LABEL,
+ COL_NAME,
+ COL_PIXBUF) = range(3)
 
 class Tasks(gtk.ScrolledWindow):
     def __init__(self, app):
@@ -78,12 +80,12 @@ class Tasks(gtk.ScrolledWindow):
         self.set_shadow_type(gtk.SHADOW_ETCHED_IN)
         self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 
-        self.model = gtk.ListStore(str, gtk.gdk.Pixbuf)
-        self.model.set_sort_column_id(COL_PATH, gtk.SORT_ASCENDING)
+        self.model = gtk.ListStore(str, str, gtk.gdk.Pixbuf)
+        self.model.set_sort_column_id(COL_LABEL, gtk.SORT_ASCENDING)
         iconView = gtk.IconView(self.model)
         iconView.set_selection_mode(gtk.SELECTION_MULTIPLE)
         iconView.connect('item-activated', self.on_item_activated)
-        iconView.set_text_column(COL_PATH)
+        iconView.set_text_column(COL_LABEL)
         iconView.set_pixbuf_column(COL_PIXBUF)
         iconView.set_item_padding(0)
 
@@ -93,82 +95,103 @@ class Tasks(gtk.ScrolledWindow):
 
     def add_defaults(self):
         self.model.clear()
-        items = [(_('Branches'), 'gtk-home'),
-                 (_('Client Categories'), 'stoq-clients'),
-                 (_('Clients'), 'stoq-clients'),
-                 (_('Cfop'), 'calc'),
-                 (_('Devices'), 'audio-card'),
-                 (_('Employee'), 'stoq-admin-app'),
-                 (_('Employee Roles'), 'stoq-users'),
-                 (_('FiscalBooks'), 'stoq-conference'),
-                 (_('Invoice Printers'), 'printer'),
-                 (_('Payment Categories'), 'stoq-payable-app'),
-                 (_('Payment Methods'), 'stoq-money'),
-                 (_('Parameters'), 'gtk-preferences'),
-                 (_('Plugins'), 'gtk-properties'),
-                 (_('Taxes'), 'calc'),
-                 (_('Stations'), 'system'),
-                 (_('Suppliers'), 'stoq-suppliers'),
-                 (_('Transporters'), 'stoq-delivery'),
-                 (_('Users'), 'stoq-hr'),
-                 (_('User Profiles'), 'config-users'),
+        items = [(_('Branches'), 'branches',
+                  'gtk-home'),
+                 (_('Client Categories'), 'client_categories',
+                  'stoq-clients'),
+                 (_('Clients'), 'clients',
+                  'stoq-clients'),
+                 (_('Cfop'), 'cfop',
+                  'calc'),
+                 (_('Devices'), 'devices',
+                  'audio-card'),
+                 (_('Employees'), 'employees',
+                  'stoq-admin-app'),
+                 (_('Employee Roles'), 'employee_roles',
+                  'stoq-users'),
+                 (_('Fiscal Books'), 'fiscal_books',
+                  'stoq-conference'),
+                 (_('Invoice Printers'), 'invoice_printers',
+                  'printer'),
+                 (_('Payment Categories'), 'payment_categories',
+                  'stoq-payable-app'),
+                 (_('Payment Methods'), 'payment_methods',
+                  'stoq-money'),
+                 (_('Parameters'), 'parameters',
+                  'gtk-preferences'),
+                 (_('Plugins'), 'plugins',
+                  'gtk-properties'),
+                 (_('Taxes'), 'taxes',
+                  'calc'),
+                 (_('Stations'), 'stations',
+                  'system'),
+                 (_('Suppliers'), 'suppliers',
+                  'stoq-suppliers'),
+                 (_('Transporters'), 'transporters',
+                  'stoq-delivery'),
+                 (_('Users'), 'users',
+                  'stoq-hr'),
+                 (_('User Profiles'), 'user_profiles',
+                  'config-users'),
                  ]
 
-        for item in items:
-            if len(item) == 2:
-                pixbuf = item[1]
-                item = item[0]
-            else:
-                pixbuf = None
-            self.add_item(item, pixbuf)
+        for label, name, pixbuf in items:
+            self.add_item(label, name, pixbuf)
 
-    def add_item(self, name, pixbuf=None, cb=None):
+    def add_item(self, label, name, pixbuf=None, cb=None):
         if type(pixbuf) == str:
             try:
                 pixbuf = self.theme.load_icon(pixbuf, 32, 0)
             except glib.GError, e:
                 pixbuf = self.render_icon(pixbuf, gtk.ICON_SIZE_DIALOG)
-        self.model.append([name, pixbuf])
+        self.model.append([label, name, pixbuf])
         if cb is not None:
             setattr(self,
-                    'open_' + name.replace(' ', '_'),
+                    '_open_' + name,
                     lambda : cb(None))
 
     def on_item_activated(self, icon_view, path):
-        name = self.model[path][0].replace(' ', '_')
+        name = self.model[path][COL_NAME]
+        self.run_task(name)
 
-        func = getattr(self, 'open_%s' % name, None)
+    def run_task(self, name):
+        func = getattr(self, '_open_%s' % name, None)
         if not func:
+            logger.info("Couldn't open dialog: %r" % (name, ))
             return
+        logger.info("Opening dialog: %r" % (name, ))
         func()
 
-    def open_Branches(self):
+    def _open_branches(self):
         self.app.run_dialog(BranchSearch, self.app.conn,
                             hide_footer=True)
 
-    def open_Clients(self):
+    def _open_clients(self):
         self.app.run_dialog(ClientSearch, self.app.conn)
 
-    def open_Client_Categories(self):
+    def _open_client_categories(self):
         trans = new_transaction()
         model = self.app.run_dialog(ClientCategoryDialog, trans)
         finish_transaction(trans, model)
         trans.close()
 
-    def open_Devices(self):
+    def _open_devices(self):
         self.app.run_dialog(DeviceSettingsDialog, self.app.conn)
 
-    def open_Employee_Roles(self):
-        self.app.run_dialog(EmployeeRoleSearch, self.app.conn)
-
-    def open_Employee(self):
+    def _open_employees(self):
         self.app.run_dialog(EmployeeSearch, self.app.conn)
 
-    def open_FiscalBooks(self):
+    def _open_employee_roles(self):
+        self.app.run_dialog(EmployeeRoleSearch, self.app.conn)
+
+    def _open_fiscal_books(self):
         self.app.run_dialog(FiscalBookEntrySearch, self.app.conn,
                             hide_footer=True)
 
-    def open_Invoice_Printers(self):
+    def _open_invoice_layouts(self):
+        self.app.run_dialog(InvoiceLayoutDialog, self.app.conn)
+
+    def _open_invoice_printers(self):
         if not InvoiceLayout.select(connection=self.app.conn):
             info(_(
                 "You must create at least one invoice layout before adding an"
@@ -177,40 +200,53 @@ class Tasks(gtk.ScrolledWindow):
 
         self.app.run_dialog(InvoicePrinterDialog, self.app.conn)
 
-    def open_Payment_Categories(self):
+    def _open_payment_categories(self):
         trans = new_transaction()
         model = self.app.run_dialog(PaymentCategoryDialog, trans)
         finish_transaction(trans, model)
         trans.close()
 
-    def open_Payment_Methods(self):
+    def _open_payment_methods(self):
         self.app.run_dialog(PaymentMethodsDialog, self.app.conn)
 
-    def open_Parameters(self):
+    def _open_parameters(self):
         self.app.run_dialog(ParameterSearch, self.app.conn)
 
-    def open_Plugins(self):
+    def _open_plugins(self):
         self.app.run_dialog(PluginManagerDialog, self.app.conn)
 
-    def open_Cfop(self):
+    def _open_cfop(self):
         self.app.run_dialog(CfopSearch, self.app.conn, hide_footer=True)
 
-    def open_Stations(self):
+    def _open_sintegra(self):
+        branch = get_current_branch(self.app.conn)
+        if branch.manager is None:
+            info(_(
+                "You must define a manager to this branch before you can create"
+                " a sintegra archive"))
+            return
+
+        self.app.run_dialog(SintegraDialog, self.app.conn)
+
+    def _open_stations(self):
         self.app.run_dialog(StationSearch, self.app.conn, hide_footer=True)
 
-    def open_Suppliers(self):
+    def _open_suppliers(self):
         self.app.run_dialog(SupplierSearch, self.app.conn)
 
-    def open_Taxes(self):
+    def _open_taxes(self):
         self.app.run_dialog(SellableTaxConstantsDialog, self.app.conn)
 
-    def open_Transporters(self):
+    def _open_tax_templates(self):
+        self.app.run_dialog(TaxTemplatesSearch, self.app.conn)
+
+    def _open_transporters(self):
         self.app.run_dialog(TransporterSearch, self.app.conn)
 
-    def open_Users(self):
+    def _open_users(self):
         self.app.run_dialog(UserSearch, self.app.conn)
 
-    def open_User_Profiles(self):
+    def _open_user_profiles(self):
         self.app.run_dialog(UserProfileSearch, self.app.conn)
 
 
@@ -219,14 +255,17 @@ class AdminApp(AppWindow):
     app_name = _('Administrative')
     app_icon_name = 'stoq-admin-app'
     gladefile = "admin"
+    # FIXME: Remove
     klist_name = "foo"
 
     def __init__(self, app):
+        # FIXME: Remove
         class x: pass
         self.foo = x()
         self.foo.get_columns = lambda : []
         self.foo.set_columns = lambda *x: None
         self.foo.set_selection_mode = lambda *x: None
+
         self.tasks = Tasks(self)
         self.tasks.add_defaults()
         AppWindow.__init__(self, app)
@@ -238,107 +277,75 @@ class AdminApp(AppWindow):
         self._version_checker = VersionChecker(self.conn, self)
         self._version_checker.check_new_version()
 
+    # FIXME: Remove
     def _update_view(self):
         pass
     def get_columns(self):
         return []
 
     #
-    # Private
-    #
-
-    def _run_invoice_printer_dialog(self):
-        if not InvoiceLayout.select(connection=self.conn):
-            info(_(
-                "You must create at least one invoice layout before adding an"
-                "invoice printer"))
-            return
-
-        self.run_dialog(InvoicePrinterDialog, self.conn)
-
-    def _run_payment_categories_dialog(self):
-        trans = new_transaction()
-        model = self.run_dialog(PaymentCategoryDialog, trans)
-        finish_transaction(trans, model)
-        trans.close()
-
-    def _run_client_categories_dialog(self):
-        trans = new_transaction()
-        model = self.run_dialog(ClientCategoryDialog, trans)
-        finish_transaction(trans, model)
-        trans.close()
-
-    #
     # Callbacks
     #
 
     def _on_fiscalbook_action_clicked(self, button):
-        self.run_dialog(FiscalBookEntrySearch, self.conn, hide_footer=True)
+        self.tasks.run_task('fiscal_books')
 
     def on_taxes__activate(self, action):
-        self.run_dialog(SellableTaxConstantsDialog, self.conn)
+        self.tasks.run_task('taxes')
 
     def on_sintegra__activate(self, action):
-        branch = get_current_branch(self.conn)
-
-        if branch.manager is None:
-            info(_(
-                "You must define a manager to this branch before you can create"
-                " a sintegra archive"))
-            return
-
-        self.run_dialog(SintegraDialog, self.conn)
+        self.tasks.run_task('sintegra')
 
     def on_BranchSearch__activate(self, action):
-        self.run_dialog(BranchSearch, self.conn, hide_footer=True)
+        self.tasks.run_task('branches')
 
     def on_BranchStationSearch__activate(self, action):
-        self.run_dialog(StationSearch, self.conn, hide_footer=True)
+        self.tasks.run_task('stations')
 
     def on_CfopSearch__activate(self, action):
-        self.run_dialog(CfopSearch, self.conn, hide_footer=True)
+        self.tasks.run_task('cfop')
 
     def on_EmployeeSearch__activate(self, action):
-        self.run_dialog(EmployeeSearch, self.conn, hide_footer=True)
+        self.tasks.run_task('employees')
 
     def on_UserSearch__activate(self, action):
-        self.run_dialog(UserSearch, self.conn)
+        self.tasks.run_task('users')
 
     def on_EmployeeRole__activate(self, action):
-        self.run_dialog(EmployeeRoleSearch, self.conn)
+        self.tasks.run_task('employee_roles')
 
     def on_UserProfile__activate(self, action):
-        self.run_dialog(UserProfileSearch, self.conn)
+        self.tasks.run_task('user_profiles')
 
     def on_about_menu__activate(self, action):
         self._run_about()
 
     def on_devices_setup__activate(self, action):
-        self.run_dialog(DeviceSettingsDialog, self.conn)
+        self.tasks.run_task('devices')
 
     def on_system_parameters__activate(self, action):
-        self.run_dialog(ParameterSearch, self.conn)
+        self.tasks.run_task('parameters')
 
     def on_invoice_printers__activate(self, action):
-        self._run_invoice_printer_dialog()
+        self.tasks.run_task('invoice_printers')
 
     def on_invoices__activate(self, action):
-        self.run_dialog(InvoiceLayoutDialog, self.conn)
+        self.tasks.run_task('invoice_layouts')
 
     def on_PaymentMethod__activate(self, action):
-        self.run_dialog(PaymentMethodsDialog, self.conn)
+        self.tasks.run_task('payment_methods')
 
     def on_payment_categories__activate(self, action):
-        self._run_payment_categories_dialog()
+        self.tasks.run_task('payment_categories')
 
     def on_client_categories__activate(self, action):
-        self._run_client_categories_dialog()
+        self.tasks.run_task('client_categories')
 
     def on_Plugins__activate(self, action):
-        self.run_dialog(PluginManagerDialog, self.conn)
+        self.tasks.run_task('plugins')
 
     def on_TaxTemplates__activate(self, action):
-        self.run_dialog(TaxTemplatesSearch, self.conn)
+        self.tasks.run_task('tax_templates')
 
     def on_help_contents__activate(self, action):
         show_contents()
