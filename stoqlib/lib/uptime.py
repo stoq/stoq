02468e0ea85cf352c30cb3edd1813d12c072bbf6
jdahlin@async.com.br <None>
2011-06-20 16:52:39 -0300

    Add uptime module

---
+++ stoqlib/lib/uptime.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2011 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s): Stoq Team <stoq-devel@async.com.br>
+##
+##
+""" Application uptime"""
+
+import time
+
+_start_time = None
+
+def set_initial():
+    _start_time = time.time()
+
+def get_uptime():
+    """Get the uptime of the application
+    Returns: number of seconds the application has been running
+    """
+    if not _start_time:
+        return 0
+    return int(time.time() - _start_time)
+
+
