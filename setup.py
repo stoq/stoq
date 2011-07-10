49a8be2ce62b888e9c737512bbd0febafc9d31d4
jdahlin@async.com.br <None>
2011-07-11 05:36:41 +1000

    Update script name here as well

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ packages.extend(listpackages('stoqlib', exclude='stoqlib.tests'))
 
 scripts = [
     'bin/stoq',
-    'bin/stoqcreatedbuser',
+    'bin/stoqcreatedatabase',
     'bin/stoqdbadmin',
     'bin/stoqruncmd',
     ]
