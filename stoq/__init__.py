307e3a3c36a70be04a88860a77b5f348e42e4eb4
Johan Dahlin <jdahlin@async.com.br>
2011-07-28 17:49:29 -0300

    Back to development

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -28,9 +28,9 @@ website         = 'http://www.stoq.com.br'
 major_version   = 1
 minor_version   = 0
 micro_version   = 0
-extra_version   = 0
+extra_version   = 90
 release_date    = (2011, 7, 28)
-stable          = True
+stable          = False
 
 version         = '%d.%d.%d' % (major_version, minor_version, micro_version)
 if extra_version > 0:
