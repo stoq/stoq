aaad809d6a93b663750df40af8d5ea2eb22f56fd
Johan Dahlin <jdahlin@async.com.br>
2011-10-02 15:55:55 +0200

    Add a stoqdbadmin.py script

---
+++ stoqdbadmin.py
@@ -0,0 +1,24 @@
+import errno
+import os
+import sys
+
+os.environ['LANGUAGE'] = 'pt_BR.UTF-8'
+
+logdir = os.path.join(os.environ['APPDATA'], "stoqdbadmin", "logs")
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
+# http://www.py2exe.org/index.cgi/StderrLog
+for name in ['stdout', 'stderr']:
+    filename = os.path.join(logdir, name + ".log")
+    try:
+        fp = open(filename, "w")
+    except IOError, e:
+        if e.errno != errno.EACCES:
+            raise
+        fp = open(os.devnull, "w")
+    setattr(sys, name, fp)
+
+os.environ['PATH'] += os.pathsep + '.'
+
+from stoq.dbadmin import main
+sys.exit(main(sys.argv))
