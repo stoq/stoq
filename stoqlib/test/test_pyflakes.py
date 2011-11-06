1350bc63be9911c2d319dabb340a1db9a845d22f
Johan Dahlin <jdahlin@async.com.br>
2011-11-06 17:08:43 -0200

    Improve pyflakes test * Remove redundant checks * Import stoq and use the path from the imported module * Avoid printing absolute paths * When asserting, include the error messages in the assertion message

--- stoqlib/test/test_pyflakes.py
+++ stoqlib/test/test_pyflakes.py
@@ -26,43 +26,100 @@
 Useful to early find syntax errors and other common problems.
 """
 
+import compiler
 import sys
-import os.path
+import os
 
 from twisted.trial import unittest
-try:
-    from pyflakes.scripts import pyflakes
-    pyflakes # pyflakes (ironique ah?)
-except ImportError:
-    pyflakes = None
 
+import stoq
+
+# stolen from pyflakes
+def check(codeString, filename, warnings):
+    try:
+        try:
+            compile(codeString, filename, "exec")
+        except MemoryError:
+            # Python 2.4 will raise MemoryError if the source can't be
+            # decoded.
+            if sys.version_info[:2] == (2, 4):
+                raise SyntaxError(None)
+            raise
+    except (SyntaxError, IndentationError), value:
+        msg = value.args[0]
+
+        (lineno, offset, text) = value.lineno, value.offset, value.text
+
+        # If there's an encoding problem with the file, the text is None.
+        if text is None:
+            # Avoid using msg, since for the only known case, it contains a
+            # bogus message that claims the encoding the file declared was
+            # unknown.
+            print >> sys.stderr, "%s: problem decoding source" % (filename, )
+        else:
+            line = text.splitlines()[-1]
+
+            if offset is not None:
+                offset = offset - (len(text) - len(line))
+
+            print >> sys.stderr, '%s:%d: %s' % (filename, lineno, msg)
+            print >> sys.stderr, line
+
+            if offset is not None:
+                print >> sys.stderr, " " * offset, "^"
+
+        return 1
+    except UnicodeError, msg:
+        print >> sys.stderr, 'encoding error at %r: %s' % (filename, msg)
+        return 1
+    else:
+        # Okay, it's syntactically valid.  Now parse it into an ast and check
+        # it.
+        tree = compiler.parse(codeString)
+        w = checker.Checker(tree, filename)
+        warnings.extend(w.messages)
+        return len(warnings)
+
+checker = __import__('pyflakes.checker').checker
 
 class TestPyflakes(unittest.TestCase):
+    def runPyflakes(self, path):
+        warnings = []
+        msgs = []
+        result = 0
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                if not filename.endswith('.py'):
+                    continue
+                filename = os.path.join(dirpath, filename)
+                try:
+                    fd = file(filename, 'U')
+                    try:
+                        result = check(fd.read(), filename, warnings)
+                    finally:
+                        fd.close()
+                except IOError, msg:
+                    print >> sys.stderr, "%s: %s" % (filename, msg.args[1])
+                    result = 1
 
-    def __init__(self, *args, **kwargs):
-        super(TestPyflakes, self).__init__(*args, **kwargs)
+        warnings.sort(lambda a, b: cmp(a.lineno, b.lineno))
+        for warning in warnings:
+            msg = str(warning).replace(root, '')
+            print msg
+            msgs.append(msg)
+        if result:
+            raise AssertionError("%d warnings:\n%s\n" % (len(msgs), '\n'.join(msgs), ))
 
-    def _test_path(self, path_name):
-        # Skip test if user doesn't have pyflakes installed.
-        if not pyflakes:
-            raise unittest.SkipTest("Pyflakes not installed")
-
-        test_path = None
-        for path in sys.path:
-            if path.endswith('stoq'):
-                test_path = path
-                break
-        self.assertTrue(test_path)
-
-        path = os.path.join(test_path, path_name)
-        retval = pyflakes.main([path])
-        self.assertEqual(retval, 0)
-
-
-for subpath in ('stoq', 'stoqlib', 'plugins'):
-    # subpath has to be passed as a kw, or it will always be plugins (the
-    # last subpath variable on for).
-    func = lambda self, subpath=subpath: self._test_path(subpath)
-    name = 'test_%s_pyflakes' % (subpath,)
+root = os.path.dirname(os.path.dirname(stoq.__file__)) + '/'
+for dirpath in ['stoq', 'stoqlib', 'plugins']:
+    path = os.path.abspath(os.path.join(root, dirpath))
+    name = 'test_%s_pyflakes' % (dirpath,)
+    func = lambda self, path=path: self.runPyflakes(path)
     func.__name__ = name
     setattr(TestPyflakes, name, func)
+
+
+
+
+
+
