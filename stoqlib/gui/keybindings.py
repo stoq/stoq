85f023d98cdb7395c33c1f0c9b7240db0348374e
Ronaldo Maia <romaia@async.com.br>
2012-01-10 10:34:44 -0200

    Dont fail if there is no section Shortcuts in the config

--- stoqlib/gui/keybindings.py
+++ stoqlib/gui/keybindings.py
@@ -307,7 +307,12 @@ def add_bindings(bindings):
 
 def load_user_keybindings():
     from stoqlib.api import api
-    for key, value in api.config.items('Shortcuts'):
+    from ConfigParser import NoSectionError
+    try:
+        shortcuts = api.config.items('Shortcuts')
+    except NoSectionError:
+        return
+    for key, value in shortcuts:
         set_user_binding(key, value)
 
 
