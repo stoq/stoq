c62c085ebb1f82d8ecf68747da47ccb3bc688a8b
Ronaldo Maia <romaia@async.com.br>
2012-01-27 09:08:09 -0200

    Also search for barcode on SellableSearch (pos)

--- stoqlib/gui/search/sellablesearch.py
+++ stoqlib/gui/search/sellablesearch.py
@@ -130,7 +130,8 @@ class SellableSearch(SearchEditor):
         super(SellableSearch, self).confirm()
 
     def create_filters(self):
-        self.set_text_field_columns(['description', 'category_description'])
+        self.set_text_field_columns(['description', 'category_description',
+                                     'barcode'])
         self.executer.set_query(self._executer_query)
 
     def get_columns(self):
