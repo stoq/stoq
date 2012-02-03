3dfdbf56d6e7403743ecfb8f62cc3260d89acdf4
Ronaldo Maia <romaia@async.com.br>
2012-02-03 11:56:07 -0200

    Fix patch 03-01, to not register accounts again.

--- data/sql/patch-03-01.py
+++ data/sql/patch-03-01.py
@@ -1,8 +1,15 @@
-from stoqlib.database.admin import (register_accounts,
-                                    register_payment_methods)
+from stoqlib.lib.parameters import sysparam
+from stoqlib.database.admin import register_payment_methods
 
 def apply_patch(trans):
     # This might run on an empty database so make sure we have
     # the imbalance account created
-    register_accounts(trans)
+
+    account = sysparam(trans).IMBALANCE_ACCOUNT
+    # There is no parameter yet. This means the database is brand new. No need
+    # to register the new payment now, since it will be created by
+    # initialize_system
+    if not account:
+        return
+
     register_payment_methods(trans)
