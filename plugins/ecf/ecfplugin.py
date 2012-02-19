3c1d29ac409461120aac417530eda96e0eee2db5
Johan Dahlin <jdahlin@async.com.br>
2012-02-19 21:48:26 -0200

    Import the module in activate(), to avoid a dependency on gtk when activating the plugin

--- plugins/ecf/ecfplugin.py
+++ plugins/ecf/ecfplugin.py
@@ -34,7 +34,6 @@ from stoqlib.lib.pluginmanager import register_plugin
 
 plugin_root = os.path.dirname(__file__)
 sys.path.append(plugin_root)
-from ecfui import ECFUI
 
 
 class ECFPlugin(object):
@@ -54,6 +53,9 @@ class ECFPlugin(object):
 
     def activate(self):
         environ.add_resource('glade', os.path.join(plugin_root, 'glade'))
+        # Do this in a nested import so we can import the plugin without
+        # importing gtk.
+        from ecfui import ECFUI
         self.ui = ECFUI()
 
 
