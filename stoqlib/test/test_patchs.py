d9b5c9147d7e5e378ce554bc5e7c9b1d8dadaf32
Thiago Bellini <hackedbellini@async.com.br>
2012-02-28 14:58:00 -0300

    Add a test for finding duplicated patchs.
    Sometimes, if we don't pay attention, we can duplicate patchs. Like, when we
    write a 'patch-03-01.py' and on rebase someone did already add a
    'patch-03-01.sql'. bzr won't conflict in this case scenario (because of the
    different extention).
    This test tries to catch this kind of error earlier.

---
+++ stoqlib/test/test_patchs.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2012 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s): Stoq Team <stoq-devel@async.com.br>
+##
+"""Test patchs for Stoq"""
+
+import os
+
+import stoqlib
+from twisted.trial import unittest
+from stoqlib.lib.osutils import list_recursively
+
+_ROOT = os.path.dirname(os.path.dirname(stoqlib.__file__))
+
+
+class TestPatchs(unittest.TestCase):
+
+    def test_duplicated(self):
+        path = os.path.join(_ROOT, 'data')
+        sql_patchs = set([os.path.basename(fname)[:-4] for fname in
+                          list_recursively(path, '*.sql')])
+        py_patchs = set([os.path.basename(fname)[:-3] for fname in
+                         list_recursively(path, '*.py')])
+        identical_patchs = sql_patchs & py_patchs
+
+        if identical_patchs:
+            raise AssertionError("The patchs [%s] are duplicated" %
+                                 (','.join(identical_patchs,)))
