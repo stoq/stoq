03da914317c13aafb9820d5fa41dc921e49fc574
Ronaldo Maia <romaia@async.com.br>
2012-03-01 11:20:38 -0300

    Create tax constant names with 2 decimal places

--- plugins/ecf/ecfdomain.py
+++ plugins/ecf/ecfdomain.py
@@ -105,8 +105,10 @@ class ECFPrinter(Domain):
                            connection=conn)
 
         for constant, device_value, value in constants.get_tax_constants():
+            # FIXME: Looks like this is not used and/or is duplicating code from
+            # ecfpriterdialog.py (_populate_constants)
             if constant == TaxType.CUSTOM:
-                constant_name = _('%d %%') % value
+                constant_name = _('%0.2f %%') % value
             else:
                 constant_name = describe_constant(constant)
             DeviceConstant(constant_type=DeviceConstant.TYPE_TAX,
