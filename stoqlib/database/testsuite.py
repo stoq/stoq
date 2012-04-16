8ab498ce96dddb44b78914c503272395805175e8
Ronaldo Maia <romaia@async.com.br>
2012-04-16 15:33:50 -0300

    - Fix another issue when dropping datbase with --force - Explain the logic when check if its safe to drop the database - Force dropping of the database when running tests

--- stoqlib/database/testsuite.py
+++ stoqlib/database/testsuite.py
@@ -234,7 +234,7 @@ def bootstrap_testsuite(address=None, dbname=None, port=5432, username=None,
             # XXX: Why clearing_cache if initialize_system will drop the
             # database?!
             sysparam(get_connection()).clear_cache()
-            initialize_system(testsuite=True)
+            initialize_system(testsuite=True, force=True)
             ensure_admin_user("")
             create(utilities=True)
     except Exception:
