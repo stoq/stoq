90fcf742515e49c2daf616177ec53fb1490eed15
Johan Dahlin <jdahlin@async.com.br>
2012-07-24 16:08:01 -0300

    Move the zope autointerface extension to docs/api and update conf

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -29,7 +29,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     # Forked from sphinxcontrib.zopeext
-    'stoqlib.lib.zopeautointerface',
+    'zopeautointerface',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
