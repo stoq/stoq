290d0456bb58d143e6a2e28fb4584fd6a4ed367d
Johan Dahlin <jdahlin@async.com.br>
2012-08-06 15:33:46 -0300

    Add a calls test

---
+++ stoqlib/gui/editors/test/test_calls_editor.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2012 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s): Stoq Team <stoq-devel@async.com.br>
+##
+
+import datetime
+import unittest
+
+from stoqlib.domain.person import Calls
+from stoqlib.gui.editors.callseditor import CallsEditor
+from stoqlib.gui.uitestutils import GUITest
+
+
+class TestCallsEditor(GUITest):
+    def testCreate(self):
+        person = self.create_person()
+        editor = CallsEditor(self.trans, None, person, None)
+        self.assertTrue(isinstance(editor.model, Calls))
+        editor.date.update(datetime.date(2012, 1, 1))
+
+        self.check_editor(editor, 'editor-calls-create',
+                          ignores=[str(person)])
+
+    def testShow(self):
+        person = self.create_person()
+        calls = self.create_call()
+        calls.person = person
+        editor = CallsEditor(self.trans, calls, person, None)
+
+        self.check_editor(editor, 'editor-calls-show',
+                          ignores=[str(calls.person),
+                                   str(calls.attendant)])
+
+
+if __name__ == '__main__':
+    from stoqlib.api import api
+    c = api.prepare_test()
+    unittest.main()
