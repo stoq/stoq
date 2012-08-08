563c60857e32eaaa72182d233cf884fe01a473b5
Johan Dahlin <jdahlin@async.com.br>
2012-08-08 17:04:06 -0300

    Add a payable app ui test

---
+++ stoq/gui/test/test_payable.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2012 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s): Stoq Team <stoq-devel@async.com.br>
+##
+
+from kiwi.component import provide_utility
+import mock
+
+from stoqlib.api import api
+from stoqlib.gui.uitestutils import GUITest
+from stoqlib.lib.interfaces import  IApplicationDescriptions
+from stoq.gui.application import App
+from stoq.gui.launcher import Launcher
+from stoq.gui.payable import PayableApp
+from stoq.lib.applist import ApplicationDescriptions
+
+provide_utility(IApplicationDescriptions, ApplicationDescriptions(), replace=True)
+
+
+class TestPayable(GUITest):
+    def create_app(self, app_name):
+        self.user = api.get_current_user(self.trans)
+        self.profile = self.create_profile_settings(self.user.profile, app_name)
+        self.shell = mock.Mock()
+        self.options = mock.Mock(spec=['debug'])
+        self.options.debug = False
+        self.launcher = Launcher(self.options, self.shell, conn=self.trans)
+        app = App(PayableApp, None, self.options, self.shell, True,
+                  self.launcher, app_name)
+        app.show()
+        return app
+
+    def testInitial(self):
+        api.sysparam(self.trans).update_parameter('SMART_LIST_LOADING', '0')
+        app = self.create_app('payable')
+        self.check_wizard(app.main_window, 'app-payable')
