e312fb92db3d4f6bca23acef4e4bc9495d4c22e0
Ronaldo Maia <romaia@async.com.br>
2012-08-13 11:36:44 -0300

    Fix phone formatting and support 0900 as well

--- stoqlib/lib/test/test_formatters.py
+++ stoqlib/lib/test/test_formatters.py
@@ -33,12 +33,21 @@ class TestValidators(unittest.TestCase):
         self.assertEquals(format_phone_number("190"), "190")
         self.assertEquals(format_phone_number("1052"), "1052")
         self.assertEquals(format_phone_number("10325"), "103 25")
+
         self.assertEquals(format_phone_number("0300123456"), "0300 123-456")
         self.assertEquals(format_phone_number("03001234567"), "0300 123-4567")
         self.assertEquals(format_phone_number("0500700600"), "0500 700-600")
         self.assertEquals(format_phone_number("05007006005"), "0500 700-6005")
         self.assertEquals(format_phone_number("0800197878"), "0800 197-878")
         self.assertEquals(format_phone_number("08001234567"), "0800 123-4567")
+        self.assertEquals(format_phone_number("0900197878"), "0900 197-878")
+        self.assertEquals(format_phone_number("09001234567"), "0900 123-4567")
+
+        self.assertEquals(format_phone_number("1312345678"), "(13) 1234-5678")
+        self.assertEquals(format_phone_number("1512345678"), "(15) 1234-5678")
+        self.assertEquals(format_phone_number("1812345678"), "(18) 1234-5678")
+        self.assertEquals(format_phone_number("1912345678"), "(19) 1234-5678")
+
         self.assertEquals(format_phone_number("12345678"), "1234-5678")
         self.assertEquals(format_phone_number("1612345678"), "(16) 1234-5678")
         self.assertEquals(format_phone_number("01612345678"), "(16) 1234-5678")
