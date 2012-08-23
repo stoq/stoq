a004e86bc176a4b3eb6302b70e6f1383a5856c27
Johan Dahlin <jdahlin@async.com.br>
2012-08-23 15:49:25 -0300

    FirstTimeConfigWizard: add more uitests

--- stoq/gui/test/test_config_wizard.py
+++ stoq/gui/test/test_config_wizard.py
@@ -22,6 +22,7 @@
 ## Author(s): Stoq Team <stoq-devel@async.com.br>
 ##
 
+import gtk
 import mock
 from stoqlib.database.settings import DatabaseSettings
 from stoqlib.gui.uitestutils import GUITest
@@ -33,6 +34,7 @@ class _MockConfig:
     def __init__(self, settings):
         self.settings = settings
         self.options = None
+        self.flushed = False
 
     def get_settings(self):
         return self.settings
@@ -48,16 +50,27 @@ class _MockConfig:
 
     def get(self, section, value):
         if (section, value) == ('Database', 'enable_production'):
-            return True
+            return ''
         raise AssertionError((section, value))
+
+    def flush(self):
+        self.flushed = True
 
 
 class TestConfirmSaleWizard(GUITest):
     @mock.patch('stoq.gui.config.ProcessView.execute_command')
     @mock.patch('stoq.gui.config.ensure_admin_user')
     @mock.patch('stoq.gui.config.create_default_profile_settings')
-    def testLocal(self, create_default_profile_settings,
-                  ensure_admin_user, execute_command):
+    @mock.patch('stoq.gui.config.yesno')
+    @mock.patch('stoq.gui.config.warning')
+    @mock.patch('stoq.gui.config.BranchStation')
+    def testLocal(self,
+                  BranchStation,
+                  warning,
+                  yesno,
+                  create_default_profile_settings,
+                  ensure_admin_user,
+                  execute_command):
         options = mock.Mock()
         options.sqldebug = False
         options.verbose = False
@@ -78,8 +91,6 @@ class TestConfirmSaleWizard(GUITest):
         self.check_wizard(wizard, 'wizard-config-database-location')
         self.click(wizard.next_button)
 
-        step = wizard.get_current_step()
-        self.assertTrue(step.empty_database_radio.get_active())
         self.check_wizard(wizard, 'wizard-config-installation-mode')
         self.click(wizard.next_button)
 
@@ -111,11 +122,45 @@ class TestConfirmSaleWizard(GUITest):
             '-p', '5432',
             '-u', 'username',
             '-w', 'password'])
-
         step = wizard.get_current_step()
+        self.assertEquals(step.progressbar.get_text(),
+                          'Creating database...')
+
+        step.process_view.emit('read-line', 'stoqlib.database.create SCHEMA')
+        self.assertEquals(step.progressbar.get_text(),
+                          'Creating base schema...')
+
+        step.process_view.emit('read-line', 'stoqlib.database.create PATCHES:1')
+        self.assertEquals(step.progressbar.get_text(),
+                          'Creating schema, applying patches...')
+
+        step.process_view.emit('read-line', 'stoqlib.database.create PATCH:0')
+        self.assertEquals(step.progressbar.get_text(),
+                          'Creating schema, applying patch 1 ...')
+
+        step.process_view.emit('read-line', 'stoqlib.database.create INIT START')
+        self.assertEquals(step.progressbar.get_text(),
+                          'Creating additional database objects ...')
+
+        step.process_view.emit('read-line', 'stoqlib.database.create PLUGIN')
+        self.assertEquals(step.progressbar.get_text(),
+                          'Activating plugins ...')
+
+        yesno.return_value = False
+        step.process_view.emit('finished', 30)
+        yesno.assert_called_once_with(
+            'Something went wrong while trying to create the database. Try again?',
+            gtk.RESPONSE_NO, 'Change settings', 'Try again')
+
+        step.process_view.emit('finished', 999)
+        warning.assert_called_once_with(
+            "Something went wrong while trying to create the Stoq database")
+
         step.process_view.emit('finished', 0)
         create_default_profile_settings.assert_called_once_with()
         ensure_admin_user.assert_called_once_with('password')
         self.click(wizard.next_button)
 
         self.check_wizard(wizard, 'wizard-config-done')
+        self.click(wizard.next_button)
+        self.assertTrue(config.flushed)
