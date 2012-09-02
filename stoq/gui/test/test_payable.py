2c058df2f9fb5efb762fd80941b244ac963e86f5
Johan Dahlin <jdahlin@async.com.br>
2012-09-02 23:39:22 +0200

    UITest: test a couple of payable app actions

--- stoq/gui/test/test_payable.py
+++ stoq/gui/test/test_payable.py
@@ -22,14 +22,139 @@
 ## Author(s): Stoq Team <stoq-devel@async.com.br>
 ##
 
+import datetime
+
+import mock
 from stoqlib.api import api
+from stoqlib.domain.purchase import PurchaseOrder
+from stoqlib.gui.dialogs.paymentcommentsdialog import PaymentCommentsDialog
+from stoqlib.gui.dialogs.paymentchangedialog import PaymentDueDateChangeDialog
+from stoqlib.gui.editors.paymenteditor import OutPaymentEditor
+from stoqlib.gui.editors.paymentseditor import PurchasePaymentsEditor
+from stoqlib.gui.slaves.installmentslave import PurchaseInstallmentConfirmationSlave
 
 from stoq.gui.payable import PayableApp
 from stoq.gui.test.baseguitest import BaseGUITest
 
 
+class FakeAPITrans:
+    def __init__(self, trans):
+        self.trans = trans
+        self.trans.committed = True
+
+    def __call__(self):
+        return self
+
+    def __enter__(self):
+        return self.trans
+
+    def __exit__(self, *args):
+        pass
+
+
 class TestPayable(BaseGUITest):
-    def testInitial(self):
+    def setUp(self):
+        BaseGUITest.setUp(self)
         api.sysparam(self.trans).update_parameter('SMART_LIST_LOADING', '0')
+
+    def create_purchase_payment(self):
+        order = self.create_purchase_order()
+        order.identifier = 12345
+        order.status = PurchaseOrder.ORDER_PENDING
+        order.add_item(self.create_sellable(), 1)
+        payment = self.add_payments(order, method_type='money')[0]
+        payment.open_date = payment.due_date = datetime.date(2012, 1, 1)
+        order.confirm()
+        payment.identifier = 67890
+        return order, payment
+
+    def testInitial(self):
         app = self.create_app(PayableApp, 'payable')
         self.check_app(app, 'payable')
+
+    def testSelect(self):
+        purchase, payment = self.create_purchase_payment()
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[1])
+        self.check_app(app, 'payable-selected')
+
+    @mock.patch('stoq.gui.payable.run_dialog')
+    def testPay(self, run_dialog):
+        purchase, payment = self.create_purchase_payment()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[1])
+
+        with mock.patch.object(api, 'trans',
+                               new=FakeAPITrans(self.trans)):
+            self.activate(app.main_window.Pay)
+
+        run_dialog.assert_called_once_with(
+            PurchaseInstallmentConfirmationSlave, app.main_window,
+            self.trans, payments=[payment])
+
+    @mock.patch('stoq.gui.payable.run_dialog')
+    def testEdit(self, run_dialog):
+        purchase, payment = self.create_purchase_payment()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[1])
+
+        with mock.patch.object(api, 'trans',
+                               new=FakeAPITrans(self.trans)):
+            self.activate(app.main_window.Edit)
+
+        run_dialog.assert_called_once_with(
+            PurchasePaymentsEditor, app.main_window,
+            self.trans, purchase)
+
+    @mock.patch('stoq.gui.payable.run_dialog')
+    def testChangeDueDate(self, run_dialog):
+        purchase, payment = self.create_purchase_payment()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[1])
+
+        with mock.patch.object(api, 'trans',
+                               new=FakeAPITrans(self.trans)):
+            self.activate(app.main_window.ChangeDueDate)
+
+        run_dialog.assert_called_once_with(
+            PaymentDueDateChangeDialog, app.main_window,
+            self.trans, payment, purchase)
+
+    @mock.patch('stoq.gui.payable.run_dialog')
+    def testDetails(self, run_dialog):
+        purchase, payment = self.create_purchase_payment()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[1])
+
+        with mock.patch.object(api, 'trans',
+                               new=FakeAPITrans(self.trans)):
+            self.activate(app.main_window.Details)
+
+        run_dialog.assert_called_once_with(
+            OutPaymentEditor, app.main_window,
+            self.trans, payment)
+
+    @mock.patch('stoq.gui.payable.run_dialog')
+    def testComments(self, run_dialog):
+        purchase, payment = self.create_purchase_payment()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[1])
+
+        with mock.patch.object(api, 'trans',
+                               new=FakeAPITrans(self.trans)):
+            self.activate(app.main_window.Comments)
+
+        run_dialog.assert_called_once_with(
+            PaymentCommentsDialog, app.main_window,
+            self.trans, payment)
