c9270f961c58eb2bf4b2644c7c9772e8f128dcf0
Johan Dahlin <jdahlin@async.com.br>
2012-09-03 23:04:10 +0200

    Include PEP8 0.6.1 in the source tree and open it using subprocess

--- stoqlib/test/test_pep8.py
+++ stoqlib/test/test_pep8.py
@@ -27,43 +27,18 @@ Useful to early find syntax errors and other common problems.
 """
 
 import os
+import subprocess
 import unittest
-
-import mock
-import pep8
+import sys
 
 import stoqlib
 
 ERRORS = [
-    'E111', # indentation is not a multiple of four
-    'E112', # expected an indented block
-    'E113', # unexpected indentation
-    'E201', # whitespace after '{'
-    'E202', # whitespace before ')'
-    'E203', # whitespace before ':'
-    'E211', # whitespace before '('
-    'E221', # multiple spaces before operator
-    'E225', # missing whitespace around operator
-    'E231', # E231 missing whitespace after ','/':'
-    'E241', # multiple spaces after operator
-    'E251', # no spaces around keyword / parameter equals
+    'E261', # inline comment should have two spaces before
     # pep8 needs to be updated to allow the #: syntax that that
     # sphinx.ext.autodoc uses
-    # 'E262', # inline comment should start with '# '
-    'W291', # trailing whitespace
-    'W292', # no newline at end of file
-    'W293', # blank line contains whitespace
-    'E301', # expected 1 blank line, found 0
-    'E302', # expected 2 blank lines, found 1
-    'E303', # too many blank lines
-    'W391', # blank line at end of file
-    'E401', # multiple imports on one line
-    'W601', # in instead of dict.has_key
-    'W602', # deprecated form of raising exception
-    'W603', # '<>' is deprecated, use '!='"
-    'W604', # backticks are deprecated, use 'repr()'
-    'E701', # multiple statements on one line (colon)
-    'E702', # multiple statements on one line (semicolon)
+    'E262', # inline comment should start with '# '
+    'E501', # line too long
 ]
 
 
@@ -72,17 +47,17 @@ class TestPEP8(unittest.TestCase):
         self.root = os.path.dirname(
                     os.path.dirname(stoqlib.__file__)) + '/'
 
-    @mock.patch('pep8.message')
-    def test_PEP8(self, message):
-        pep8.process_options([
-            '--repeat',
-            '--select=%s' % (','.join(ERRORS), ), self.root])
-        pep8.input_dir(os.path.join(self.root, 'stoq'))
-        pep8.input_dir(os.path.join(self.root, 'stoqlib'))
-        result = pep8.get_count()
-        msgs = []
-        for call in message.call_args_list:
-            msgs.append(call[0][0][len(self.root):])
+    def test_PEP8(self):
+        cmd = [sys.executable,
+               os.path.join(self.root, 'tools', 'pep8.py'),
+               '--count',
+               '--repeat',
+               '--ignore=%s' % (','.join(ERRORS), ),
+               'stoq', 'stoqlib']
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        stdout = p.communicate()[0]
+        result = p.returncode
         if result:
             raise AssertionError(
-                "ERROR: %d PEP8 errors:\n%s" % (result, '\n'.join(msgs)))
+                "ERROR: %d PEP8 errors:\n%s" % (result, stdout))
