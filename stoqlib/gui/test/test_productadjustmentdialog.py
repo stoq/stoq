bae578a91a5e8c03404cb8fc329702d50b4b3b04
Ronaldo Maia <romaia@async.com.br>
2012-09-28 17:43:00 -0300

    Test product adjustment dialog

---
+++ stoqlib/gui/test/test_productadjustmentdialog.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2012 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s): Stoq Team <stoq-devel@async.com.br>
+##
+
+import mock
+from stoqlib.gui.uitestutils import GUITest
+from stoqlib.gui.dialogs.productadjustmentdialog import (ProductsAdjustmentDialog,
+                                                         AdjustmentDialog)
+
+
+class TestProductAdjustmentDialog(GUITest):
+    def testCreate(self):
+        inventory = self.create_inventory()
+        inventory.invoice_number = 4123
+        item = self.create_inventory_item(inventory, 5)
+        item.actual_quantity = 10
+        dialog = ProductsAdjustmentDialog(self.trans, inventory)
+        self.check_editor(dialog, 'dialog-product-adjustment')
+
+    @mock.patch('stoqlib.gui.dialogs.productadjustmentdialog.run_dialog')
+    def testAdjust(self, run_dialog):
+        inventory = self.create_inventory()
+        item = self.create_inventory_item(inventory, 5)
+        item.actual_quantity = 10
+
+        dialog = ProductsAdjustmentDialog(self.trans, inventory)
+        self.assertNotSensitive(dialog, ['adjust_button'])
+
+        dialog.invoice_number.update(123)
+        dialog.inventory_items.select(item)
+        # _run_adjustment_dialog commits the transaction. Avoid that as it will
+        # break other tests
+        finish = 'stoqlib.gui.dialogs.productadjustmentdialog.api.finish_transaction'
+        with mock.patch(finish):
+            self.click(dialog.adjust_button)
+
+        run_dialog.assert_called_once()
+
+
+class TestAdjustmentDialog(GUITest):
+
+    @mock.patch('stoqlib.gui.dialogs.productadjustmentdialog.warning')
+    def testCreate(self, warning):
+        cfop = self.create_cfop_data()
+        item = self.create_inventory_item()
+        item.actual_quantity = 10
+        dialog = AdjustmentDialog(self.trans, item, 41234)
+        self.check_editor(dialog, 'dialog-product-adjustment-item')
+
+        self.click(dialog.main_dialog.ok_button)
+        warning.assert_called_once_with(
+            'You can not adjust a product without a cfop!')
+
+        warning.reset()
+        dialog.cfop_combo.select(cfop)
+        self.click(dialog.main_dialog.ok_button)
+        warning.assert_called_with(
+            'You can not adjust a product without a reason!')
+
+        dialog.reason.update('just because')
+        warning.reset()
+        self.click(dialog.main_dialog.ok_button)
+        warning.assert_not_called()
