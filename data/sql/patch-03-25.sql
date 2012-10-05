81161422abfb801644d6ed008dce43e2e183cc1f
Ronaldo Maia <romaia@async.com.br>
2012-10-05 13:48:10 -0300

    Update NEXT schema.

--- data/sql/patch-03-25.sql
+++ data/sql/patch-03-25.sql
@@ -1,4 +1,4 @@
 -- #5106: Criar novo campo sigla, para Branches
 
 ALTER TABLE branch ADD COLUMN acronym text UNIQUE
-	CONSTRAINT acronym_not_empty CHECK (acronym != '');
+    CONSTRAINT acronym_not_empty CHECK (acronym != '');
