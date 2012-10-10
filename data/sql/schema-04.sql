63094bb84cea54e085116c67a320c220d5e6b1b5
Ronaldo Maia <romaia@async.com.br>
2012-10-10 16:43:34 -0300

    Create schema 04

--- data/sql/schema-04.sql
+++ data/sql/schema-04.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (C) 2006-2011 Async Open Source
+-- Copyright (C) 2006-2012 Async Open Source
 --
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU Lesser General Public License
