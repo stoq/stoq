79cfff5adb3f1cbad52b10dc244c84a0f4d94c35
João Sampaio <jpmelos@async.com.br>
2012-10-15 16:55:14 -0300

    Add TransferOrderSearch test. r=romaia

---
+++ stoqlib/gui/test/test_transfersearch.py
@@ -0,0 +1,48 @@
+# vi:si:et:sw=4:sts=4:ts=4
+
+##
+## Copyright (C) 2012 Async Open Source <http://www.async.com.br>
+## All rights reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., or visit: http://www.gnu.org/.
+##
+## Author(s): Stoq Team <stoq-devel@async.com.br>
+##
+
+import datetime
+
+from stoqlib.domain.transfer import TransferOrder, TransferOrderItem
+from stoqlib.gui.search.transfersearch import TransferOrderSearch
+from stoqlib.gui.uitestutils import GUITest
+
+
+class TestTransferOrderSearch(GUITest):
+    def testSearch(self):
+        # XXX: Workaround to remove orders created by previous tests.
+        for item in TransferOrderItem.select(connection=self.trans):
+            TransferOrderItem.delete(item.id, self.trans)
+        for order in TransferOrder.select(connection=self.trans):
+            TransferOrder.delete(order.id, self.trans)
+
+        order = self.create_transfer_order()
+        self.create_transfer_order_item(order=order)
+
+        order.identifier = 75168
+        order.open_date = datetime.datetime(2012, 1, 1)
+
+        search = TransferOrderSearch(self.trans)
+
+        search.search.refresh()
+        self.check_search(search, 'transfer-no-filter')
