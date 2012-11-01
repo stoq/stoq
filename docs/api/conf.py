c1a033f03970a60f57657c6cdd7fed28567943d5
Johan Dahlin <jdahlin@async.com.br>
2012-11-01 17:49:29 -0200

    Add stoqautodoc and document PropertyCol/Reference

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -38,6 +38,8 @@ extensions = [
     'sphinx.ext.viewcode',
     # Forked from sphinxcontrib.zopeext
     'zopeautointerface',
+    # Document Stoq specific APIs
+    'stoqautodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
