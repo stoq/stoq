6d69ac3178ce25b95982c01b69df875c7bd072de
Ronaldo Maia <romaia@async.com.br>
2012-11-26 12:23:10 -0200

    1.5 RC5

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 5
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc4'
+extra_version = 'rc5'
 
 #: the date the software was released
-release_date = (2012, 11, 21)
+release_date = (2012, 11, 26)
 
 #: if this is a stable release
 stable = True
