4daf9467dbdc94a75b61508d1acfbb0b0c9e08ef
Luiz Menezes <luiz@async.com.br>
2012-12-03 15:29:20 -0200

    Improved tests for stoq.gui.Payable. r=romaia

--- stoq/gui/test/test_payable.py
+++ stoq/gui/test/test_payable.py
@@ -25,18 +25,43 @@
 import datetime
 
 import mock
+from stoqlib.domain.payment.payment import Payment
 from stoqlib.domain.purchase import PurchaseOrder
 from stoqlib.gui.dialogs.paymentcommentsdialog import PaymentCommentsDialog
 from stoqlib.gui.dialogs.paymentchangedialog import PaymentDueDateChangeDialog
 from stoqlib.gui.editors.paymenteditor import OutPaymentEditor
 from stoqlib.gui.editors.paymentseditor import PurchasePaymentsEditor
+from stoqlib.gui.search.paymentsearch import OutPaymentBillCheckSearch
 from stoqlib.gui.slaves.installmentslave import PurchaseInstallmentConfirmationSlave
+from stoqlib.reporting.payments_receipt import OutPaymentReceipt
 
 from stoq.gui.payable import PayableApp
 from stoq.gui.test.baseguitest import BaseGUITest
 
 
 class TestPayable(BaseGUITest):
+    @mock.patch('stoq.gui.payable.run_dialog')
+    @mock.patch('stoq.gui.payable.api.new_transaction')
+    def _check_run_dialog(self, app, action, dialog, other_args,
+                          new_transaction, run_dialog):
+        new_transaction.return_value = self.trans
+
+        with mock.patch.object(self.trans, 'commit'):
+            with mock.patch.object(self.trans, 'close'):
+                self.activate(action)
+                run_dialog.assert_called_once()
+                args, kwargs = run_dialog.call_args
+                self.assertEquals(args[0], dialog)
+                self.assertEquals(args[1], app)
+                self.assertEquals(args[2], self.trans)
+
+                if not other_args or len(other_args) != len(args[2:]):
+                    return
+
+                for arg in args[2:]:
+                    for other_arg in other_args:
+                        self.assertEquals(arg, other_arg)
+
     def setUp(self):
         BaseGUITest.setUp(self)
 
@@ -137,3 +162,81 @@ class TestPayable(BaseGUITest):
         run_dialog.assert_called_once_with(
             PaymentCommentsDialog, app.main_window,
             self.trans.readonly, payment)
+
+    def test_can_edit(self):
+        purchase, payment = self.create_purchase_payment()
+        purchase.status = PurchaseOrder.ORDER_CANCELLED
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        self.assertFalse(app.main_window._can_edit([olist[-1]]))
+
+    def test_can_pay(self):
+        sale, payment1 = self.create_purchase_payment()
+        payment2 = self.add_payments(sale, method_type='bill')[0]
+        payment2.identifier = 67891
+
+        app = self.create_app(PayableApp, 'receivable')
+
+        olist = app.main_window.results
+        payments = list(olist)[-2:]
+
+        for payment in payments:
+            payment.status = Payment.STATUS_PENDING
+        self.assertTrue(app.main_window._can_pay(payments))
+
+    @mock.patch('stoq.gui.payable.print_report')
+    def test_print_receipt(self, print_report):
+        purchase, payment = self.create_purchase_payment()
+        payment.pay()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[-1])
+
+        self.activate(app.main_window.PrintReceipt)
+        print_report.assert_called_once_with(OutPaymentReceipt, payment=payment,
+                                             order=purchase, date=datetime.date.today())
+
+    @mock.patch('stoq.gui.payable.PayableApp.change_status')
+    def test_cancel_payment(self, change_status):
+        payment = self.create_payment()
+        payment.status = Payment.STATUS_PENDING
+        payment.payment_type = Payment.TYPE_OUT
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[-1])
+
+        self.activate(app.main_window.CancelPayment)
+        change_status.assert_called_once_with(olist[-1], None,
+                                              Payment.STATUS_CANCELLED)
+
+    @mock.patch('stoq.gui.payable.PayableApp.change_status')
+    def test_set_not_paid(self, change_status):
+        purchase, payment = self.create_purchase_payment()
+        payment.pay()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[-1])
+
+        self.activate(app.main_window.SetNotPaid)
+        change_status.assert_called_once_with(olist[-1], purchase,
+                                              Payment.STATUS_PENDING)
+
+    @mock.patch('stoq.gui.payable.PayableApp.change_due_date')
+    def test_change_due_date(self, change_due_date):
+        purchase, payment = self.create_purchase_payment()
+
+        app = self.create_app(PayableApp, 'payable')
+        olist = app.main_window.results
+        olist.select(olist[-1])
+
+        self.activate(app.main_window.ChangeDueDate)
+        change_due_date.assert_called_once_with(olist[-1], purchase)
+
+    def test_run_search(self):
+        app = self.create_app(PayableApp, 'payable')
+        self._check_run_dialog(app.main_window, app.main_window.BillCheckSearch,
+                               OutPaymentBillCheckSearch, [])
