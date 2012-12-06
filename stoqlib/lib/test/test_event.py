51df61579baf19486fc0e26cdf04e062864d603d
Thiago Bellini <hackedbellini@async.com.br>
2012-12-06 12:25:49 -0200

    Remove pydispatch from external and use a simpler solution for events
    pydispatch isn't maintained anymore, our fork is worse (since it don't get
    updated). A solution to that would be using louie, that's a continuation of
    pydispatch, but that would just increase our dependency list with something
    that we barely use.
    The solution here is very simple and easy to maintain. Also, it's using
    weakrefs. Even that we don't use them on Stoq right now, it's prepared for it.
    The Event compatibility was kept to work exactly as it was working with
    pydispatch before, so, no api changes.
    
    r=jdahlin

--- stoqlib/lib/test/test_event.py
+++ stoqlib/lib/test/test_event.py
@@ -37,3 +37,40 @@ class EventTest(unittest.TestCase):
         # Shouln't raise
         retval = MyEvent.emit()
         self.assertTrue(isinstance(retval, ReturnStatus))
+
+    def testConnectTwice(self):
+        class MyEvent(Event):
+            returnclass = ReturnStatus
+
+        MyEvent.connect(self._stub_return_corret_value)
+        self.assertRaises(AssertionError,
+                          MyEvent.connect, self._stub_return_corret_value)
+        MyEvent.disconnect(self._stub_return_corret_value)
+        MyEvent.connect(self._stub_return_corret_value)
+
+    def testDisconnect(self):
+        class MyEvent(Event):
+            returnclass = ReturnStatus
+
+        MyEvent.connect(self._stub_return_corret_value)
+        retval = MyEvent.emit()
+        self.assertTrue(isinstance(retval, ReturnStatus))
+        MyEvent.disconnect(self._stub_return_corret_value)
+        retval = MyEvent.emit()
+        self.assertEqual(retval, None)
+
+        # Trying to disconnect something not connected
+        self.assertRaises(ValueError,
+                          MyEvent.disconnect, self._stub_return_wrong_value)
+        self.assertRaises(ValueError,
+                          MyEvent.disconnect, lambda: 666)
+
+    def testWeakRef(self):
+        class MyEvent(Event):
+            returnclass = int
+
+        # lambda will die right after this since there's no other reference
+        # to it other than the weakref on event's callbacks
+        MyEvent.connect(lambda: 666)
+        retval = MyEvent.emit()
+        self.assertEqual(retval, None)
