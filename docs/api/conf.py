4e57700393cd1e30fda7498aa5a36e2b8d7cbf6f
Johan Dahlin <jdahlin@async.com.br>
2013-02-18 11:42:17 -0300

    Jenkins: Fix pep8

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -11,7 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import os
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -272,6 +273,7 @@ coverage_ignore_modules = [
     r'stoqlib.net.*',
     r'stoqlib.reporting.*',
 ]
+
 
 def setup(app):
     from devhelp2 import Devhelp2Builder
