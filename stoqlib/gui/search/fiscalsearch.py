6febc24108f53376e8132fae9ec01e4ff39cbec0
João Sampaio <jpmelos@gmail.com>
2013-02-25 11:28:43 -0300

    FiscalBookEntrySearch: Marks edit button for translation.
    Change-Id: I5c476b61b6c6c192a9d22020c30b0ba3e55da99a

--- stoqlib/gui/search/fiscalsearch.py
+++ stoqlib/gui/search/fiscalsearch.py
@@ -142,7 +142,7 @@ class FiscalBookEntrySearch(SearchDialog):
     #
 
     def setup_widgets(self):
-        self.edit_button = self.add_button('Edit')
+        self.edit_button = self.add_button(_('Edit'))
         self.edit_button.connect('clicked', self._on_edit_button__clicked)
         self.edit_button.show()
         self.edit_button.set_sensitive(False)
