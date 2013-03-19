a42fb841356e7ac2cdaccc6d7aa9ee0166156cd5
Ronaldo Maia <romaia@async.com.br>
2013-03-19 12:06:48 -0300

    Release 1.7rc2
    Change-Id: I092ee46f82f164d4c7edc63b7f1a5f2cb75acf3d

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 7
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = 'rc2'
 
 #: the date the software was released
-release_date = (2013, 3, 15)
+release_date = (2013, 3, 19)
 
 #: if this is a stable release
 stable = True
