stoq/stoq

commit 81750ea01e1f9671ee5198f1ab94c3751734f435

Optimize POS checkout
* Reduce number of queries made by half.
* Update trade infobar in POS to always recreate
* Add new tests for Sale.get_total_sale_amount and an optional
  parameter

stoqlib/gui/wizards/salewizard.py
@@ -39,7 +39,6 @@ from stoqlib.domain.fiscal import CfopData
 from stoqlib.domain.payment.card import CreditProvider
 from stoqlib.domain.payment.method import PaymentMethod
 from stoqlib.domain.payment.payment import Payment
-from stoqlib.domain.payment.renegotiation import PaymentRenegotiation
 from stoqlib.domain.person import Client, SalesPerson, Transporter
 from stoqlib.domain.sale import Sale
 from stoqlib.enums import CreatePaymentStatus
@@ -157,7 +156,7 @@ class BaseMethodSelectionStep(object):
     def _update_next_step(self, method):
         if method and method.method_name == u'money':
             self.wizard.enable_finish()
-            if self.need_create_payment():
+            if self.wizard.need_create_payment():
                 self.cash_change_slave.enable_cash_change()
             else:
                 self.cash_change_slave.disable_cash_change()
@@ -165,29 +164,16 @@ class BaseMethodSelectionStep(object):
             self.wizard.disable_finish()
             self.cash_change_slave.disable_cash_change()
 
-    def _get_total_amount(self):
-        if isinstance(self.model, Sale):
-            return self.model.get_total_to_pay()
-        elif isinstance(self.model, PaymentRenegotiation):
-            return self.model.total
-        else:
-            raise TypeError
-
     #
     #   Public API
     #
-
-    def need_create_payment(self):
-        # If the sale is already paid, we cannot let the user create more,
-        # or even edit the existing ones.
-        return self._get_total_amount() > 0
 
     def get_selected_method(self):
         return self.pm_slave.get_selected_method()
 
     def setup_cash_payment(self, total=None):
         money_method = PaymentMethod.get_by_name(self.store, u'money')
-        total = total or self._get_total_amount()
+        total = total or self.wizard.get_total_amount()
         return money_method.create_inpayment(self.model.group,
                                              self.model.branch, total)
 
@@ -196,7 +182,7 @@ class BaseMethodSelectionStep(object):
     #
 
     def post_init(self):
-        if not self.need_create_payment():
+        if not self.wizard.need_create_payment():
             for widget in [self.select_method_holder,
                            self.subtotal_expander]:
                 widget.hide()
@@ -211,13 +197,13 @@ class BaseMethodSelectionStep(object):
         self.attach_slave('select_method_holder', self.pm_slave)
 
         marker('CashChangeSlave')
-        self.cash_change_slave = CashChangeSlave(self.store, self.model)
+        self.cash_change_slave = CashChangeSlave(self.store, self.model, self.wizard)
         self.attach_slave('cash_change_holder', self.cash_change_slave)
         self.cash_change_slave.received_value.connect(
             'activate', lambda entry: self.wizard.go_to_next())
 
     def next_step(self):
-        if not self.need_create_payment():
+        if not self.wizard.need_create_payment():
             return
 
         selected_method = self.get_selected_method()
@@ -241,7 +227,7 @@ class BaseMethodSelectionStep(object):
             return None
         elif selected_method.method_name == u'store_credit':
             client = self.model.client
-            total = self._get_total_amount()
+            total = self.wizard.get_total_amount()
 
             assert client.can_purchase(selected_method, total)
 
@@ -266,7 +252,7 @@ class BaseMethodSelectionStep(object):
             if selected_method.method_name == 'multiple':
                 outstanding_value = None
             else:
-                outstanding_value = self._get_total_amount()
+                outstanding_value = self.wizard.get_total_amount()
 
             return step_class(self.wizard, self, self.store, self.model,
                               selected_method,
@@ -293,11 +279,8 @@ class SalesPersonStep(BaseMethodSelectionStep, WizardEditorStep):
     """
     gladefile = 'SalesPersonStep'
     model_type = Sale
-    proxy_widgets = ('total_lbl',
-                     'subtotal_lbl',
-                     'salesperson',
+    proxy_widgets = ('salesperson',
                      'client',
-                     'total_paid_lbl',
                      'transporter',
                      'cost_center')
 
@@ -314,6 +297,7 @@ class SalesPersonStep(BaseMethodSelectionStep, WizardEditorStep):
         marker("WizardEditorStep.__init__")
         WizardEditorStep.__init__(self, store, wizard, model)
 
+        self._update_totals()
         self.update_discount_and_surcharge()
 
     #
@@ -321,9 +305,15 @@ class SalesPersonStep(BaseMethodSelectionStep, WizardEditorStep):
     #
 
     def _update_totals(self):
-        for field_name in ['total_to_pay', 'total_paid', 'sale_subtotal']:
-            self.proxy.update(field_name)
-        self.cash_change_slave.update_total_sale_amount()
+        subtotal = self.wizard.get_subtotal()
+        self.subtotal_lbl.update(subtotal)
+
+        total_paid = self.wizard.get_total_paid()
+        self.total_paid_lbl.update(total_paid)
+
+        to_pay = self.model.get_total_sale_amount(subtotal=subtotal) - total_paid
+        self.cash_change_slave.update_total_sale_amount(to_pay)
+        self.total_lbl.update(to_pay)
 
     def _update_widgets(self):
         has_client = bool(self.client.get_selected())
@@ -484,11 +474,10 @@ class SalesPersonStep(BaseMethodSelectionStep, WizardEditorStep):
 
         marker('Entering post_init')
         self.toogle_client_details()
-        if self.need_create_payment():
+        if self.wizard.need_create_payment():
             self.wizard.payment_group.clear_unused()
         self.register_validate_function(self._refresh_next)
         self._update_next_step(self.get_selected_method())
-
         if hasattr(self, 'cash_change_slave'):
             self.cash_change_slave.received_value.grab_focus()
 
@@ -625,9 +614,27 @@ class ConfirmSaleWizard(BaseWizard):
     title = _("Sale Checkout")
     help_section = 'sale-confirm'
 
-    def __init__(self, store, model):
+    # FIXME: In the long term, we should only create the sale at the end
+    #        of this process, but that requires major surgery of the
+    #        interaction between salewizard.py, pos.py and fiscalprinter.py
+    def __init__(self, store, model,
+                 subtotal,
+                 total_paid=0):
+        """Creates a new SaleWizard that confirms a sale.
+        To avoid excessive querying of the database we pass
+        some data already queried/calculated before hand.
+
+        :param store: a store
+        :param model: a |sale|
+        :param subtotal: subtotal of the sale
+        :param total_paid: totaly value already paid
+        """
         marker('ConfirmSaleWizard')
         self._check_payment_group(model, store)
+
+        self._subtotal = subtotal
+        self._total_paid = total_paid
+        self.model = model
 
         # invoice_model is a Settable so avoid bug 4218, where more
         # than one checkout may try to use the same invoice number.
@@ -657,7 +664,35 @@ class ConfirmSaleWizard(BaseWizard):
 
     def _invoice_changed(self):
         return (self.invoice_model.invoice_number !=
-                    self.invoice_model.original_invoice)
+                self.invoice_model.original_invoice)
+
+    def get_subtotal(self):
+        """Fetch the sale subtotal without querying the database.
+        The subtotal is the value of all items that are being sold
+
+        :returns: the subtotal of the current sale
+        """
+        return self._subtotal
+
+    def get_total_amount(self):
+        """Fetch the total sale amount without querying the database.
+        The total sale amount is the subtotal with discount and markups
+        taken into account.
+
+        :returns: the total amount of the current sale
+        """
+        return self.model.get_total_sale_amount(subtotal=self._subtotal)
+
+    def get_total_paid(self):
+        """Fetch the value already paid for this sale.
+        This is only used when we return a project we already paid for.
+
+        :returns: the total paid value for the current sale
+        """
+        return self._total_paid
+
+    def need_create_payment(self):
+        return self.get_total_amount() > 0
 
     def finish(self):
         self.retval = True
@@ -674,8 +709,8 @@ class ConfirmSaleWizard(BaseWizard):
                 self.store.rollback_to_savepoint('before_set_invoice_number')
                 if self._invoice_changed():
                     warning(_(u"The invoice number %s is already used. "
-                       "Confirm the sale again to chose another one.") %
-                       invoice_number)
+                              "Confirm the sale again to chose another one.") %
+                            invoice_number)
                     self.retval = False
                     break
                 else:

stoqlib/gui/test/test_sale_wizard.py
@@ -45,7 +45,7 @@ class TestConfirmSaleWizard(GUITest):
     def testCreate(self):
         sale = self.create_sale()
         self.add_product(sale)
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
 
         self.click(wizard.next_button)
 
@@ -62,7 +62,7 @@ class TestConfirmSaleWizard(GUITest):
         cost_center = self.create_cost_center()
         sale.identifier = 56782
         self.add_product(sale)
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
         step.cost_center.select(cost_center)
         self.check_wizard(wizard, 'wizard-sale-with-cost-center')
@@ -83,7 +83,7 @@ class TestConfirmSaleWizard(GUITest):
         sale = self.create_sale()
         sale.identifier = 12345
         self.add_product(sale)
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
         step.pm_slave.select_method('check')
         self.click(wizard.next_button)
@@ -99,7 +99,7 @@ class TestConfirmSaleWizard(GUITest):
         sale = self.create_sale()
         sale.identifier = 12345
         self.add_product(sale)
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
         step.pm_slave.select_method('bill')
         self.click(wizard.next_button)
@@ -119,7 +119,7 @@ class TestConfirmSaleWizard(GUITest):
         sale = self.create_sale()
         sale.identifier = 12345
         self.add_product(sale)
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
         step.pm_slave.select_method('card')
         self.click(wizard.next_button)
@@ -135,7 +135,7 @@ class TestConfirmSaleWizard(GUITest):
         sale = self.create_sale()
         sale.identifier = 12345
         self.add_product(sale)
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
         step.pm_slave.select_method('deposit')
         self.click(wizard.next_button)
@@ -154,7 +154,7 @@ class TestConfirmSaleWizard(GUITest):
         self.add_product(sale)
         sale.client = self.create_client()
         sale.client.credit_limit = 1000
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
         step.pm_slave.select_method(u'store_credit')
         self.click(wizard.next_button)
@@ -165,7 +165,7 @@ class TestConfirmSaleWizard(GUITest):
         sale.identifier = 12345
         self.add_product(sale)
         sale.client = self.create_client()
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
         step.pm_slave.select_method(u'store_credit')
         self.assertEquals(
@@ -183,7 +183,7 @@ class TestConfirmSaleWizard(GUITest):
         sale.identifier = 12345
         self.add_product(sale)
         sale.client = self.create_client()
-        wizard = ConfirmSaleWizard(self.store, sale)
+        wizard = ConfirmSaleWizard(self.store, sale, sale.get_total_sale_amount())
         step = wizard.get_current_step()
 
         money_method = PaymentMethod.get_by_name(self.store, u'money')

data/glade/SalesPersonStep.ui
@@ -494,7 +494,6 @@
                         <property name="label" translatable="yes">0.00</property>
                         <property name="justify">right</property>
                         <property name="data_type">currency</property>
-                        <property name="model_attribute">sale_subtotal</property>
                       </object>
                       <packing>
                         <property name="left_attach">1</property>
@@ -552,7 +551,6 @@
                         <property name="label" translatable="yes">0.00</property>
                         <property name="justify">right</property>
                         <property name="data_type">currency</property>
-                        <property name="model_attribute">total_paid</property>
                       </object>
                       <packing>
                         <property name="left_attach">1</property>

stoq/gui/pos.py
@@ -124,6 +124,7 @@ class PosApp(AppWindow):
         self._current_store = None
         self._trade = None
         self._trade_infobar = None
+        self._trade_total_paid = 0
 
         AppWindow.__init__(self, app, store=store)
 
@@ -667,13 +668,11 @@ class PosApp(AppWindow):
         self._current_store = None
 
     def _clear_trade(self, remove=False):
-        if not self._trade:
-            return
-
-        if remove:
+        if self._trade and remove:
             self._trade.remove()
         self._trade = None
-        self._update_trade_infobar()
+        self._trade_total_paid = 0
+        self._remove_trade_infobar()
 
     def _edit_sale_item(self, sale_item):
         if sale_item.service:
@@ -824,9 +823,11 @@ class PosApp(AppWindow):
         else:
             assert self._coupon
 
-            ordered = self._coupon.confirm(sale, store, savepoint)
-            # Dont call finish_store here, since confirm() above did it
-            # already
+            ordered = self._coupon.confirm(sale, store, savepoint,
+                                           subtotal=self._get_subtotal(),
+                                           total_paid=self._trade_total_paid)
+            # Dont call store.confirm() here, since coupon.confirm()
+            # above already did it
             if not ordered:
                 # FIXME: Move to TEF plugin
                 manager = get_plugin_manager()
@@ -863,20 +864,26 @@ class PosApp(AppWindow):
         else:
             self._add_sale_item(search_str)
 
-    def _update_trade_infobar(self):
-        if self._trade and self._trade_infobar:
-            self._trade_infobar.show()
-        elif not self._trade and self._trade_infobar:
-            self._trade_infobar.hide()
-        elif self._trade:
-            button = gtk.Button(_("Cancel trade"))
-            button.connect('clicked', self._on_remove_trade_button__clicked)
-            value = converter.as_string(currency, self._trade.returned_total)
-            msg = _("There is a trade with value %s in progress...\n"
-                    "When checking out, it will be used as part of "
-                    "the payment.") % (value, )
-            self._trade_infobar = self.add_info_bar(gtk.MESSAGE_INFO, msg,
-                                                    action_widget=button)
+    def _remove_trade_infobar(self):
+        if not self._trade_infobar:
+            return
+        self._trade_infobar.destroy()
+        self._trade_infobar = None
+
+    def _show_trade_infobar(self, trade):
+        self._remove_trade_infobar()
+        if not trade:
+            return
+
+        button = gtk.Button(_("Cancel trade"))
+        button.connect('clicked', self._on_remove_trade_button__clicked)
+        value = converter.as_string(currency, self._trade.returned_total)
+        msg = _("There is a trade with value %s in progress...\n"
+                "When checking out, it will be used as part of "
+                "the payment.") % (value, )
+        self._trade_infobar = self.add_info_bar(gtk.MESSAGE_INFO, msg,
+                                                action_widget=button)
+        self._trade_total_paid += self._trade.returned_total
 
     #
     # Coupon related
@@ -1000,7 +1007,7 @@ class PosApp(AppWindow):
         if self._trade:
             if yesno(_("There is already a trade in progress... Do you "
                        "want to cancel it and start a new one?"),
-                     gtk.RESPONSE_NO, _("Cancel trade"), _("Don't cancel")):
+                     gtk.RESPONSE_NO, _("Cancel trade"), _("Finish trade")):
                 self._clear_trade(remove=True)
             else:
                 return
@@ -1011,16 +1018,16 @@ class PosApp(AppWindow):
         else:
             store = api.new_store()
 
-        rv = self.run_dialog(SaleTradeWizard, store)
-        if rv:
-            self._trade = rv
+        trade = self.run_dialog(SaleTradeWizard, store)
+        if trade:
+            self._trade = trade
             self._current_store = store
         elif self._current_store:
             store.rollback_to_savepoint('before_run_wizard_saletrade')
         else:
             store.rollback(close=True)
 
-        self._update_trade_infobar()
+        self._show_trade_infobar(trade)
 
     #
     # Other callbacks

stoq/gui/test/test_pos.py
@@ -87,7 +87,8 @@ class TestPos(BaseGUITest):
         self._add_product(pos, sellable)
         return service
 
-    def _auto_confirm_sale_wizard(self, wizard, app, store, sale):
+    def _auto_confirm_sale_wizard(self, wizard, app, store, sale,
+                                  subtotal, total_paid):
         # This is in another store and as we want to avoid committing
         # we need to open the till again
         self._open_till(store)
@@ -161,7 +162,8 @@ class TestPos(BaseGUITest):
         sellable = self.store.find(Sellable)[0]
         sale_item = self._add_product(pos, sellable)
 
-        def mock_confirm(sale, store, savepoint=None):
+        def mock_confirm(sale, store, savepoint=None,
+                         subtotal=None, total_paid=None):
             return True
 
         with mock.patch.object(pos._coupon, 'confirm', mock_confirm):

commit fcd1949f14e38afa7543fa235818c75bbd000e9b

Fix a problem when selling the same value traded
This is a regression from commit 81750ea0.

When buying the same amount that was traded, it was generating a traceback when
the wizard wanted to create a payment with value 0.

When the total traded is equal to the sale's total, it should not be possible
to create more payments. The problem here was that we should use
get_total_to_pay instead of get_total_amount. That's true for the
store_credit's step.

Change-Id: If471afc23494f1a4f44ded742a6690e08ae1ba2f

stoqlib/gui/wizards/salewizard.py
@@ -227,7 +227,7 @@ class BaseMethodSelectionStep(object):
             return None
         elif selected_method.method_name == u'store_credit':
             client = self.model.client
-            total = self.wizard.get_total_amount()
+            total = self.wizard.get_total_to_pay()
 
             assert client.can_purchase(selected_method, total)
 
@@ -700,7 +700,7 @@ class ConfirmSaleWizard(BaseWizard):
         return self.get_total_amount() - self.get_total_paid()
 
     def need_create_payment(self):
-        return self.get_total_amount() > 0
+        return self.get_total_to_pay() > 0
 
     def finish(self):
         self.retval = True

stoqlib/domain/test/domaintest.py
@@ -234,6 +234,8 @@ class DomainTest(unittest.TestCase, ExampleCreator):
             stock_item = item.sellable.product_storable.get_stock_item(branch)
             models.append(stock_item)
             models.append(item)
-        p = list(sale.payments)[0]
-        p.description = p.description.rsplit(u' ', 1)[0]
+        payments = list(sale.payments)
+        if len(payments):
+            p = payments[0]
+            p.description = p.description.rsplit(u' ', 1)[0]
         return models

stoqlib/gui/test/test_sale_wizard.py
@@ -114,6 +114,21 @@ class TestConfirmSaleWizard(GUITest):
         # Since $30 was already paid, the client only had to pay $70
         self.assertEquals(self.sale.payments[0].value, 7)
 
+    def testSaleWithTradeSameValue(self):
+        self._create_wizard(total_paid=10)
+        self._go_to_next()
+
+        self.assertFalse(self.wizard.need_create_payment())
+        self.assertNotVisible(self.step, ['select_method_holder',
+                                          'subtotal_expander'])
+        self.assertNotSensitive(self.step.cash_change_slave,
+                                ['received_value'])
+
+        self._check_wizard('wizard-sale-with-trade-same-value')
+
+        # No payment created, since the client already paid the whole value
+        self.assertEquals(self.sale.payments.count(), 0)
+
     @mock.patch('stoqlib.gui.wizards.salewizard.yesno')
     def testSaleWithCostCenter(self, yesno):
         cost_center = self.create_cost_center()

tests/data/ui/wizard-sale-with-trade-same-value.uitest
@@ -1,0 +1,153 @@
+wizard: ConfirmSaleWizard
+GtkDialog(PluggableWizard): title='Sale Checkout', hidden
+  GtkVBox(PluggableWizard-vbox):
+    GtkVBox(vbox1, expand=True, fill=True):
+      ProxyLabel(header_lbl, fill=True): hidden
+      GtkEventBox(slave_area, expand=True, fill=True): slave SalesPersonStep is attached
+        GtkVBox(step.toplevel):
+          GtkHBox(step.hbox1, fill=True):
+            GtkVBox(step.vbox3, expand=True, fill=True):
+              GtkTable(step.table3, fill=True):
+                ProxyLabel(step.salesperson_lbl): 'Salesperson:'
+                ProxyComboEntry(step.salesperson): insensitive
+                  item: 'Administrator'
+                  item: 'Cleusa Blanc Ferreira Norbiato'
+                  item: 'Deivis Alexandre Junior'
+                  item: 'Maria Aparecida Ardana'
+                  item: 'Osvaldo Denadai Marin'
+                  item: 'SalesPerson', selected
+                GtkLabel(step.label1): 'Client:'
+                ProxyComboEntry(step.client):
+                  item: 'Alessandra Almeida Itaber\xc3\xa1'
+                  item: 'Franciso Elisio de Lima Junior'
+                  item: 'Luis Sergio da Silva Marin'
+                  item: 'Vitalina Claudino'
+                GtkButton(step.create_client):
+                GtkButton(step.client_details): insensitive
+                GtkLabel(step.transporter_lbl): 'Transporter:'
+                ProxyComboEntry(step.transporter):
+                  item: 'Jos\xc3\xa9 Carlos de Carvalho'
+                GtkButton(step.create_transporter):
+                GtkLabel(step.nature_lbl): 'Operation Nature:', hidden
+                ProxyEntry(step.operation_nature): '', hidden
+            GtkVBox(step.vbox4, expand=True, fill=True):
+              GtkTable(step.table4, fill=True):
+                GtkLabel(step.invoice_number_label): 'Invoice Number:'
+                ProxySpinButton(step.invoice_number): '1004'
+                GtkLabel(step.expire_label): 'Expire', hidden
+                ProxyDateEntry(step.expire_date): None, hidden
+                GtkLabel(step.cfop_lbl): 'C.F.O.P.:', hidden
+                ProxyComboEntry(step.cfop): hidden
+                GtkButton(step.create_cfop): hidden
+                GtkLabel(step.client_category_lbl): 'Client Category:', hidden
+                ProxyComboBox(step.client_category): hidden
+                GtkLabel(step.cost_center_lbl): 'Cost Center:', hidden
+                ProxyComboBox(step.cost_center): hidden
+          GtkHSeparator(step.hseparator1, fill=True):
+          GtkVBox(step.total_box, fill=True):
+            GtkHBox(step.hbox2, fill=True):
+              GtkTable(step.table1, expand=True, fill=True):
+                ProxyLabel(step.kiwilabel3): 'Subtotal:'
+                ProxyLabel(step.subtotal_lbl): '<span >$10.00</span>'
+                ProxyLabel(step.kiwilabel1): 'Already paid:'
+                ProxyLabel(step.total_paid_lbl): '<span >$10.00</span>'
+                ProxyLabel(step.kiwilabel5): 'Total:'
+                ProxyLabel(step.total_lbl): '<span >$ 0.00</span>'
+              GtkLabel(step.label3, expand=True, fill=True):
+              GtkHButtonBox(step.hbuttonbox1, pack-end):
+                GtkButton(step.notes_button, pack-end): 'Notes...'
+            GtkHBox(step.hbox4, expand=True, fill=True):
+              GtkEventBox(step.cash_change_holder, expand=True, fill=True): slave CashChangeSlave is attached
+                GtkVBox(toplevel):
+                  GtkTable(table1, padding=6):
+                    ProxyLabel(title_lbl): '<span underline="single">Cash Change</span>'
+                    ProxyLabel(kiwilabel1): 'Received: ($CURRENCY)'
+                    ProxyEntry(received_value): '$ 0.00', insensitive
+                    ProxyLabel(kiwilabel2): 'Change:'
+                    ProxyLabel(change_value_lbl): '<span weight="bold">$ 0.00</span>'
+              GtkExpander(step.subtotal_expander, expand=True, fill=True): hidden
+                GtkEventBox(step.discount_surcharge_slave): slave SaleDiscountSlave is attached
+                  GtkVBox(toplevel):
+                    GtkHBox(hbox6, fill=True):
+                      ProxyLabel(kiwilabel9, fill=True): 'Discount:'
+                    GtkTable(table1, fill=True):
+                      ProxyRadioButton(discount_perc_ck): active
+                      GtkHBox(hbox2):
+                        ProxyEntry(discount_perc, fill=True): '0.0'
+                        ProxyLabel(kiwilabel1, fill=True): '%'
+                      ProxyRadioButton(discount_value_ck):
+                      GtkHBox(hbox4):
+                        ProxyEntry(discount_value, fill=True): '0.00', insensitive
+                        GtkLabel(label4, expand=True, fill=True):
+                GtkLabel(step.label2): 'Order Details'
+          GtkEventBox(step.select_method_holder, fill=True, pack-end): slave SelectPaymentMethodSlave is attached, hidden
+            GtkVBox(toplevel):
+              GtkFrame(frame1, expand=True, fill=True):
+                GtkHBox(methods_box):
+                  GtkRadioButton(padding=6): 'Money', active
+                  GtkRadioButton(padding=6): 'Bill', hidden
+                  GtkRadioButton(padding=6): 'Card'
+                  GtkRadioButton(padding=6): 'Check'
+                  GtkRadioButton(padding=6): 'Deposit'
+                  GtkRadioButton(padding=6): 'Store Credit', hidden
+                  GtkRadioButton(padding=6): 'Multiple'
+                GtkLabel(): 'Payment method:'
+      ProxyLabel(notification_lbl, fill=True): hidden
+      GtkHBox(hbox1, fill=True, pack-end):
+        GtkHButtonBox(buttonbox, expand=True, fill=True, pack-end):
+          GtkButton(cancel_button): 'gtk-cancel'
+          GtkButton(previous_button): 'gtk-go-back', insensitive
+          GtkButton(next_button): '_Finish'
+          GtkButton(ok_button): 'gtk-ok', hidden
+          GtkButton(help_button): 'gtk-help'
+    GtkHButtonBox(dialog-action_area1, fill=True, pack-end):
+
+model: Sale<1>
+  cancel_date: None
+  close_date: None
+  confirm_date: None
+  discount_value: <currency $ 0>
+  expire_date: None
+  group_id: PaymentGroup<2>
+  invoice_number: 1004
+  notes: u''
+  open_date: datetime.today()
+  operation_nature: u''
+  return_date: None
+  service_invoice_number: None
+  status: 4
+  surcharge_value: <currency $ 0>
+  total_amount: <currency $ 0>
+
+model: PaymentGroup<2>
+
+model: Sellable<3>
+  barcode: u''
+  base_price: <currency $10>
+  code: u''
+  commission: <currency $ 0>
+  cost: <currency $125>
+  description: u'Description'
+  max_discount: Decimal('0')
+  notes: u''
+  on_sale_end_date: None
+  on_sale_price: <currency $ 0>
+  on_sale_start_date: None
+  status: 0
+
+model: ProductStockItem<4>
+  quantity: Decimal('100.000')
+  stock_cost: <currency $ 0>
+
+model: SaleItem<5>
+  average_cost: <currency $ 0>
+  base_price: <currency $10>
+  completion_date: None
+  estimated_fix_date: datetime.today()
+  notes: None
+  price: <currency $10>
+  quantity: Decimal('1.000')
+  quantity_decreased: Decimal('0.000')
+  sale_id: Sale<1>
+  sellable_id: Sellable<3>
+