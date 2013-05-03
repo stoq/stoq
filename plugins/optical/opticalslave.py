2f1ce3d1bd7cc02d494dfb6492d82d3d570878ee
Ronaldo Maia <romaia@async.com.br>
2013-05-03 16:50:02 -0300

    Improve domain for work order optical data.
    Also save the details in the work order editor
    
    Change-Id: I37211ade588607c6ced459b75ca793315d554df6

--- plugins/optical/opticalslave.py
+++ plugins/optical/opticalslave.py
@@ -21,7 +21,9 @@
 ##
 ## Author(s): Stoq Team <stoq-devel@async.com.br>
 ##
-""" Slaves for books """
+""" Slaves for optical stores """
+
+import gtk
 
 from stoqlib.gui.editors.baseeditor import BaseEditorSlave
 from stoqlib.lib.translation import stoqlib_gettext
@@ -86,9 +88,33 @@ class WorkOrderOpticalSlave(BaseEditorSlave):
     gladefile = 'WorkOrderOpticalSlave'
     title = _(u'Optical Details')
     model_type = object
-    proxy_widgets = ['prescription_date', 'od_esferico', 'oe_esferico',
-                     'patient']
+    proxy_widgets = ['prescription_date', 'patient']
     workorder_widgets = ['estimated_finish']
+
+    # This is a dictionary of specifications for each widget in the slave. each item
+    # in this dict should be a touple that defines the following:
+    #   - Minimum allowed value
+    #   - Maximum allowed value
+    #   - Number of decimal places that should be used
+    #   - Step increment for the adjustment
+    #   - Page increment for the adjustment
+    # The key shoud be the name of the widget (There are actually 2 widgets, one
+    # for each eye)
+    # TODO: Find out the correct values here.
+    optical_widgets = {
+        'distance_spherical': (0, 30, 2, 0.25, 1),
+        'distance_cylindrical': (0, 30, 2, 0.25, 1),
+        'distance_axis': (0, 30, 2, 0.25, 1),
+        'distance_pd': (0, 30, 2, 0.25, 1),
+        'distance_prism': (0, 30, 2, 0.25, 1),
+        'distance_base': (0, 30, 2, 0.25, 1),
+        'distance_height': (0, 30, 2, 0.25, 1),
+        'addition': (0, 30, 2, 0.25, 1),
+        'near_spherical': (0, 30, 2, 0.25, 1),
+        'near_cylindrical': (0, 30, 2, 0.25, 1),
+        'near_axis': (0, 30, 2, 0.25, 1),
+        'near_pd': (0, 30, 2, 0.25, 1),
+    }
 
     def __init__(self, store, workorder, show_finish_date=False):
         """
@@ -110,10 +136,33 @@ class WorkOrderOpticalSlave(BaseEditorSlave):
             model = OpticalWorkOrder(work_order=self._workorder, store=store)
         return model
 
+    def _setup_widgets(self):
+        """This will setup the adjustments for the prescription widgets, and
+        will return a list with all widget names, so they can be added to the
+        proxy later.
+        """
+        widget_names = []
+        for eye in ['le', 're']:
+            for element in self.optical_widgets:
+                name = eye + '_' + element
+                widget_names.append(name)
+                widget = getattr(self, name)
+
+                lower, upper, digits, step, page = self.optical_widgets[element]
+                widget.set_adjustment(gtk.Adjustment(lower=lower, upper=upper,
+                                                     step_incr=step, page_incr=page))
+                widget.set_digits(digits)
+
+        return widget_names
+
     def setup_proxies(self):
-        self.proxy = self.add_proxy(self.model, self.proxy_widgets)
+        widgets = self._setup_widgets()
+        self.add_proxy(self.model, self.proxy_widgets + widgets)
+
+        # Finish date should only be visible if we are creating a new workorder
+        # from the sale wizard
         if self._show_finish_date:
-            self.wo_proxy = self.add_proxy(self._workorder, self.workorder_widgets)
+            self.add_proxy(self._workorder, self.workorder_widgets)
         else:
             self.estimated_finish_lbl.hide()
             self.estimated_finish.hide()
