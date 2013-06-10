30cf511ab9e151f85c1ec0e007c567902b7d9c22
Ronaldo Maia <romaia@async.com.br>
2013-06-10 12:16:43 -0300

    Release 1.7.2
    Change-Id: I70c851378aec883d92cc3385035c020fa20655c6

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -34,13 +34,13 @@ major_version = 1
 minor_version = 7
 
 #: Micro version, increase when doing a bug fix for a stable release
-micro_version = 1
+micro_version = 2
 
 #: extra version, rc1, rc2, etc goes here.
 extra_version = 0
 
 #: the date the software was released
-release_date = (2013, 4, 25)
+release_date = (2013, 6, 10)
 
 #: if this is a stable release
 stable = True
