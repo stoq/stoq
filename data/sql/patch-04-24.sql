e992595ddb321cc1722073b7a0df4091d895dd19
João Sampaio <jpmelos@gmail.com>
2013-06-19 11:00:46 -0300

    Fix patch-04-24.sql.
    Change-Id: Ic7e9e3c18585db39c6579d8f8927e37a29d2e6cd

--- data/sql/patch-04-24.sql
+++ data/sql/patch-04-24.sql
@@ -1,8 +1,7 @@
-
+-- Remove no longer used tables.
 
 DROP TABLE credit_card_details;
 DROP TABLE debit_card_details;
-DROP TABLE card_installment_settings;
 DROP TABLE card_installments_store_details;
 DROP TABLE card_installments_provider_details;
-
+DROP TABLE card_installment_settings;
