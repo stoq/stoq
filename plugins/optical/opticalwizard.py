15512eee128d6ff7159c1bc117dfe9d581674a02
Ronaldo Maia <romaia@async.com.br>
2013-06-24 07:59:39 -0300

    Fix price validation on optical sale wizard
    Change-Id: I7da9ca58adbb9e6f3bf0d272dcb396abac139741

--- plugins/optical/opticalwizard.py
+++ plugins/optical/opticalwizard.py
@@ -42,6 +42,7 @@ from stoqlib.gui.base.dialogs import run_dialog
 from stoqlib.gui.base.wizards import BaseWizardStep, WizardEditorStep
 from stoqlib.gui.dialogs.batchselectiondialog import BatchDecreaseSelectionDialog
 from stoqlib.gui.dialogs.clientdetails import ClientDetailsDialog
+from stoqlib.gui.dialogs.credentialsdialog import CredentialsDialog
 from stoqlib.gui.editors.baseeditor import BaseEditor
 from stoqlib.gui.editors.noteeditor import NoteEditor
 from stoqlib.gui.editors.personeditor import ClientEditor
@@ -398,6 +399,48 @@ class _ItemSlave(SellableItemSlave):
             Column('total', title=_(u'Total'),
                    data_type=currency),
         ]
+
+    def _validate_sellable_price(self, price):
+        s = self.proxy.model.sellable
+        category = self.model.client_category
+        default_price = s.get_price_for_category(category)
+
+        if price > default_price:
+            if not sysparam(self.store).ALLOW_HIGHER_SALE_PRICE:
+                return ValidationError(
+                    _(u'The sell price cannot be greater than %s.') %
+                    default_price)
+
+        self.manager = self.manager or api.get_current_user(self.store)
+
+        valid_data = s.is_valid_price(price, category, self.manager)
+        if not valid_data['is_valid']:
+            return ValidationError(
+                _(u'Max discount for this product is %.2f%%.' %
+                  valid_data['max_discount']))
+
+    #
+    # Callbacks
+    #
+
+    def on_cost__validate(self, widget, value):
+        if not self.proxy.model.sellable:
+            return
+
+        if value <= Decimal(0):
+            return ValidationError(_(u"The price must be greater than zero."))
+        return self._validate_sellable_price(value)
+
+    def on_cost__icon_press(self, entry, icon_pos, event):
+        if icon_pos != gtk.ENTRY_ICON_PRIMARY:
+            return
+
+        # Ask for the credentials of a different user that can possibly allow a
+        # bigger discount.
+        toplevel = self.get_toplevel()
+        self.manager = run_dialog(CredentialsDialog, toplevel.get_toplevel(), self.store)
+        if self.manager:
+            self.cost.validate(force=True)
 
 
 class OpticalItemStep(BaseWizardStep):
