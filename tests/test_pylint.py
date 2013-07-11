3ca3f09e76479f96c5957745a75e472d90f612c7
Johan Dahlin <jdahlin@async.com.br>
2013-07-11 10:45:45 -0300

    Remove pylint.sh
    And migrate the list of warnings to test_pylint.py
    
    Change-Id: I3b795a9bb687afb8039f832b0dc014d155474160

--- tests/test_pylint.py
+++ tests/test_pylint.py
@@ -30,6 +30,61 @@ import stoqlib
 from stoqlib.lib.process import Process
 
 
+DISABLED = [
+    # Reports
+    'similarities',  # Similarity report
+
+    # Convention
+    'C0103',  # Invalid name "xxx" for type variable
+              #  (should match [a-z_][a-z0-9_]{2,30}$)
+    'C0111',  # Missing docstring
+    'C0112',  # TODO: Empty docstring
+    'C0202',  # Class method xxx should have 'cls' as first argument
+    'C0301',  # Line too long
+    'C0302',  # Too many lines in module
+
+    # Errors
+    'E0211',  # Method has no argument - Interfaces
+    'E0213',  # Method should have "self" as first argument - Interfaces
+    'E0611',  # Pylint is confused about GTK
+
+    # Fatal
+    'F0202',  # Bug in pylint
+    #'F0203',  # Bug in pylint (Unable to resolve gtk.XXXX)
+
+    # Info
+    'I0011',  # Locally disabling
+
+    # Refactor
+    'R0201',  # Method could be a function
+    'R0902',  # Too many instance attributes
+    'R0903',  # Too few public methods
+    'R0904',  # Too many public methods
+    'R0911',  # Too many return statements
+    'R0912',  # Too many branches
+    'R0913',  # Too many arguments
+    'R0914',  # Too many local variables
+
+    # Warnings
+    'W0101',  # TODO: Unreachable code
+    'W0104',  # Statement seems to have no effect
+    'W0109',  # TODO: Duplicate key 'XX' in dictionary
+    'W0142',  # Used * or ** magic
+    'W0201',  # Attribute 'loaded_uis' defined outside __init__
+    'W0212',  # Method could be a function (SQLObject from/to_python)
+    'W0222',  # Signature differs from overriden method
+    'W0221',  # Arguments number differs from overriden method
+    'W0223',  # Method 'add' is abstract in class 'xxx' but is not overriden
+    'W0232',  # Class has no __init__ method
+    'W0404',  # TODO: Reimport 'XX'
+    'W0511',  # FIXME/TODO/XXX
+    'W0613',  # Unused argument
+    'W0621',  # Redefining name 'xxx' from outer scope
+    'W0622',  # Redefined built-in variable
+    'W0704',  # Except doesn't do anything
+]
+
+
 class TestPylint(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
@@ -38,9 +93,12 @@ class TestPylint(unittest.TestCase):
 
     def test_stoqlib_domain(self):
         args = ["pylint",
-                "--rcfile=%s/tools/pylint.rcfile" % (self.root,),
+                "--dummy-variables=unused,_",
+                "--disable=%s" % (",".join(DISABLED)),
+                "--include-ids=y",
                 "--load-plugins", "tools/pylint_stoq",
-                "-E",
+                "--rcfile=%s/tools/pylint.rcfile" % (self.root,),
+                "--reports=n",
                 "stoqlib.domain"]
         p = Process(args)
         retval = p.wait()
