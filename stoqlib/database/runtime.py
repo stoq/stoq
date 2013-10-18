stoq/stoq

commit e09172646b78a42d9eea6d5b1a10cddf21890b07

Register a new station in the current branch.
Use the parameter MAIN_COMPANY when registering a new branch, instead of
using the first branch in the results.

Change-Id: Ia6945db90b504bc054c3a6a8ad656f659ec9ae87

stoqlib/database/runtime.py
@@ -628,31 +628,19 @@ def _register_branch(caller_store, station_name):
         if not user:
             error(_("Must login as 'admin'"))
 
-    from stoqlib.domain.person import Branch
     from stoqlib.domain.station import BranchStation
 
-    branches = caller_store.find(Branch)
-    if branches.is_empty():
-        error(_("Schema error, no branches found"))
-
-    # TODO
-    # Always select the first branch as the main branch, until we
-    # support multiple branches properly. And then, provide a way to the
-    # user choose which one will be the main branch.
-    branch = branches[0]
-
     store = new_store()
+    branch = sysparam().get_object(store, 'MAIN_COMPANY')
     try:
         station = BranchStation.create(store,
-                                       branch=store.fetch(branch),
+                                       branch=branch,
                                        name=station_name)
     except StoqlibError as e:
         error(_("ERROR: %s") % e)
 
-    station_id = station.id
     store.commit(close=True)
-
-    return caller_store.find(BranchStation, id=station_id).one()
+    return caller_store.fetch(station)
 
 
 def set_current_branch_station(store, station_name):
@@ -661,13 +649,6 @@ def set_current_branch_station(store, station_name):
     :param store: a store
     :param station_name: name of the station to register
     """
-
-    # This might be called early, so make sure SQLObject
-    # knows about Branch which might not have
-    # been imported yet
-    from stoqlib.domain.person import Branch
-    Branch  # pylint: disable=W0104
-
     if station_name is None:
         station_name = get_hostname()
 
@@ -685,6 +666,8 @@ def set_current_branch_station(store, station_name):
 
     provide_utility(ICurrentBranchStation, station, replace=True)
 
+    # The station may still not be associated with a branch when creating an
+    # empty database
     if station.branch:
         provide_utility(ICurrentBranch, station.branch, replace=True)
 

commit 3f2a9f693b51d6e830f317d93f773af526f7c7c1

Fix calendar app
This is a regression from commit e09172646b78a42d9eea6d5b1a10cddf21890b07.

Although the stoq process doesn't need to import Branch, because for sure it's
already loaded, the calendar runs on another process that doesn't know about it

Change-Id: I11bafcdde3c07e43870e6c024b8ceb06ad2c5e8a

stoqlib/database/runtime.py
@@ -655,6 +655,10 @@ def set_current_branch_station(store, station_name):
     :param store: a store
     :param station_name: name of the station to register
     """
+    # This is called from stoq-daemon, which doesn't know about Branch yet
+    from stoqlib.domain.person import Branch
+    Branch  # pylint: disable=W0104
+
     if station_name is None:
         station_name = get_hostname()
 