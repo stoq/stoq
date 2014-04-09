stoq/stoq

commit 130a42ccadb901c5bef47085f4891b9b40198f99

Corrections of Deprecate of delete() to store.remove
This patch makes changes to correct deprecated function delete()
to store.remove().

Change-Id: Ie52da11ae5a15b31bdacfb7c0f2f15c5f977836b

stoqlib/gui/editors/invoiceeditor.py
@@ -191,7 +191,7 @@ class InvoiceLayoutEditor(BaseEditor):
 
     def _field_removed(self, grid_field):
         invoice_field = grid_field.model
-        InvoiceField.delete(invoice_field.id, self.store)
+        self.model.remove(invoice_field)
 
     def _print_preview(self):
         # Get the last opened date

stoqlib/gui/editors/personeditor.py
@@ -209,7 +209,7 @@ class EmployeeRoleEditor(BaseEditor):
         # XXX This will prevent problems in case that you can't
         # update the store.
         if not self.edit_mode:
-            self.model_type.delete(self.model.id, store=self.store)
+            self.model.remove(self.model)
 
     #
     # Kiwi handlers

data/sql/patch-02-23.py
@@ -90,8 +90,7 @@ def apply_patch(store):
                                           quantity=ret[1],
                                           sellable=product.sellable)
         decrease.add_item(decrease_item)
-
-        ProductHistory.delete(hist[0], store)
+        store.remove(hist[0])
         ProductHistory(branch_id=branch_id, sellable=product.sellable,
                        quantity_decreased=decrease_item.quantity,
                        decreased_date=decrease.confirm_date,

stoqlib/database/orm.py
@@ -70,7 +70,7 @@ class SQLObjectBase(Storm):
     @classmethod
     def delete(cls, id, store=None):
         warnings.warn("use store.remove()", DeprecationWarning, stacklevel=2)
-        obj = cls.get(id, store=store)
+        obj = store.get(cls, id)
         Store.of(obj).remove(obj)
 
 

stoqlib/database/viewable.py
@@ -170,7 +170,7 @@ class Viewable(ClassInittableObject):
     def sync(self):
         """Update the values of this object from the database
         """
-        new_obj = self.store.find(type(self), id=self.id).one()
+        new_obj = self._store.find(type(self), id=self.id).one()
         self.__dict__.update(new_obj.__dict__)
 
     def __hash__(self):

stoqlib/domain/account.py
@@ -317,7 +317,7 @@ class Account(Domain):
                 store.remove(option)
             store.remove(bank)
 
-        self.delete(self.id, store=store)
+        self.store.remove(self)
 
     def has_child_accounts(self):
         """If this account has child accounts

stoqlib/domain/exampledata.py
@@ -216,7 +216,6 @@ class ExampleCreator(object):
             'FiscalBookEntry': self.create_fiscal_book_entry,
             'Client': self.create_client,
             'Company': self.create_company,
-            'Branch': self.create_branch,
             'Employee': self.create_employee,
             'Image': self.create_image,
             'Individual': self.create_individual,
@@ -227,6 +226,7 @@ class ExampleCreator(object):
             'Transporter': self.create_transporter,
             'LoginUser': self.create_user,
             'Payment': self.create_payment,
+            'CardPaymentOperation': self.create_card_operation_payment,
             'ParameterData': self.create_parameter_data,
             'Person': self.create_person,
             'Branch': self.create_branch,
@@ -241,6 +241,7 @@ class ExampleCreator(object):
             'LoginUser': self.create_user,
             'MilitaryData': self.create_military_data,
             'Product': self.create_product,
+            'ProductManufacturer': self.create_product_manufacturer,
             'ProductSupplierInfo': self.create_product_supplier_info,
             'Storable': self.create_storable,
             'PurchaseOrder': self.create_purchase_order,
@@ -445,6 +446,14 @@ class ExampleCreator(object):
                                     unit_cost=10)
 
         return product
+
+    def create_product_manufacturer(self, name=None):
+        from stoqlib.domain.product import ProductManufacturer
+
+        if name is None:
+            name = u''
+
+        return ProductManufacturer(store=self.store, name=name)
 
     def create_product_component(self, product=None, component=None,
                                  storable=False):
@@ -830,6 +839,10 @@ class ExampleCreator(object):
                        category=None,
                        store=self.store,
                        payment_type=payment_type)
+
+    def create_card_operation_payment(self):
+        from stoqlib.domain.payment.operation import CardPaymentOperation
+        return CardPaymentOperation()
 
     def create_card_payment(self, date=None, provider_id=u'AMEX', device=None,
                             payment_value=None):

stoqlib/domain/image.py
@@ -77,7 +77,7 @@ class Image(Domain):
         if self.description:
             return self.description
         # TODO: Dont use id here
-        return _(u"Stoq image #%d") % (self.id,)
+        return _(u"Stoq image #%d") % self.id
 
     #
     #  ORMObject
@@ -85,8 +85,9 @@ class Image(Domain):
 
     @classmethod
     def delete(cls, id, store):
-        ImageRemoveEvent.emit(cls.get(id, store))
-        super(Image, cls).delete(id, store)
+        image = store.get(cls, id)
+        ImageRemoveEvent.emit(image)
+        store.remove(image)
 
     #
     # Domain

commit 4d5ba8d8c53b0eec63503ab9d46df16ee2d0ebe0

Call .remove() from the right object
This is a regression from commit 130a42cc.

Change-Id: I46e9f70942fe091d5a9cac5c36539a63cacd1505

stoqlib/gui/editors/invoiceeditor.py
@@ -191,7 +191,7 @@ class InvoiceLayoutEditor(BaseEditor):
 
     def _field_removed(self, grid_field):
         invoice_field = grid_field.model
-        self.model.remove(invoice_field)
+        self.store.remove(invoice_field)
 
     def _print_preview(self):
         # Get the last opened date

stoqlib/gui/editors/personeditor.py
@@ -207,7 +207,7 @@ class EmployeeRoleEditor(BaseEditor):
         # XXX This will prevent problems in case that you can't
         # update the store.
         if not self.edit_mode:
-            self.model.remove(self.model)
+            self.store.remove(self.model)
 
     #
     # Kiwi handlers