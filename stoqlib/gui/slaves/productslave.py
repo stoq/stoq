stoq/stoq

commit 47a2bbb3c8b1f80ac5b239050da5c8018cb7b8ed

Prettify sellable editor and subclasses
* Put details slave directly on SellableEditor, saving some space on
  ProductEditor for the extra details
* Create some sizegroups on SellableEditor and ProductInformationSlave so
  we can have widgets aligned without having to be on the same table (used
  by the details slave)
* Improve ProductInformationSlave to make it look nicer
* Put tax slave inside an alignment so it can have some padding

Change-Id: I2941aac47fff4dcff1655207cd1a5d586c82c69d

stoqlib/gui/slaves/productslave.py
@@ -47,7 +47,6 @@ from stoqlib.gui.editors.producteditor import (TemporaryProductComponent,
                                                ProductComponentEditor,
                                                QualityTestEditor,
                                                ProductSupplierEditor)
-from stoqlib.gui.slaves.sellableslave import SellableDetailsSlave
 from stoqlib.lib.defaults import quantize, MAX_INT
 from stoqlib.lib.formatters import get_formatted_cost
 from stoqlib.lib.message import info, yesno, warning
@@ -65,7 +64,7 @@ class ProductInformationSlave(BaseEditorSlave):
                      'product_model', 'brand', 'family']
     storable_widgets = ['minimum_quantity', 'maximum_quantity']
 
-    def __init__(self, store, model, db_form, visual_mode=False):
+    def __init__(self, store, model, db_form=None, visual_mode=False):
         self.db_form = db_form
         BaseEditorSlave.__init__(self, store, model, visual_mode)
 
@@ -213,17 +212,6 @@ class ProductInformationSlave(BaseEditorSlave):
         if minimum and minimum > value:
             return ValidationError(_(u'Maximum must be greater than the '
                                      'minimum value.'))
-
-
-class ProductDetailsSlave(SellableDetailsSlave):
-
-    def setup_slaves(self):
-        self.info_slave = ProductInformationSlave(self.store, self.model.product,
-                                                  self.db_form, self.visual_mode)
-        self.attach_slave('details_holder', self.info_slave)
-
-    def hide_stock_details(self):
-        self.info_slave.hide_stock_details()
 
 
 class ProductTaxSlave(BaseEditorSlave):

data/glade/ImageHolder.ui
@@ -1,36 +1,30 @@
-<?xml version="1.0"?>
-<!--*- mode: xml -*-->
+<?xml version="1.0" encoding="UTF-8"?>
 <interface>
+  <requires lib="gtk+" version="2.16"/>
+  <!-- interface-naming-policy toplevel-contextual -->
   <object class="GtkWindow" id="ImageHolder">
-    <property name="default_height">1</property>
     <property name="default_width">1</property>
+    <property name="default_height">1</property>
     <child>
       <object class="GtkHBox" id="hbox1">
         <property name="visible">True</property>
         <child>
-          <object class="GtkVSeparator" id="separator">
-            <property name="visible">True</property>
-          </object>
-          <packing>
-            <property name="expand">False</property>
-            <property name="fill">False</property>
-          </packing>
-        </child>
-        <child>
           <object class="GtkEventBox" id="eventbox">
-            <property name="border_width">6</property>
             <property name="visible">True</property>
+            <property name="border_width">6</property>
             <child>
               <object class="GtkImage" id="image">
+                <property name="width_request">64</property>
                 <property name="height_request">64</property>
                 <property name="visible">True</property>
-                <property name="width_request">64</property>
                 <property name="xpad">6</property>
               </object>
             </child>
           </object>
           <packing>
-            <property name="position">1</property>
+            <property name="expand">True</property>
+            <property name="fill">True</property>
+            <property name="position">0</property>
           </packing>
         </child>
       </object>

data/glade/ProductStockEditor.ui
@@ -1,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <!-- interface-requires gtk+ 2.12 -->
+  <!-- interface-naming-policy toplevel-contextual -->
+  <object class="GtkWindow" id="ProductStockEditor">
+    <property name="default_width">440</property>
+    <property name="default_height">250</property>
+    <child>
+      <object class="GtkVBox" id="vbox1">
+        <property name="visible">True</property>
+        <child>
+          <object class="GtkEventBox" id="information_holder">
+            <property name="visible">True</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkHBox" id="hbox1">
+            <property name="visible">True</property>
+            <child>
+              <object class="GtkFrame" id="frame1">
+                <property name="visible">True</property>
+                <property name="label_xalign">0</property>
+                <child>
+                  <object class="GtkAlignment" id="alignment1">
+                    <property name="visible">True</property>
+                    <property name="top_padding">6</property>
+                    <property name="bottom_padding">6</property>
+                    <property name="left_padding">6</property>
+                    <property name="right_padding">6</property>
+                    <child>
+                      <object class="GtkEventBox" id="details_holder">
+                        <property name="visible">True</property>
+                        <child>
+                          <placeholder/>
+                        </child>
+                      </object>
+                    </child>
+                  </object>
+                </child>
+                <child type="label">
+                  <object class="GtkLabel" id="label1">
+                    <property name="visible">True</property>
+                    <property name="xpad">6</property>
+                    <property name="label" translatable="yes">&lt;b&gt;Extra&lt;/b&gt;</property>
+                    <property name="use_markup">True</property>
+                  </object>
+                </child>
+              </object>
+              <packing>
+                <property name="expand">True</property>
+                <property name="fill">True</property>
+                <property name="padding">6</property>
+                <property name="position">0</property>
+              </packing>
+            </child>
+          </object>
+          <packing>
+            <property name="expand">True</property>
+            <property name="fill">True</property>
+            <property name="position">1</property>
+          </packing>
+        </child>
+      </object>
+    </child>
+  </object>
+</interface>

data/glade/ProductTaxSlave.ui
@@ -1,66 +1,93 @@
-<?xml version="1.0"?>
-<!--*- mode: xml -*-->
+<?xml version="1.0" encoding="UTF-8"?>
 <interface>
+  <!-- interface-requires gtk+ 2.12 -->
+  <!-- interface-requires kiwiwidgets 0.0 -->
+  <!-- interface-naming-policy toplevel-contextual -->
   <object class="GtkWindow" id="ProductTaxSlave">
-    <property name="default_height">250</property>
     <property name="default_width">440</property>
+    <property name="default_height">250</property>
     <property name="mnemonics_visible">False</property>
     <child>
-      <object class="GtkTable" id="table1">
-        <property name="column_spacing">6</property>
-        <property name="n_columns">3</property>
-        <property name="n_rows">3</property>
-        <property name="row_spacing">6</property>
+      <object class="GtkAlignment" id="alignment1">
         <property name="visible">True</property>
+        <property name="top_padding">6</property>
+        <property name="bottom_padding">6</property>
+        <property name="left_padding">6</property>
+        <property name="right_padding">6</property>
         <child>
-          <object class="GtkLabel" id="label1">
-            <property name="label" translatable="yes">ICMS Class:</property>
+          <object class="GtkTable" id="table1">
             <property name="visible">True</property>
-            <property name="xalign">1.0</property>
+            <property name="n_rows">3</property>
+            <property name="n_columns">3</property>
+            <property name="column_spacing">6</property>
+            <property name="row_spacing">6</property>
+            <child>
+              <object class="GtkLabel" id="label1">
+                <property name="visible">True</property>
+                <property name="xalign">1</property>
+                <property name="label" translatable="yes">ICMS Class:</property>
+              </object>
+              <packing>
+                <property name="x_options">GTK_FILL</property>
+                <property name="y_options">GTK_FILL</property>
+              </packing>
+            </child>
+            <child>
+              <object class="GtkLabel" id="label2">
+                <property name="visible">True</property>
+                <property name="xalign">1</property>
+                <property name="label" translatable="yes">IPI Class:</property>
+              </object>
+              <packing>
+                <property name="top_attach">1</property>
+                <property name="bottom_attach">2</property>
+                <property name="x_options">GTK_FILL</property>
+                <property name="y_options">GTK_FILL</property>
+              </packing>
+            </child>
+            <child>
+              <object class="ProxyComboBox" id="icms_template">
+                <property name="visible">True</property>
+                <property name="data_type">object</property>
+                <property name="model_attribute">icms_template</property>
+              </object>
+              <packing>
+                <property name="left_attach">1</property>
+                <property name="right_attach">2</property>
+                <property name="y_options">GTK_FILL</property>
+              </packing>
+            </child>
+            <child>
+              <object class="ProxyComboBox" id="ipi_template">
+                <property name="visible">True</property>
+                <property name="data_type">object</property>
+                <property name="model_attribute">ipi_template</property>
+              </object>
+              <packing>
+                <property name="left_attach">1</property>
+                <property name="right_attach">2</property>
+                <property name="top_attach">1</property>
+                <property name="bottom_attach">2</property>
+                <property name="x_options">GTK_FILL</property>
+                <property name="y_options">GTK_FILL</property>
+              </packing>
+            </child>
+            <child>
+              <placeholder/>
+            </child>
+            <child>
+              <placeholder/>
+            </child>
+            <child>
+              <placeholder/>
+            </child>
+            <child>
+              <placeholder/>
+            </child>
+            <child>
+              <placeholder/>
+            </child>
           </object>
-          <packing>
-            <property name="x_options">fill</property>
-            <property name="y_options">fill</property>
-          </packing>
-        </child>
-        <child>
-          <object class="GtkLabel" id="label2">
-            <property name="label" translatable="yes">IPI Class:</property>
-            <property name="visible">True</property>
-            <property name="xalign">1.0</property>
-          </object>
-          <packing>
-            <property name="bottom_attach">2</property>
-            <property name="top_attach">1</property>
-            <property name="x_options">fill</property>
-            <property name="y_options">fill</property>
-          </packing>
-        </child>
-        <child>
-          <object class="ProxyComboBox" id="icms_template">
-            <property name="data_type">object</property>
-            <property name="model_attribute">icms_template</property>
-            <property name="visible">True</property>
-          </object>
-          <packing>
-            <property name="left_attach">1</property>
-            <property name="right_attach">2</property>
-            <property name="y_options">fill</property>
-          </packing>
-        </child>
-        <child>
-          <object class="ProxyComboBox" id="ipi_template">
-            <property name="data_type">object</property>
-            <property name="model_attribute">ipi_template</property>
-            <property name="visible">True</property>
-          </object>
-          <packing>
-            <property name="bottom_attach">2</property>
-            <property name="left_attach">1</property>
-            <property name="right_attach">2</property>
-            <property name="top_attach">1</property>
-            <property name="y_options">fill</property>
-          </packing>
         </child>
       </object>
     </child>

data/glade/SellableDetailsSlave.ui
@@ -9,24 +9,13 @@
     <child>
       <object class="GtkVBox" id="vbox1">
         <property name="visible">True</property>
-        <property name="border_width">6</property>
         <property name="spacing">6</property>
         <child>
           <object class="GtkTable" id="table1">
             <property name="visible">True</property>
-            <property name="n_rows">2</property>
             <property name="n_columns">3</property>
             <property name="column_spacing">6</property>
             <property name="row_spacing">6</property>
-            <child>
-              <object class="GtkEventBox" id="details_holder">
-                <property name="visible">True</property>
-              </object>
-              <packing>
-                <property name="right_attach">2</property>
-                <property name="y_options">GTK_FILL</property>
-              </packing>
-            </child>
             <child>
               <object class="GtkLabel" id="notes_lbl">
                 <property name="visible">True</property>
@@ -35,8 +24,6 @@
                 <property name="label" translatable="yes">Details:</property>
               </object>
               <packing>
-                <property name="top_attach">1</property>
-                <property name="bottom_attach">2</property>
                 <property name="x_options">GTK_FILL</property>
                 <property name="y_options">GTK_FILL</property>
               </packing>
@@ -58,9 +45,6 @@
               <packing>
                 <property name="left_attach">1</property>
                 <property name="right_attach">2</property>
-                <property name="top_attach">1</property>
-                <property name="bottom_attach">2</property>
-                <property name="y_options">GTK_FILL</property>
               </packing>
             </child>
             <child>
@@ -81,14 +65,9 @@
               <packing>
                 <property name="left_attach">2</property>
                 <property name="right_attach">3</property>
-                <property name="top_attach">1</property>
-                <property name="bottom_attach">2</property>
                 <property name="x_options">GTK_FILL</property>
                 <property name="y_options">GTK_FILL</property>
               </packing>
-            </child>
-            <child>
-              <placeholder/>
             </child>
           </object>
           <packing>

data/glade/SellableEditor.ui
@@ -19,7 +19,7 @@
             <child>
               <object class="GtkTable" id="table">
                 <property name="visible">True</property>
-                <property name="n_rows">9</property>
+                <property name="n_rows">8</property>
                 <property name="n_columns">4</property>
                 <property name="column_spacing">6</property>
                 <property name="row_spacing">6</property>
@@ -29,6 +29,10 @@
                     <property name="xalign">1</property>
                     <property name="label" translatable="yes">Code:</property>
                   </object>
+                  <packing>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
+                  </packing>
                 </child>
                 <child>
                   <object class="GtkLabel" id="barcode_lbl">
@@ -39,6 +43,8 @@
                   <packing>
                     <property name="top_attach">1</property>
                     <property name="bottom_attach">2</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -50,6 +56,8 @@
                   <packing>
                     <property name="top_attach">2</property>
                     <property name="bottom_attach">3</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -61,6 +69,8 @@
                   <packing>
                     <property name="top_attach">3</property>
                     <property name="bottom_attach">4</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -73,6 +83,8 @@
                   <packing>
                     <property name="top_attach">4</property>
                     <property name="bottom_attach">5</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -85,6 +97,8 @@
                   <packing>
                     <property name="top_attach">5</property>
                     <property name="bottom_attach">6</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -96,8 +110,8 @@
                   </object>
                   <packing>
                     <property name="right_attach">4</property>
-                    <property name="top_attach">8</property>
-                    <property name="bottom_attach">9</property>
+                    <property name="top_attach">7</property>
+                    <property name="bottom_attach">8</property>
                   </packing>
                 </child>
                 <child>
@@ -114,6 +128,7 @@
                   <packing>
                     <property name="left_attach">1</property>
                     <property name="right_attach">2</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -132,6 +147,7 @@
                     <property name="right_attach">2</property>
                     <property name="top_attach">1</property>
                     <property name="bottom_attach">2</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -145,6 +161,7 @@
                     <property name="right_attach">2</property>
                     <property name="top_attach">2</property>
                     <property name="bottom_attach">3</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -164,6 +181,7 @@
                     <property name="right_attach">4</property>
                     <property name="top_attach">3</property>
                     <property name="bottom_attach">4</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -216,6 +234,7 @@
                     <property name="right_attach">2</property>
                     <property name="top_attach">5</property>
                     <property name="bottom_attach">6</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -227,6 +246,8 @@
                   <packing>
                     <property name="left_attach">2</property>
                     <property name="right_attach">3</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -240,6 +261,8 @@
                     <property name="right_attach">3</property>
                     <property name="top_attach">2</property>
                     <property name="bottom_attach">3</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -253,6 +276,8 @@
                     <property name="right_attach">3</property>
                     <property name="top_attach">4</property>
                     <property name="bottom_attach">5</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -266,6 +291,8 @@
                     <property name="right_attach">3</property>
                     <property name="top_attach">5</property>
                     <property name="bottom_attach">6</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -277,6 +304,7 @@
                   <packing>
                     <property name="left_attach">3</property>
                     <property name="right_attach">4</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -337,6 +365,7 @@
                     <property name="right_attach">4</property>
                     <property name="top_attach">4</property>
                     <property name="bottom_attach">5</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -361,6 +390,7 @@
                     <property name="right_attach">4</property>
                     <property name="top_attach">5</property>
                     <property name="bottom_attach">6</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -376,6 +406,7 @@
                     <property name="right_attach">4</property>
                     <property name="top_attach">6</property>
                     <property name="bottom_attach">7</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -408,6 +439,7 @@
                     <property name="right_attach">2</property>
                     <property name="top_attach">4</property>
                     <property name="bottom_attach">5</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
                 </child>
                 <child>
@@ -471,19 +503,8 @@
                     <property name="right_attach">4</property>
                     <property name="top_attach">2</property>
                     <property name="bottom_attach">3</property>
+                    <property name="y_options">GTK_FILL</property>
                   </packing>
-                </child>
-                <child>
-                  <placeholder/>
-                </child>
-                <child>
-                  <placeholder/>
-                </child>
-                <child>
-                  <placeholder/>
-                </child>
-                <child>
-                  <placeholder/>
                 </child>
                 <child>
                   <placeholder/>
@@ -502,7 +523,7 @@
                 </child>
               </object>
               <packing>
-                <property name="expand">False</property>
+                <property name="expand">True</property>
                 <property name="fill">True</property>
                 <property name="position">0</property>
               </packing>
@@ -511,5 +532,23 @@
         </child>
       </object>
     </child>
+  </object>
+  <object class="GtkSizeGroup" id="left_labels_group">
+    <widgets>
+      <widget name="code_lbl"/>
+      <widget name="barcode_lbl"/>
+      <widget name="label10"/>
+      <widget name="label2"/>
+      <widget name="label4"/>
+      <widget name="label5"/>
+    </widgets>
+  </object>
+  <object class="GtkSizeGroup" id="right_labels_group">
+    <widgets>
+      <widget name="label8"/>
+      <widget name="category_lbl"/>
+      <widget name="label9"/>
+      <widget name="label3"/>
+    </widgets>
   </object>
 </interface>

stoqlib/gui/editors/producteditor.py
@@ -316,10 +316,12 @@ class ProductEditor(SellableEditor):
     #
 
     def setup_slaves(self):
-        from stoqlib.gui.slaves.productslave import ProductDetailsSlave
-        self.details_slave = ProductDetailsSlave(self.store, self.model.sellable,
-                                                 self.db_form, self.visual_mode)
-        self.add_extra_tab(_(u'Details'), self.details_slave)
+        super(ProductEditor, self).setup_slaves()
+
+        from stoqlib.gui.slaves.productslave import ProductInformationSlave
+        info_slave = ProductInformationSlave(self.store, self.model, self.db_form,
+                                             visual_mode=self.visual_mode)
+        self.add_extra_tab(_(u'Details'), info_slave)
 
     #
     #   Callbacks
@@ -412,13 +414,22 @@ class ProductionProductEditor(ProductEditor):
 class ProductStockEditor(BaseEditor):
     model_name = _('Product')
     model_type = Product
-    gladefile = 'HolderTemplate'
+    gladefile = 'ProductStockEditor'
 
     def setup_slaves(self):
-        from stoqlib.gui.slaves.productslave import ProductDetailsSlave
-        details_slave = ProductDetailsSlave(self.store, self.model.sellable)
-        details_slave.hide_stock_details()
-        self.attach_slave('place_holder', details_slave)
+        from stoqlib.gui.slaves.productslave import ProductInformationSlave
+        info_slave = ProductInformationSlave(self.store, self.model,
+                                             visual_mode=self.visual_mode)
+        info_slave.nfe_frame.hide()
+        self.attach_slave('information_holder', info_slave)
+
+        from stoqlib.gui.slaves.sellableslave import SellableDetailsSlave
+        details_slave = SellableDetailsSlave(self.store, self.model.sellable,
+                                             visual_mode=self.visual_mode)
+        self.attach_slave('details_holder', details_slave)
+
+        # Make everything aligned by pytting notes_lbl on the same size group
+        info_slave.left_labels_group.add_widget(details_slave.notes_lbl)
 
 
 class ProductManufacturerEditor(BaseEditor):

commit 36731a740ae83d21ef79e4d47e423fdd1e3e3d78

When using forms, hide the right widgets
If all widgets inside a product frame becomes invisible after a form change,
hide their frames.

This is a regression from commit 47a2bbb3c8b1f80ac5b239050da5c8018cb7b8ed. That
commit changed a single label to 3 frames, and those label names were not
available anymore.

Change-Id: I904d64557e1cbe91c414a3f472f9dc8ab76050a8

stoqlib/gui/slaves/productslave.py
@@ -113,7 +113,7 @@ class ProductInformationSlave(BaseEditorSlave):
         if (not self.minimum_quantity.get_visible() and
             not self.maximum_quantity.get_visible() and
             not self.location.get_visible()):
-            self.stock_lbl.hide()
+            self.storable_frame.hide()
 
         # Mercosul
         self.db_form.update_widget(self.ncm, other=self.ncm_lbl)
@@ -123,7 +123,7 @@ class ProductInformationSlave(BaseEditorSlave):
         if (not self.ncm.get_visible() and
             not self.ex_tipi.get_visible() and
             not self.genero.get_visible()):
-            self.mercosul_lbl.hide()
+            self.nfe_frame.hide()
 
     def setup_proxies(self):
         self._setup_widgets()