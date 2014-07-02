617f12abdb289e5a83df083965d717a590ae61df
Ronaldo Maia <romaia@async.com.br>
2014-07-02 11:04:41 -0300

    Fix patch 05-03.sql
    This patch is droping a production_order constraint, but there was a typo in
    the original patch that created this constraint, so we should try to drop
    the correct one and the one with the typo.
    
    Change-Id: I6904b0271a9aedb98ece40e695166988d905f5a2

--- data/sql/patch-05-03.sql
+++ data/sql/patch-05-03.sql
@@ -1,4 +1,6 @@
-ALTER TABLE production_order DROP CONSTRAINT valid_status;
+-- There was a typo in patch 02-39
+ALTER TABLE production_order DROP CONSTRAINT IF EXISTS valid_statuos;
+ALTER TABLE production_order DROP CONSTRAINT IF EXISTS valid_status;
 ALTER TABLE production_order
     ADD CONSTRAINT valid_status CHECK (status >= 0 and status < 6);
 
