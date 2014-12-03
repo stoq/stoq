8e629947611c13ac022e8cf321d77820df92638c
Ronaldo Maia <romaia@async.com.br>
2014-12-03 16:08:45 -0200

    1.9 Release Candidate 5
    Change-Id: I64272c9265c7eedbe3712d10f0866e0b62491144

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 9
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc4'
+extra_version = 'rc5'
 
 #: the date the software was released
-release_date = (2014, 11, 27)
+release_date = (2014, 12, 3)
 
 #: if this is a stable release
 stable = True
