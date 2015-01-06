3f3487dd948505bf1e3137bd3d7ed31db57389c1
Gabriel Gerga <gerga@async.com.br>
2015-01-06 14:09:40 -0200

    When adding a payment using credit in SalePaymentsEditor, set the payment as paid
    - This avoid the credit to be used on another sale.
    - Add tests.
    
    Change-Id: I4399b7e3c8ec9240a6682e308cd23d43be3f5017

--- stoqlib/gui/test/test_salepaymentseditor.py
+++ stoqlib/gui/test/test_salepaymentseditor.py
@@ -23,6 +23,8 @@
 ##
 
 
+from stoqlib.domain.payment.method import PaymentMethod
+from stoqlib.domain.payment.payment import Payment
 from stoqlib.gui.editors.paymentseditor import SalePaymentsEditor
 from stoqlib.gui.test.uitestutils import GUITest
 
@@ -33,3 +35,44 @@ class TestSalePaymentsEditor(GUITest):
         sale = self.create_sale(client=client)
         editor = SalePaymentsEditor(self.store, sale)
         self.check_editor(editor, 'editor-salepayments-show')
+
+    def test_pay_with_credit(self):
+        client = self.create_client()
+        sale = self.create_sale(client=client)
+        sale.identifier = 1234
+        sellable = self.create_sellable(price=10)
+        sale.add_sellable(sellable)
+
+        # Create credit to the client.
+        method = PaymentMethod.get_by_name(self.store, u'credit')
+        group = self.create_payment_group(payer=client.person)
+        payment = self.create_payment(value=20, method=method, group=group)
+        payment.set_pending()
+        payment.pay()
+        self.assertEquals(client.credit_account_balance, 20)
+
+        editor = SalePaymentsEditor(self.store, sale)
+        # Select credit method.
+        for radio in editor.slave.methods_box.get_children():
+            if radio.get_label() == 'Credit ($20.00)':
+                radio.set_active(True)
+                break
+        # Add credit payment.
+        editor.slave.value.update(10)
+        self.assertSensitive(editor.slave, ['add_button'])
+        self.click(editor.slave.add_button)
+        editor.confirm()
+        self.assertEquals(client.credit_account_balance, 10)
+        for payment in sale.payments:
+            self.assertEquals(payment.status, Payment.STATUS_PAID)
+
+        # Test remove payment.
+        self.assertNotSensitive(editor.slave, ['remove_button'])
+        editor = SalePaymentsEditor(self.store, sale)
+        payments = editor.slave.payments
+        payments.select(payments[0])
+        self.assertSensitive(editor.slave, ['remove_button'])
+        self.click(editor.slave.remove_button)
+        editor.confirm()
+        for payment in sale.payments:
+            self.assertEquals(payment.status, Payment.STATUS_CANCELLED)
