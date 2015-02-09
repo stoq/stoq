c017ac8b9cf58a10ce6257c9158835ce7ffcd0a0
Ronaldo Maia <romaia@async.com.br>
2015-02-09 11:37:28 -0200

    Release 1.9 RC 9
    Change-Id: I01640f9211fd6681c84c80263fb5992e205bbdaa

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 9
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc8'
+extra_version = 'rc9'
 
 #: the date the software was released
-release_date = (2015, 1, 6)
+release_date = (2015, 2, 9)
 
 #: if this is a stable release
 stable = True
