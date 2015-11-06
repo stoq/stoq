787707d51874af0ce86bc20268255c2b020226d4
Thiago Bellini <hackedbellini@gmail.com>
2015-11-06 16:08:24 -0200

    Release 1.10 final
    Change-Id: I11af340d041c883fc0f672edb4f6afd3eb8a7b61

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 10
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc4'
+extra_version = ''
 
 #: the date the software was released
-release_date = (2015, 10, 27)
+release_date = (2015, 11, 6)
 
 #: if this is a stable release
 stable = True
