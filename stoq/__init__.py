e5e736e9031161f4c48c81905b2a0ea95a52b57f
Thiago Bellini <hackedbellini@gmail.com>
2016-05-02 17:07:36 -0300

    Release 1.11rc6
    Change-Id: Ib736761e97a8cae3efb684c8fa42c1a62d1d7d60

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 11
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc5'
+extra_version = 'rc6'
 
 #: the date the software was released
-release_date = (2016, 4, 28)
+release_date = (2016, 5, 2)
 
 #: if this is a stable release
 stable = True
