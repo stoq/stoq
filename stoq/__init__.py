0395fd109805e4c0e5ee2c41fd883b591fb4b16e
Thiago Bellini <hackedbellini@gmail.com>
2016-09-15 13:23:18 -0300

    Release 1.11.3
    Change-Id: Ia4df51ea2929d0a83f830d9a2c33b450fecf3056

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -34,13 +34,13 @@ major_version = 1
 minor_version = 11
 
 #: Micro version, increase when doing a bug fix for a stable release
-micro_version = 2
+micro_version = 3
 
 #: extra version, rc1, rc2, etc goes here.
 extra_version = ''
 
 #: the date the software was released
-release_date = (2016, 6, 30)
+release_date = (2016, 9, 15)
 
 #: if this is a stable release
 stable = True
