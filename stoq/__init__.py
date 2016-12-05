2670342755c7973c9b0f16045bdcc358a43606d2
Thiago Bellini <hackedbellini@gmail.com>
2016-12-05 15:24:08 -0200

    Bugfix release 1.12.1
    Change-Id: I00d8e3e4bfe926ff74e70d80e6b5117806038fe7

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -34,13 +34,13 @@ major_version = 1
 minor_version = 12
 
 #: Micro version, increase when doing a bug fix for a stable release
-micro_version = 0
+micro_version = 1
 
 #: extra version, rc1, rc2, etc goes here.
 extra_version = ''
 
 #: the date the software was released
-release_date = (2016, 11, 22)
+release_date = (2016, 12, 5)
 
 #: if this is a stable release
 stable = True
