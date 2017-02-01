stoq/stoq

commit 292bf5de4f8216a9de33e56b963557d4b1902c43

Always call config.get_settings() in stoq startup
This will workaround an issue where the database configuration is only setup
correctly when calling get_settings. This needs some better organization
sometime.

Also, workaround an issue with stoq server being connected to postgres on an
ipv6 address.

Change-Id: I4c6a60e3fa62daa66ece17d5f2f43bc4c4749be5

stoq/lib/startup.py
@@ -117,14 +117,14 @@ def setup(config=None, options=None, register_station=True, check_schema=True,
     provide_utility(IApplicationDescriptions, ApplicationDescriptions(),
                     replace=True)
 
+    try:
+        default_store = get_default_store()
+    except DatabaseError as e:
+        error(e.short, str(e.msg))
+    db_settings = config.get_settings()
+
     if register_station:
-        try:
-            default_store = get_default_store()
-        except DatabaseError as e:
-            error(e.short, str(e.msg))
-
-        config.get_settings().check_version(default_store)
-
+        db_settings.check_version(default_store)
         if check_schema:
             migration = StoqlibSchemaMigration()
             migration.check()
@@ -140,7 +140,6 @@ def setup(config=None, options=None, register_station=True, check_schema=True,
         manager.activate_installed_plugins()
 
     if check_schema:
-        default_store = get_default_store()
         if not default_store.table_exists('system_table'):
             error(
                 _("Database schema error"),

stoqlib/lib/pluginmanager.py
@@ -157,11 +157,9 @@ class PluginManager(object):
         self._read_plugin_descriptions()
 
     def _create_eggs_cache(self):
-        log.info("Creating cache for plugins eggs")
-
         # $HOME/.stoq/plugins
-        default_store = get_default_store()
         path = os.path.join(get_application_dir(), 'plugins')
+        log.info("Creating cache for plugins eggs (path=%s)", path)
         if not os.path.exists(path):
             os.makedirs(path)
 
@@ -171,6 +169,7 @@ class PluginManager(object):
 
         # Now extract all eggs from the database and put it where stoq know
         # how to load them
+        default_store = get_default_store()
         for plugin_name, egg_md5sum in default_store.using(PluginEgg).find(
                 (PluginEgg.plugin_name, PluginEgg.egg_md5sum)):
             # A little optimization to avoid loading the egg in memory if we

stoqlib/net/server.py
@@ -111,6 +111,12 @@ class ServerProxy(object):
                     res = store.execute(query, params=params).get_one()
                 if res is not None and res[0] is not None:
                     address = res[0]
+                    # For now we only support ipv4
+                    if address == '::1':
+                        address = '127.0.0.1'
+                    # Is there a better way to detect ipv6?
+                    #if ':' in address:
+                    #    address = '[{}]'.format(address)
                 elif res is not None:
                     # If the client_addr is NULL, then stoqserver is connected
                     # using the unix socket, which means that he is in the same

commit 9bde1b6240cca49e82956970a774cd4b345bbc5c

Fix issue introduced by commit 292bf5d
We should be more flexible when calling setup, and only raise an fatal error
if an connection with the database is actually required (like when
registering a station).

This was the behaviour before the mentioned commit

Change-Id: I1f25713dbbd53700df122398330a373d998c8a7b

stoq/lib/startup.py
@@ -121,7 +121,11 @@ def setup(config=None, options=None, register_station=True, check_schema=True,
     try:
         default_store = get_default_store()
     except DatabaseError as e:
-        error(e.short, str(e.msg))
+        # Only raise an error if a database is actually required
+        if register_station or load_plugins or check_schema:
+            error(e.short, str(e.msg))
+        else:
+            default_store = None
 
     if register_station:
         db_settings.check_version(default_store)