stoq/stoq

commit 68b6dae12d43b85ab46f7e4ab4e3270dae1c81ee

Add more fields to sellable mass editor.
Let use update more fields in the mass editor: Unit and CFOP (sellable) NCM,
Location, Brand, Family, Model and Manufacturer

Change-Id: Ice216c19fb2dd5209b1121a2136efb3dde9d3444

stoqlib/gui/dialogs/masseditordialog.py
@@ -38,6 +38,10 @@ for the update and set some parameters depending on those rules. For instance:
     - Set to    [   |v]
 """
 
+import logging
+import sys
+import traceback
+
 import datetime
 from decimal import Decimal
 
@@ -57,6 +61,7 @@ from stoqlib.lib.message import marker, warning, yesno
 from stoqlib.lib.translation import stoqlib_gettext
 
 _ = stoqlib_gettext
+log = logging.getLogger(__name__)
 
 
 #
@@ -353,9 +358,10 @@ class Field(object):
     This class implements basic value caching/storage for the editor
     """
 
-    def __init__(self, data_type, validator=None, unique=False):
+    def __init__(self, data_type, validator=None, unique=False, visible=True):
         self.data_type = data_type
         self.validator = validator
+        self.visible = visible
         # FIXME: Use this
         self.unique = unique
         self.new_values = {}
@@ -368,7 +374,8 @@ class Field(object):
 
     def get_column(self, spec):
         return Column('id', title=self.label, data_type=self.data_type,
-                      format_func=self.format_func, format_func_data=self)
+                      format_func=self.format_func, format_func_data=self,
+                      visible=self.visible)
 
     def format_func(self, item, data=None):
         value = self.get_new_value(item)
@@ -393,7 +400,7 @@ class Field(object):
 class AccessorField(Field):
 
     def __init__(self, label, obj_name, attribute, data_type, unique=False,
-                 validator=None):
+                 validator=None, visible=True):
         """A field that updates a value of another object
 
         :param obj_name: the name of the object that will be updated, or None if
@@ -402,9 +409,13 @@ class AccessorField(Field):
         :param unique: If the field is unique, the user will not be able to set
           the field to an specific value. FIXME: not implemented yet
         :param validator: A callable that should return True/False
+        :param visible: If the column will be visible by default or not
+        :param editable: If the field should be editable or not. Sometimes it is
+          usefull to have fields visible that are not editable, but can be
+          filterable
         """
         super(AccessorField, self).__init__(data_type, validator=validator,
-                                            unique=unique)
+                                            unique=unique, visible=visible)
         self.label = label
         self.obj_name = obj_name
         self.attribute = attribute
@@ -421,7 +432,7 @@ class AccessorField(Field):
 
     def get_value(self, item):
         obj = self._get_obj(item)
-        return getattr(obj, self.attribute)
+        return obj and getattr(obj, self.attribute)
 
     def save_value(self, item):
         try:
@@ -430,7 +441,8 @@ class AccessorField(Field):
             # The value wasn't changed. Ignore
             return
         dest_obj = self._get_obj(item)
-        setattr(dest_obj, self.attribute, value)
+        if dest_obj:
+            setattr(dest_obj, self.attribute, value)
 
     def get_column(self, spec):
         # SearchColumn expects str instead of unicode and objects are rendered
@@ -448,12 +460,12 @@ class AccessorField(Field):
                             data_type=data_type, editable=editable,
                             format_func=self.format_func,
                             search_attribute=self.get_search_spec(spec),
-                            format_func_data=self)
+                            format_func_data=self, visible=self.visible)
 
 
 class ReferenceField(AccessorField):
     def __init__(self, label, obj_name, attribute, reference_class,
-                 reference_attr):
+                 reference_attr, visible=True):
         """A field that updates a reference to another object
 
         :param label: The label to be displayed
@@ -468,7 +480,8 @@ class ReferenceField(AccessorField):
         self._reference_class = reference_class
         self._reference_attr = reference_attr
         super(ReferenceField, self).__init__(label, obj_name, attribute,
-                                             data_type=object, unique=False)
+                                             data_type=object, unique=False,
+                                             visible=visible)
 
     def get_search_spec(self, spec=None):
         return getattr(self._reference_class, self._reference_attr)
@@ -501,8 +514,10 @@ class MassEditorWidget(gtk.HBox):
         return [f for f in self._fields if f.data_type == data_type]
 
     def _setup_editor(self, field):
-        # Reuse editor if its possible
-        if self._editor and self._editor.data_type == field.data_type:
+        # Reuse editor if its possible (not when data_type is an object, since
+        # that requires changing the reference values)
+        if (self._editor and field.data_type is not object and
+                self._editor.data_type == field.data_type):
             self._editor.set_field(field)
             return
 
@@ -667,6 +682,7 @@ class MassEditorSearch(SearchDialog):
         except Exception as e:
             d.stop()
             self.retval = False
+            log.error(''.join(traceback.format_exception(*sys.exc_info())))
             warning(_('There was an error saving one of the values'), str(e))
             self.close()
             return

stoqlib/gui/search/searchdialog.py
@@ -141,6 +141,9 @@ class SearchDialog(BasicDialog):
     #: be displayed
     unlimited_results = False
 
+    #: If the column settings should be saved or not.
+    save_columns = False
+
     def __init__(self, store, search_spec=None, hide_footer=True,
                  title='', selection_mode=None, double_click_confirm=False,
                  initial_string=''):
@@ -204,10 +207,14 @@ class SearchDialog(BasicDialog):
     def _setup_search(self):
         self.columns = self.get_columns()
         SearchDialogSetupSearchEvent.emit(self)
+        if self.save_columns:
+            restore_name = self.__class__.__name__
+        else:
+            restore_name = None
         self.search = SearchSlave(
             self.columns,
             tree=self.tree,
-            restore_name=self.__class__.__name__,
+            restore_name=restore_name,
             store=self.store,
             search_spec=self.search_spec,
             fast_iter=self.fast_iter,
@@ -314,13 +321,15 @@ class SearchDialog(BasicDialog):
         if retval is None:
             retval = self.get_selection()
         self.retval = retval
-        self.search.save_columns()
+        if self.save_columns:
+            self.search.save_columns()
         # FIXME: This should chain up so the "confirm" signal gets emitted
         self.close()
 
     def cancel(self, *args):
         self.retval = []
-        self.search.save_columns()
+        if self.save_columns:
+            self.search.save_columns()
         # FIXME: This should chain up so the "cancel" signal gets emitted
         self.close()
 

tests/data/ui/search-product-search-extended.uitest
@@ -36,23 +36,23 @@ GtkDialog(toplevel): title='Product Search', hidden
             column: title='Description', expand
             column: title='Manufacturer', hidden
             column: title='Model', hidden
+            column: title='NCM', hidden
             column: title='Unit', hidden
             column: title='Location', hidden
             column: title='Cost'
             column: title='Price'
             column: title='Stock'
-            column: title='NCM', hidden
             column: title='NCM'
-            row: u'01', u'2368694135945', u'Bermudas Sarja', u'Bermuda Sarja', None, u'', None, u'', <currency $70>, Decimal('149.00'), Decimal('3.000'), u'12345678', u'12345678'
-            row: u'02', u'6234564656756', u'Bermudas Shorts', u'Shorts com Costuras', None, u'', None, u'', <currency $139>, Decimal('198.00'), Decimal('3.000'), None, None
-            row: u'03', u'6985413595971', u'Tinturada', u'Camiseta Tinturada Xcuba Comp', None, u'', None, u'', <currency $56>, Decimal('89.00'), Decimal('3.000'), None, None
-            row: u'04', u'2692149835416', u'Chinelos', u'Chinelo de Dedo', None, u'', None, u'', <currency $43>, Decimal('79.00'), Decimal('3.000'), None, None
-            row: u'05', u'1595843695465', u'Cal\xe7as Jeans', u'Cal\xe7a Jeans com Bolso Traseiro', None, u'', None, u'', <currency $101>, Decimal('189.00'), Decimal('3.000'), None, None
-            row: u'06', u'8596458216412', u'Cal\xe7as Poliester', u'Cal\xe7a Brigilda', None, u'', None, u'', <currency $325>, Decimal('438.00'), Decimal('3.000'), None, None
-            row: u'07', u'9586249534513', u'Algod\xe3o', u'Casaco Vilan', None, u'', None, u'', <currency $401>, Decimal('503.00'), Decimal('3.000'), None, None
-            row: u'08', u'7826592136954', u'Blusas Algod\xe3o', u'Blusa Frente-\xdanica com Pregas', None, u'', None, u'', <currency $153>, Decimal('220.00'), Decimal('3.000'), None, None
-            row: u'09', u'5892458629421', u'Blusas Poliester', u'Blusa Balon\xea com Bordado nas Costas', None, u'', None, u'', <currency $99>, Decimal('150.00'), Decimal('3.000'), None, None
-            row: u'10', u'1598756984265', u'Viscose', u'Vestido Vadalena', None, u'', None, u'', <currency $199>, Decimal('383.00'), Decimal('4.000'), None, None
+            row: u'01', u'2368694135945', u'Bermudas Sarja', u'Bermuda Sarja', None, u'', u'12345678', None, u'', <currency $70>, Decimal('149.00'), Decimal('3.000'), u'12345678'
+            row: u'02', u'6234564656756', u'Bermudas Shorts', u'Shorts com Costuras', None, u'', None, None, u'', <currency $139>, Decimal('198.00'), Decimal('3.000'), None
+            row: u'03', u'6985413595971', u'Tinturada', u'Camiseta Tinturada Xcuba Comp', None, u'', None, None, u'', <currency $56>, Decimal('89.00'), Decimal('3.000'), None
+            row: u'04', u'2692149835416', u'Chinelos', u'Chinelo de Dedo', None, u'', None, None, u'', <currency $43>, Decimal('79.00'), Decimal('3.000'), None
+            row: u'05', u'1595843695465', u'Cal\xe7as Jeans', u'Cal\xe7a Jeans com Bolso Traseiro', None, u'', None, None, u'', <currency $101>, Decimal('189.00'), Decimal('3.000'), None
+            row: u'06', u'8596458216412', u'Cal\xe7as Poliester', u'Cal\xe7a Brigilda', None, u'', None, None, u'', <currency $325>, Decimal('438.00'), Decimal('3.000'), None
+            row: u'07', u'9586249534513', u'Algod\xe3o', u'Casaco Vilan', None, u'', None, None, u'', <currency $401>, Decimal('503.00'), Decimal('3.000'), None
+            row: u'08', u'7826592136954', u'Blusas Algod\xe3o', u'Blusa Frente-\xdanica com Pregas', None, u'', None, None, u'', <currency $153>, Decimal('220.00'), Decimal('3.000'), None
+            row: u'09', u'5892458629421', u'Blusas Poliester', u'Blusa Balon\xea com Bordado nas Costas', None, u'', None, None, u'', <currency $99>, Decimal('150.00'), Decimal('3.000'), None
+            row: u'10', u'1598756984265', u'Viscose', u'Vestido Vadalena', None, u'', None, None, u'', <currency $199>, Decimal('383.00'), Decimal('4.000'), None
       GtkHBox(fill=True):
         GtkEventBox(extra_holder, expand=True, fill=True, padding=6): slave SearchEditorToolBar is attached
           GtkHBox(toplevel):

stoqlib/gui/dialogs/sellabledialog.py
@@ -30,7 +30,9 @@ from stoqdrivers.enum import TaxType
 
 from stoqlib.api import api
 from stoqlib.domain.sellable import (Sellable, ClientCategoryPrice,
-                                     SellableCategory, SellableTaxConstant)
+                                     SellableUnit, SellableCategory,
+                                     SellableTaxConstant)
+from stoqlib.domain.fiscal import CfopData
 from stoqlib.gui.base.lists import ModelListDialog, ModelListSlave
 from stoqlib.gui.editors.sellableeditor import SellableTaxConstantEditor
 from stoqlib.gui.dialogs.masseditordialog import (Field, MassEditorSearch,
@@ -39,7 +41,7 @@ from stoqlib.lib.translation import stoqlib_gettext
 from stoqlib.lib.message import info
 from stoqlib.database.viewable import Viewable
 from stoqlib.domain.service import Service
-from stoqlib.domain.product import Product, Storable
+from stoqlib.domain.product import Product, Storable, ProductManufacturer
 from stoqlib.domain.person import ClientCategory
 
 _ = stoqlib_gettext
@@ -124,7 +126,6 @@ class SellableView(Viewable):
     product = Product
     storable = Storable
     service = Service
-    category = SellableCategory
 
     id = Sellable.id
 
@@ -146,12 +147,20 @@ class SellableMassEditorDialog(MassEditorSearch):
     search_spec = SellableView
 
     def get_fields(self, store):
+        # TODO: Add: status, max_discount
         default_fields = [
+            # Sellable fields
             AccessorField(_('Code'), 'sellable', 'code', unicode, unique=True),
             AccessorField(_('Barcode'), 'sellable', 'barcode', unicode, unique=True),
             ReferenceField(_('Category'), 'sellable', 'category',
                            SellableCategory, 'description'),
             AccessorField(_('Description'), 'sellable', 'description', unicode),
+            ReferenceField(_('Unit'), 'sellable', 'unit',
+                           SellableUnit, 'description', visible=False),
+            ReferenceField(_('C.F.O.P.'), 'sellable', 'default_sale_cfop',
+                           CfopData, 'description', visible=False),
+
+            # Sellable values
             AccessorField(_('Cost'), 'sellable', 'cost', currency,
                           validator=validate_price),
             AccessorField(_('Default Price'), 'sellable', 'base_price', currency,
@@ -162,6 +171,16 @@ class SellableMassEditorDialog(MassEditorSearch):
                           datetime.date),
             AccessorField(_('On Sale End Date'), 'sellable', 'on_sale_end_date',
                           datetime.date),
+
+            # Product Fields
+            AccessorField(_('NCM'), 'product', 'ncm', unicode, visible=False),
+            AccessorField(_('Location'), 'product', 'location', unicode, visible=False),
+            AccessorField(_('Brand'), 'product', 'brand', unicode, visible=False),
+            AccessorField(_('Family'), 'product', 'family', unicode, visible=False),
+            AccessorField(_('Model'), 'product', 'model', unicode, visible=False),
+
+            ReferenceField(_('Manufacturer'), 'product', 'manufacturer',
+                           ProductManufacturer, 'name', visible=False),
         ]
 
         category_fields = []

stoqlib/gui/test/test_search.py
@@ -261,6 +261,7 @@ class TestSearchGeneric(DomainTest):
     ignored_classes = [
         '_BaseBillCheckSearch',
         'SearchEditor',
+        'SellableMassEditorDialog',
         'MassEditorSearch',
         'BasePersonSearch',
     ]

commit d5a702aad832becfd11bd5f119cad2cc33a825dc

Fix a few issues with user settings
- Remove (really) old migration code. It was making welcome dialog be
  displayed twice.
- standarize naming in all set/get/remove methods. Fixes a few sentry
  issues.
- Fix column restoring (bug introduced by me in commit 68b6dae)

Change-Id: I007a1e155e8914371a6f84cc9640a7ba2c3406e1

stoq/gui/shell/shell.py
@@ -106,7 +106,7 @@ class ShellDatabaseConnection(object):
         from stoqlib.lib.message import error
         try:
             store_uri = self._config.get_settings().get_store_uri()
-        except:
+        except Exception:
             type, value, trace = sys.exc_info()
             error(_("Could not open the database config file"),
                   _("Invalid config file settings, got error '%s', "
@@ -188,7 +188,7 @@ class ShellDatabaseConnection(object):
 
         compaines = default_store.find(Company)
         if (compaines.count() == 0 or
-            not sysparam.has_object('MAIN_COMPANY')):
+                not sysparam.has_object('MAIN_COMPANY')):
             from stoqlib.gui.base.dialogs import run_dialog
             from stoqlib.gui.dialogs.branchdialog import BranchDialog
             if self._ran_wizard:
@@ -225,6 +225,7 @@ class Shell(object):
     - handles login
     - runs applications
     """
+
     def __init__(self, bootstrap, options, initial=True):
         global _shell
         _shell = self
@@ -303,7 +304,7 @@ class Shell(object):
         from stoqlib.lib.pluginmanager import get_plugin_manager
         manager = get_plugin_manager()
         if (sysparam.get_bool('DEMO_MODE') and
-            manager.is_active(u'ecf')):
+                manager.is_active(u'ecf')):
             pos = shell_window.toplevel.get_position()
             if pos[0] < 220:
                 shell_window.toplevel.move(220, pos[1])

stoq/gui/shell/shellwindow.py
@@ -134,7 +134,6 @@ class ShellWindow(GladeDelegate):
                 'NSApplicationBlockTermination',
                 self._on_osx__block_termination)
             self._osx_app.set_use_quartz_accelerators(True)
-        api.user_settings.migrate()
         self._app_settings = api.user_settings.get('app-ui', {})
         self._create_shared_actions()
         if self.options.debug:

stoqlib/gui/dialogs/masseditordialog.py
@@ -598,6 +598,7 @@ class MassEditorWidget(gtk.HBox):
 class MassEditorSearch(SearchDialog):
     size = (850, 450)
     unlimited_results = True
+    save_columns = False
 
     def __init__(self, store):
         self._fields = self.get_fields(store)

stoqlib/gui/search/searchdialog.py
@@ -142,7 +142,7 @@ class SearchDialog(BasicDialog):
     unlimited_results = False
 
     #: If the column settings should be saved or not.
-    save_columns = False
+    save_columns = True
 
     def __init__(self, store, search_spec=None, hide_footer=True,
                  title='', selection_mode=None, double_click_confirm=False,

stoqlib/gui/search/searchslave.py
@@ -25,7 +25,6 @@
 import datetime
 import decimal
 import logging
-import os
 import warnings
 
 import gtk
@@ -51,7 +50,6 @@ from stoqlib.gui.search.searchresultview import (SearchResultListView,
                                                  SearchResultTreeView)
 from stoqlib.gui.widgets.lazyobjectlist import LazySummaryLabel
 from stoqlib.gui.widgets.searchfilterbutton import SearchFilterButton
-from stoqlib.lib.osutils import get_application_dir
 from stoqlib.lib.parameters import sysparam
 from stoqlib.lib.translation import stoqlib_gettext
 
@@ -174,19 +172,6 @@ class SearchSlave(SlaveDelegate):
         self.search_button.show()
 
         self.filters_box = filters_box
-
-    def _migrate_from_pickle(self):
-        username = api.get_current_user(self.store).username
-        filename = os.path.join(get_application_dir(), 'columns-%s' % username,
-                                self._restore_name + '.pickle')
-        log.info("Migrating columns from pickle: %s" % (filename, ))
-        try:
-            with open(filename) as fd:
-                import cPickle
-                return cPickle.load(fd)
-        except Exception, e:
-            log.info("Exception while migrating: %r" % (e, ))
-            return {}
 
     def _set_filter_position(self, search_filter, position):
         """
@@ -641,10 +626,7 @@ class SearchSlave(SlaveDelegate):
             return cols
 
         columns = api.user_settings.get(self._settings_key, {})
-        if columns:
-            saved = columns.get(self._restore_name, {})
-        else:
-            saved = self._migrate_from_pickle()
+        saved = columns.get(self._restore_name, {})
 
         cols_dict = {}
         for original_pos, col in enumerate(cols):

stoqlib/lib/settings.py
@@ -29,9 +29,6 @@ import json
 import logging
 import os
 
-from kiwi.component import get_utility
-
-from stoqlib.lib.interfaces import IStoqConfig
 from stoqlib.lib.osutils import get_application_dir
 
 log = logging.getLogger(__name__)
@@ -76,24 +73,30 @@ def _encode_object(obj):
         'is not JSON serializable' % (type(obj), obj))
 
 
+def _fix_name(name):
+    name = name.replace('_', '-')
+    return name.encode('utf-8')
+
+
 class UserSettings(object):
     domain = 'stoq'
 
-    def __init__(self):
+    def __init__(self, filename=None):
         self._root = {}
-        self._migrated = False
-        self._read()
+        self._read(filename)
 
     def set(self, name, value):
-        name = name.replace('_', '-')
-        self._root[name.encode('utf-8')] = value
+        self._root[_fix_name(name)] = value
 
     def get(self, name, default=None):
+        name = _fix_name(name)
         if not name in self._root:
             self.set(name, default)
-        return self._root[name]
+        value = self._root[name]
+        return value
 
     def remove(self, name):
+        name = _fix_name(name)
         if name in self._root:
             del self._root[name]
 
@@ -116,8 +119,8 @@ class UserSettings(object):
 
     # Private
 
-    def _read(self):
-        filename = self.get_filename()
+    def _read(self, filename):
+        filename = filename or self.get_filename()
         try:
             fd = open(filename)
         except IOError as e:
@@ -131,9 +134,6 @@ class UserSettings(object):
             self._root = json.loads(data, object_hook=_decode_dict)
         except ValueError:
             self._root = {}
-
-        # FIXME: should be loaded
-        self._migrated = True
 
     def _write(self, data):
         filename = self.get_filename()
@@ -150,42 +150,6 @@ class UserSettings(object):
                 return
         fd.close()
 
-    def migrate(self):
-        if self._migrated:
-            return
-        log.info("Migrating settings from Stoq.conf")
-
-        # Migrate from old configuration settings
-        try:
-            config = get_utility(IStoqConfig)
-        except NotImplementedError:
-            # For unittests, migrating jenkins from old to new settings
-            return
-
-        self.set('hide-demo-warning',
-                 config.get('UI', 'hide_demo_warning') == 'True')
-
-        width = int(config.get('Launcher', 'window_width') or -1)
-        height = int(config.get('Launcher', 'window_height') or -1)
-        x = int(config.get('Launcher', 'window_x') or -1)
-        y = int(config.get('Launcher', 'window_y') or -1)
-        self.set('launcher-geometry',
-                 dict(width=width, height=height, x=x, y=y))
-
-        self.set('last-version-check',
-                 config.get('General', 'last-version-check'))
-
-        self.set('latest-version',
-                 config.get('General', 'latest-version'))
-
-        self.set('show-welcome-dialog',
-                 config.get('General', 'show_welcome_dialog') != 'False')
-
-        d = {}
-        for k, v in config.items('Shortcuts'):
-            d[k] = v
-        self.set('shortcuts', d)
-        self._migrated = True
 
 _settings = None
 
@@ -195,6 +159,7 @@ def get_settings():
     if _settings is None:
         _settings = UserSettings()
     return _settings
+
 
 if __name__ == '__main__':  # pragma nocover
     s = UserSettings()

stoqlib/lib/test/test_settings.py
@@ -1,0 +1,44 @@
+import datetime
+from decimal import Decimal
+import unittest
+import tempfile
+
+from stoqlib.lib.settings import UserSettings, _decode_list, _encode_object
+
+
+class SettingsTest(unittest.TestCase):
+
+    def test_settings_process(self):
+        with tempfile.NamedTemporaryFile() as tmp:
+            settings = UserSettings(filename=tmp.name)
+
+            # Set a value and recover it.
+            settings.set('value', 123)
+            self.assertEquals(settings.get('value'), 123)
+
+            # Get a non existing value with a default fallback
+            self.assertEquals(settings.get('other_value', 456), 456)
+
+            # Getting it again should have saved the value
+            self.assertEquals(settings.get('other_value'), 456)
+
+            # Now, lets remove that value.
+            settings.remove('other_value')
+            self.assertEquals(settings.items(), [('value', 123)])
+
+            # Getting it again should return None
+            self.assertEquals(settings.get('other_value'), None)
+
+            # Flush
+            settings.flush()
+
+    def test_decode_list(self):
+        value = [u'unicode', [123], {u'a': u'b'}]
+        self.assertEquals(_decode_list(value), value)
+
+    def test_encode_object(self):
+        self.assertEquals(_encode_object(Decimal('0.99')), '0.99')
+        self.assertEquals(_encode_object(datetime.date(2016, 02, 07)),
+                          '2016-02-07')
+        self.assertEquals(_encode_object(datetime.datetime(2016, 02, 07, 19, 45, 30)),
+                          '2016-02-07T19:45:30')

tests/data/ui/search-product-search-extended.uitest
@@ -36,23 +36,23 @@ GtkDialog(toplevel): title='Product Search', hidden
             column: title='Description', expand
             column: title='Manufacturer', hidden
             column: title='Model', hidden
-            column: title='NCM', hidden
             column: title='Unit', hidden
             column: title='Location', hidden
             column: title='Cost'
             column: title='Price'
             column: title='Stock'
+            column: title='NCM', hidden
             column: title='NCM'
-            row: u'01', u'2368694135945', u'Bermudas Sarja', u'Bermuda Sarja', None, u'', u'12345678', None, u'', <currency $70>, Decimal('149.00'), Decimal('3.000'), u'12345678'
-            row: u'02', u'6234564656756', u'Bermudas Shorts', u'Shorts com Costuras', None, u'', None, None, u'', <currency $139>, Decimal('198.00'), Decimal('3.000'), None
-            row: u'03', u'6985413595971', u'Tinturada', u'Camiseta Tinturada Xcuba Comp', None, u'', None, None, u'', <currency $56>, Decimal('89.00'), Decimal('3.000'), None
-            row: u'04', u'2692149835416', u'Chinelos', u'Chinelo de Dedo', None, u'', None, None, u'', <currency $43>, Decimal('79.00'), Decimal('3.000'), None
-            row: u'05', u'1595843695465', u'Cal\xe7as Jeans', u'Cal\xe7a Jeans com Bolso Traseiro', None, u'', None, None, u'', <currency $101>, Decimal('189.00'), Decimal('3.000'), None
-            row: u'06', u'8596458216412', u'Cal\xe7as Poliester', u'Cal\xe7a Brigilda', None, u'', None, None, u'', <currency $325>, Decimal('438.00'), Decimal('3.000'), None
-            row: u'07', u'9586249534513', u'Algod\xe3o', u'Casaco Vilan', None, u'', None, None, u'', <currency $401>, Decimal('503.00'), Decimal('3.000'), None
-            row: u'08', u'7826592136954', u'Blusas Algod\xe3o', u'Blusa Frente-\xdanica com Pregas', None, u'', None, None, u'', <currency $153>, Decimal('220.00'), Decimal('3.000'), None
-            row: u'09', u'5892458629421', u'Blusas Poliester', u'Blusa Balon\xea com Bordado nas Costas', None, u'', None, None, u'', <currency $99>, Decimal('150.00'), Decimal('3.000'), None
-            row: u'10', u'1598756984265', u'Viscose', u'Vestido Vadalena', None, u'', None, None, u'', <currency $199>, Decimal('383.00'), Decimal('4.000'), None
+            row: u'01', u'2368694135945', u'Bermudas Sarja', u'Bermuda Sarja', None, u'', None, u'', <currency $70>, Decimal('149.00'), Decimal('3.000'), u'12345678', u'12345678'
+            row: u'02', u'6234564656756', u'Bermudas Shorts', u'Shorts com Costuras', None, u'', None, u'', <currency $139>, Decimal('198.00'), Decimal('3.000'), None, None
+            row: u'03', u'6985413595971', u'Tinturada', u'Camiseta Tinturada Xcuba Comp', None, u'', None, u'', <currency $56>, Decimal('89.00'), Decimal('3.000'), None, None
+            row: u'04', u'2692149835416', u'Chinelos', u'Chinelo de Dedo', None, u'', None, u'', <currency $43>, Decimal('79.00'), Decimal('3.000'), None, None
+            row: u'05', u'1595843695465', u'Cal\xe7as Jeans', u'Cal\xe7a Jeans com Bolso Traseiro', None, u'', None, u'', <currency $101>, Decimal('189.00'), Decimal('3.000'), None, None
+            row: u'06', u'8596458216412', u'Cal\xe7as Poliester', u'Cal\xe7a Brigilda', None, u'', None, u'', <currency $325>, Decimal('438.00'), Decimal('3.000'), None, None
+            row: u'07', u'9586249534513', u'Algod\xe3o', u'Casaco Vilan', None, u'', None, u'', <currency $401>, Decimal('503.00'), Decimal('3.000'), None, None
+            row: u'08', u'7826592136954', u'Blusas Algod\xe3o', u'Blusa Frente-\xdanica com Pregas', None, u'', None, u'', <currency $153>, Decimal('220.00'), Decimal('3.000'), None, None
+            row: u'09', u'5892458629421', u'Blusas Poliester', u'Blusa Balon\xea com Bordado nas Costas', None, u'', None, u'', <currency $99>, Decimal('150.00'), Decimal('3.000'), None, None
+            row: u'10', u'1598756984265', u'Viscose', u'Vestido Vadalena', None, u'', None, u'', <currency $199>, Decimal('383.00'), Decimal('4.000'), None, None
       GtkHBox(fill=True):
         GtkEventBox(extra_holder, expand=True, fill=True, padding=6): slave SearchEditorToolBar is attached
           GtkHBox(toplevel):