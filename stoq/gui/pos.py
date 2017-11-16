22fbd293ce5242f6f8be8a63e0c4e71d6c89487c
Ronaldo Maia <romaia@async.com.br>
2017-11-16 16:06:52 -0200

    Print sale details on POS with confirm sales on till
    When param to confirm sales on till is set and the param to print a sale report on POS
    is also set, print the sale details report so that the client can go to the till and pay
    for it.
    
    Fixes: https://tree.taiga.io/project/stoq/task/1075
    Change-Id: Ia2b39c8f92acc5e2bf64f95654896c859f3b7972

--- stoq/gui/pos.py
+++ stoq/gui/pos.py
@@ -78,8 +78,10 @@ from stoqlib.gui.search.paymentreceivingsearch import PaymentReceivingSearch
 from stoqlib.gui.search.workordersearch import WorkOrderFinishedSearch
 from stoqlib.gui.utils.keybindings import get_accels
 from stoqlib.gui.utils.logo import render_logo_pixbuf
+from stoqlib.gui.utils.printing import print_report
 from stoqlib.gui.wizards.loanwizard import CloseLoanWizard
 from stoqlib.gui.wizards.salereturnwizard import SaleTradeWizard
+from stoqlib.reporting.sale import SaleOrderReport
 
 from stoq.gui.shell.shellapp import ShellApp
 
@@ -1229,6 +1231,8 @@ class PosApp(ShellApp):
             if sale.status != Sale.STATUS_ORDERED and not sale.get_items().is_empty():
                 sale.order()
             store.commit()
+            if sysparam.get_bool('PRINT_SALE_DETAILS_ON_POS'):
+                self._print_sale_details(sale)
         else:
             if sysparam.get_bool('USE_SALE_TOKEN'):
                 coupon = self._open_coupon()
@@ -1322,6 +1326,11 @@ class PosApp(ShellApp):
                 "the payment.") % (value, )
         self._trade_infobar = self.window.add_info_bar(
             gtk.MESSAGE_INFO, msg, action_widget=button)
+
+    def _print_sale_details(self, sale):
+        if yesno(_("Do you want to print this sale's details?"), gtk.RESPONSE_YES,
+                 _("Print Details"), _("Don't Print")):
+            print_report(SaleOrderReport, sale)
 
     #
     # Coupon related
