7b411cf50ca7b782122c45f794cf93ab44df0f75
Ronaldo Maia <romaia@async.com.br>
2018-02-19 15:58:47 -0300

    Simplify idle timeout detection.
    We had 3 different codes to detect idle time (for automatic logout). Leave only the one
    that uses pure gdk, since that works both on linux and windows.
    
    It's also better to use that one since it will still logout the user from stoq even if
    he is stil using other apps besides stoq. I.e: this one consideres inactivity only
    within stoq.
    
    Change-Id: I8b59901a8ae54c9e0930a283db90697030f62427
    Fixes: https://tree.taiga.io/project/stoq/task/1436

--- stoqlib/gui/utils/idle.py
+++ stoqlib/gui/utils/idle.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 
 ##
-## Copyright (C) 2013 Async Open Source
+## Copyright (C) 2013-2018 Async Open Source
 ##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU Lesser General Public License
@@ -27,92 +27,7 @@ This modules has one function :func:`get_idle_seconds` that returns
 the number of the seconds since the user has used the keyboard or mouse.
 """
 
-# Based on Pidgin's gtkidle.c
-
-import ctypes
-import ctypes.util
-import platform
-
-from gi.repository import Gtk, Gdk, GLib, GIRepository
-
-
-class LASTINPUTINFO(ctypes.Structure):
-    _fields_ = [
-        ('cbSize', ctypes.c_uint),
-        ('dwTime', ctypes.c_uint),
-    ]
-
-
-class XScreenSaverInfo(ctypes.Structure):
-    _fields_ = [('window', ctypes.c_long),
-                ('state', ctypes.c_int),
-                ('kind', ctypes.c_int),
-                ('til_or_since', ctypes.c_ulong),
-                ('idle', ctypes.c_ulong),
-                ('eventMask', ctypes.c_ulong)]
-
-
-class IdleXScreenSaver(object):
-    def __init__(self):
-        self.xss = self._get_library('Xss')
-
-        # FIXME: We could use gi.repository.GdkX11 here but then we would
-        # not be able to pass the python objects to xss.
-        repo = GIRepository.Repository.get_default()
-        # This is probably libgdk-3.so
-        gdk_path = repo.get_shared_library('GdkX11').split(',')[0]
-        self.gdk_ = ctypes.cdll.LoadLibrary(gdk_path)
-
-        self.gdk_.gdk_display_get_default.restype = ctypes.c_void_p
-        # GDK_DISPLAY_XDISPLAY expands to gdk_x11_display_get_xdisplay
-        self.gdk_.gdk_x11_display_get_xdisplay.restype = ctypes.c_void_p
-        self.gdk_.gdk_x11_display_get_xdisplay.argtypes = [ctypes.c_void_p]
-        # GDK_ROOT_WINDOW expands to gdk_x11_get_default_root_xwindow
-        self.gdk_.gdk_x11_get_default_root_xwindow.restype = ctypes.c_void_p
-
-        self.xss.XScreenSaverAllocInfo.restype = ctypes.POINTER(XScreenSaverInfo)
-        self.xss.XScreenSaverQueryExtension.restype = ctypes.c_int
-        self.xss.XScreenSaverQueryExtension.argtypes = [ctypes.c_void_p,
-                                                        ctypes.POINTER(ctypes.c_int),
-                                                        ctypes.POINTER(ctypes.c_int)]
-        self.xss.XScreenSaverQueryInfo.restype = ctypes.c_int
-        self.xss.XScreenSaverQueryInfo.argtypes = [ctypes.c_void_p,
-                                                   ctypes.c_void_p,
-                                                   ctypes.POINTER(XScreenSaverInfo)]
-
-        # has_extension = XScreenSaverQueryExtension(
-        #     GDK_DISPLAY_XDISPLAY(gdk_display_get_default()),
-        #     &event_base, &error_base);
-        event_base = ctypes.c_int()
-        error_base = ctypes.c_int()
-        gtk_display = self.gdk_.gdk_display_get_default()
-        self.dpy = self.gdk_.gdk_x11_display_get_xdisplay(gtk_display)
-        available = self.xss.XScreenSaverQueryExtension(self.dpy,
-                                                        ctypes.byref(event_base),
-                                                        ctypes.byref(error_base))
-        if available == 1:
-            self.xss_info = self.xss.XScreenSaverAllocInfo()
-        else:
-            self.xss_info = None
-
-    def _get_library(self, libname):
-        path = ctypes.util.find_library(libname)
-        if not path:
-            raise ImportError('Could not find library "%s"' % (libname, ))
-        lib = ctypes.cdll.LoadLibrary(path)
-        assert lib
-        return lib
-
-    def get_idle(self):
-        if not self.xss_info:
-            return 0
-
-        # XScreenSaverQueryInfo(GDK_DISPLAY_XDISPLAY(gdk_display_get_default()),
-        #                       GDK_ROOT_WINDOW(), mit_info);
-        drawable = self.gdk_.gdk_x11_get_default_root_xwindow()
-        self.xss.XScreenSaverQueryInfo(self.dpy, drawable, self.xss_info)
-        # return (mit_info->idle) / 1000;
-        return self.xss_info.contents.idle / 1000
+from gi.repository import Gtk, Gdk, GLib
 
 
 class IdleEventHandler(object):
@@ -138,8 +53,8 @@ class IdleEventHandler(object):
     def get_idle(self):
         return self._idle
 
+
 _idle = None
-_system = None
 
 
 def get_idle_seconds():
@@ -149,29 +64,7 @@ def get_idle_seconds():
     :returns: idle seconds
     :rtype: int
     """
-    global _idle, _system
-    if _system is None:
-        _system = platform.system()
-
-    if _system == 'Linux':
-        if _idle is None:
-            _idle = IdleXScreenSaver()
-
-            # For X servers such as XMing which lacks the
-            # the screen saver extension
-            if _idle.xss_info is None:
-                _idle = IdleEventHandler()
-
-    elif _system == 'Windows':
-        # Same as pidgin:
-        # http://stackoverflow.com/questions/911856/detecting-idle-time-in-python
-        lastInputInfo = LASTINPUTINFO()
-        lastInputInfo.cbSize = ctypes.sizeof(lastInputInfo)
-        ctypes.windll.user32.GetLastInputInfo(ctypes.byref(lastInputInfo))
-        millis = ctypes.windll.kernel32.GetTickCount() - lastInputInfo.dwTime
-        return int(millis / 1000.0)
-
-    else:
-        raise NotImplementedError(_system)
-
+    global _idle
+    if _idle is None:
+        _idle = IdleEventHandler()
     return _idle.get_idle()
