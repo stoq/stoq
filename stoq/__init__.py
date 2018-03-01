11e82c607fc630792755d96a2a97ac4e7f9773b9
felipenehmi <felipe@async.com.br>
2018-03-01 10:25:17 -0300

    Release Stoq 3.0.0 beta3
    Change-Id: I4cc60065c0092a595303896323ebc41ba86c47b0
    Fixes: https://tree.taiga.io/project/stoq/us/1474

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 0
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'beta2'
+extra_version = 'beta3'
 
 #: the date the software was released
-release_date = (2017, 12, 12)
+release_date = (2018, 3, 1)
 
 #: if this is a stable release
 stable = True
