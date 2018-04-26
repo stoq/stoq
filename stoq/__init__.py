99113a6c22473533fe34499fd8313450b45c7b15
Ronaldo Maia <romaia@async.com.br>
2018-04-26 14:43:47 -0300

    Release 3.0rc2
    Fixes: https://tree.taiga.io/project/stoq/task/1645
    Change-Id: Ia15270a03642bde536f308615af123addb46a113

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 0
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = 'rc2'
 
 #: the date the software was released
-release_date = (2018, 4, 18)
+release_date = (2018, 4, 26)
 
 #: if this is a stable release
 stable = True
