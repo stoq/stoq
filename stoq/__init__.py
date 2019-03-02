880de5528615ec2b7787946ab737c78b1af791ed
Ronaldo Maia <romaia@async.com.br>
2019-03-02 12:06:26 -0300

    Release 3.1 final
    Fixes: https://gitlab.com/stoqtech/stoq/milestones/1
    Change-Id: I8563cf73000e3d93222852571e42741f2b42c77d

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 1
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc10'
+extra_version = ''
 
 #: the date the software was released
-release_date = (2019, 2, 22)
+release_date = (2019, 3, 2)
 
 #: if this is a stable release
 stable = True
