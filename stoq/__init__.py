d74c247097aa6ec4263980ce07cdcb1190fb7310
Luis Oishi <luisoishi@async.com.br>
2019-04-30 15:55:51 -0300

    Release bugfix 3.1.2
    Fixes: https://gitlab.com/stoqtech/stoq/issues/38
    Change-Id: I2837010c77deb2949bea938b89fd0b5001cafd63

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -34,13 +34,13 @@ major_version = 3
 minor_version = 1
 
 #: Micro version, increase when doing a bug fix for a stable release
-micro_version = 1
+micro_version = 2
 
 #: extra version, rc1, rc2, etc goes here.
 extra_version = ''
 
 #: the date the software was released
-release_date = (2019, 3, 5)
+release_date = (2019, 4, 30)
 
 #: if this is a stable release
 stable = True
