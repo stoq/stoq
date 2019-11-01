2833f9f75d59df7ed9253daa2e2a9b0dcd73be93
Luiz Menezes <luiz.menezesf@gmail.com>
2019-11-01 17:51:12 -0300

    Add missing product_branch_override.c_benef column to database patch

--- data/sql/patch-06-15.sql
+++ data/sql/patch-06-15.sql
@@ -1,4 +1,5 @@
 ALTER TABLE product ADD COLUMN c_benef TEXT default NULL;
+ALTER TABLE product_branch_override ADD COLUMN c_benef TEXT default NULL;
 
 ALTER TABLE invoice_item_icms ADD COLUMN v_icms_deson NUMERIC(20, 2) default NULL;
 ALTER TABLE product_icms_template ADD COLUMN mot_des_icms INTEGER;
