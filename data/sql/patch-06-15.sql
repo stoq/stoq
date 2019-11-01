5bbee0ed549fe2b83e9ab3544ab47af2813c7efd
Luiz Menezes <luiz.menezesf@gmail.com>
2019-11-01 15:36:37 -0300

    Add schema for new mandatory fields of the NFe
    Fields: cBenef (código benefício fiscal), motDesICMS (motivo desoneração ICMS)
    and vICMSDeson

---
+++ data/sql/patch-06-15.sql
@@ -0,0 +1,5 @@
+ALTER TABLE product ADD COLUMN c_benef TEXT default NULL;
+
+ALTER TABLE invoice_item_icms ADD COLUMN v_icms_deson NUMERIC(20, 2) default NULL;
+ALTER TABLE product_icms_template ADD COLUMN mot_des_icms INTEGER;
+ALTER TABLE product_icms_template ADD CONSTRAINT valid_mot_des_icms CHECK (mot_des_icms IN (3, 9, 12));
