94b5a3c455e37a40f0bff205b64a92c4523e4d77
Fabricio C Zuardi <fabricio@fabricio.org>
2019-11-11 11:41:15 +0000

    Release 4.2
    Related to: https://gitlab.com/stoqtech/squads/kazoo/tasks/issues/28

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 2
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = ''
 
 #: the date the software was released
-release_date = (2019, 11, 6)
+release_date = (2019, 11, 11)
 
 #: if this is a stable release
 stable = True
