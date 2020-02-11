c2d3f50da3fde1baf12c8f9510051afb2b0f4a55
Fabricio C Zuardi <fabricio@fabricio.org>
2020-02-11 13:36:15 -0300

    Release 4.5~rc2
    Includes decimal fix

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 5
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = 'rc2'
 
 #: the date the software was released
-release_date = (2020, 2, 3)
+release_date = (2020, 2, 11)
 
 #: if this is a stable release
 stable = True
