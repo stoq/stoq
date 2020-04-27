8eac2c3924b8f1a3e45dab514aa0a149ac3f1e8b
Fabricio C Zuardi <fabricio@fabricio.org>
2020-04-27 10:38:07 -0300

    Release 4.8 stable
    Related to: https://gitlab.com/stoqtech/squads/kazoo/tasks/-/issues/55

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 8
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = ''
 
 #: the date the software was released
-release_date = (2020, 4, 19)
+release_date = (2020, 4, 27)
 
 #: if this is a stable release
 stable = True
