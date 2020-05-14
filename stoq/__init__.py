1a261078b20c446c0849cef296b3a9f7b58e53ff
Ronaldo Maia <romaia@async.com.br>
2020-05-14 09:34:22 -0300

    Release 4.9~rc2

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 9
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = 'rc2'
 
 #: the date the software was released
-release_date = (2020, 5, 13)
+release_date = (2020, 5, 14)
 
 #: if this is a stable release
 stable = True
