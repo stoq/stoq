a66c08b226af81e934b165b469ef03b3b76ea032
Ronaldo Maia <romaia@async.com.br>
2020-05-18 15:22:23 -0300

    Release 5.0~rc1

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -28,16 +28,16 @@ website = 'http://www.stoq.com.br'
 
 #: Major version, should only increase when big, important features
 #: are integrated.
-major_version = 4
+major_version = 5
 
 #: Minor version, increase when doing a new stable release
-minor_version = 9
+minor_version = 0
 
 #: Micro version, increase when doing a bug fix for a stable release
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = ''
+extra_version = 'rc1'
 
 #: the date the software was released
 release_date = (2020, 5, 18)
