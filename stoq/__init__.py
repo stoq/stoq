8aecf21543c6cd49187da9b06848f738cd4779f5
Vinícius Matarazo Camillo <vmcamillo@outlook.com>
2020-06-16 11:28:19 -0300

    Release 5.0~rc3
    Related to: https://gitlab.com/stoqtech/squads/kazoo/tasks/-/issues/69
    
    Change-Id: Icb220fcfe0d1b77d1f80d5069090b4869054bcb6

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 0
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = 'rc3'
 
 #: the date the software was released
-release_date = (2020, 5, 18)
+release_date = (2020, 6, 16)
 
 #: if this is a stable release
 stable = True
