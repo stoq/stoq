dd0a1b2ef6df48e6ff2f0a4b585e95bfd8971946
Vinícius Matarazo Camillo <vmcamillo@outlook.com>
2020-07-20 16:56:19 -0300

    Release 5.1
    Related to: https://gitlab.com/stoqtech/squads/kazoo/tasks/-/issues/76

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -37,10 +37,10 @@ minor_version = 1
 micro_version = 0
 
 #: extra version, rc1, rc2, etc goes here.
-extra_version = 'rc1'
+extra_version = ''
 
 #: the date the software was released
-release_date = (2020, 7, 10)
+release_date = (2020, 7, 20)
 
 #: if this is a stable release
 stable = True
