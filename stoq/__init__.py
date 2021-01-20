326734994da1d3187353cc46150084e5532e1cf0
Vinícius Matarazo Camillo <vmcamillo@outlook.com>
2021-01-20 14:56:56 -0300

    Fix generate_deb job

--- stoq/__init__.py
+++ stoq/__init__.py
@@ -34,13 +34,13 @@ major_version = 7
 minor_version = 4
 
 #: Micro version, increase when doing a bug fix for a stable release
-micro_version = 0
+micro_version = 1
 
 #: extra version, rc1, rc2, etc goes here.
 extra_version = ''
 
 #: the date the software was released
-release_date = (2021, 1, 14)
+release_date = (2021, 1, 20)
 
 #: if this is a stable release
 stable = True
